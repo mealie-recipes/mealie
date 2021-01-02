8631152aa379dadec7504eafdd4f9ad7af5a34d5
Hayden <hay-kot@pm.me>
2021-01-01 16:56:19 -0900

    docker file

--- mealie/db/mongo_setup.py
+++ mealie/db/mongo_setup.py
@@ -2,11 +2,10 @@ import mongoengine
 from settings import DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME
 
 
-
 def global_init():
     mongoengine.register_connection(
         alias="core",
-        name="demo_mealie",
+        name="mealie",
         host=DB_HOST,
         port=int(DB_PORT),
         username=DB_USERNAME,
