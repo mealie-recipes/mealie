80ac39c87d8d074eaa8df9d09dea222a16e63296
Hayden <hay-kot@pm.me>
2021-01-03 12:11:36 -0900

    new env variables

--- mealie/db/mongo_setup.py
+++ mealie/db/mongo_setup.py
@@ -1,15 +1,14 @@
 import mongoengine
-from settings import DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME
+from settings import DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME, MEALIE_DB_NAME
 
 
 def global_init():
     mongoengine.register_connection(
         alias="core",
-        name="mealie",
+        name=MEALIE_DB_NAME,
         host=DB_HOST,
         port=int(DB_PORT),
         username=DB_USERNAME,
         password=DB_PASSWORD,
         authentication_source="admin",
     )
-
