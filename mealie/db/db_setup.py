d1fe1f44b8b231549827d28e0768306b1eb445cd
Hayden <64056131+hay-kot@users.noreply.github.com>
2021-01-17 15:06:43 -0900

    fix mongo bug

--- mealie/db/db_setup.py
+++ mealie/db/db_setup.py
@@ -2,9 +2,10 @@ from app_config import SQLITE_FILE, USE_MONGO, USE_SQL
 
 from db.sql.db_session import globa_init as sql_global_init
 
-sql_exists = SQLITE_FILE.is_file()
+sql_exists = True
 
 if USE_SQL:
+    sql_exists = SQLITE_FILE.is_file()
     sql_global_init(SQLITE_FILE)
 
     pass
