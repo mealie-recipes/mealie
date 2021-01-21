a58b0d6a19e360742fa4341767ee63658a33c345
Hayden <hay-kot@pm.me>
2021-01-20 19:18:58 -0900

    fixed type check error

--- vetur.config.js
+++ vetur.config.js
@@ -1,35 +1,15 @@
 // vetur.config.js
 /** @type {import('vls').VeturConfig} */
 module.exports = {
-  // **optional** default: `{}`
-  // override vscode settings part
-  // Notice: It only affects the settings used by Vetur.
   settings: {
     "vetur.useWorkspaceDependencies": true,
     "vetur.experimental.templateInterpolationService": true,
+    "vetur.validation.interpolation": false,
   },
-  // **optional** default: `[{ root: './' }]`
-  // support monorepos
   projects: [
     {
-      // **required**
-      // Where is your project?
-      // It is relative to `vetur.config.js`.
       root: "./frontend",
-      // **optional** default: `'package.json'`
-      // Where is `package.json` in the project?
-      // We use it to determine the version of vue.
-      // It is relative to root property.
       package: "package.json",
-      // **optional**
-      // Where is TypeScript config file in the project?
-      // It is relative to root property.
-      // Where is vetur custom snippets folders?
-      // **optional** default: `[]`
-      // Register globally Vue component glob.
-      // If you set it, you can get completion by that components.
-      // It is relative to root property.
-      // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
       globalComponents: ["./src/components/**/*.vue"],
     },
   ],
