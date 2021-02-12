c5c5e2825f323dccf46c854a592704adc57a8a33
hay-kot <hay-kot@pm.me>
2021-02-11 20:47:35 -0900

    remove static files from FastAPI

--- mealie/app.py
+++ mealie/app.py
@@ -1,14 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 # import utils.startup as startup
-from app_config import (APP_VERSION, PORT, PRODUCTION, WEB_PATH, docs_url,
-                        redoc_url)
-from routes import (backup_routes, debug_routes, meal_routes, migration_routes,
-                    setting_routes, static_routes, theme_routes)
-from routes.recipe import (all_recipe_routes, category_routes,
-                           recipe_crud_routes, tag_routes)
+from app_config import APP_VERSION, PORT, WEB_PATH, docs_url, redoc_url
+from routes import (
+    backup_routes,
+    debug_routes,
+    meal_routes,
+    migration_routes,
+    setting_routes,
+    theme_routes,
+)
+from routes.recipe import (
+    all_recipe_routes,
+    category_routes,
+    recipe_crud_routes,
+    tag_routes,
+)
 from services.settings_services import default_settings_init
 from utils.logger import logger
 
@@ -19,10 +27,6 @@ app = FastAPI(
     docs_url=docs_url,
     redoc_url=redoc_url,
 )
-
-
-def mount_static_files():
-    app.mount("/static", StaticFiles(directory=WEB_PATH, html=True))
 
 
 def start_scheduler():
@@ -52,25 +56,7 @@ def api_routers():
     app.include_router(debug_routes.router)
 
 
-if PRODUCTION:
-    pass
-    # mount_static_files()
-
 api_routers()
-
-# API 404 Catch all CALL AFTER ROUTERS
-@app.get("/api/{full_path:path}", status_code=404, include_in_schema=False)
-def invalid_api():
-    return None
-
-
-app.include_router(static_routes.router)
-
-
-# Generate API Documentation
-# if not PRODUCTION:
-#     generate_api_docs(app)
-
 start_scheduler()
 init_settings()
 
