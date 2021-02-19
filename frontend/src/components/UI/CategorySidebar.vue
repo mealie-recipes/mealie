761e276ace799dc572769bcc72bca8eb3fbfeee4
hay-kot <hay-kot@pm.me>
2021-02-18 21:24:28 -0900

    fixed category sidebar on mobile

--- frontend/src/components/UI/CategorySidebar.vue
+++ frontend/src/components/UI/CategorySidebar.vue
@@ -1,20 +1,42 @@
 <template>
-  <v-navigation-drawer width="175px" clipped app :expand-on-hover="!mobile">
-    <v-list nav dense>
-      <v-list-item v-for="nav in links" :key="nav.title" link :to="nav.to">
-        <v-list-item-icon>
-          <v-icon>{{ nav.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ nav.title | titleCase }}</v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+  <div>
+    <v-btn
+      class="mt-9 ml-n1"
+      fixed
+      left
+      bottom
+      fab
+      small
+      color="primary"
+      @click="showSidebar = !showSidebar"
+    >
+      <v-icon>mdi-tag</v-icon></v-btn
+    >
+
+    <v-navigation-drawer
+      :value="mobile ? showSidebar : true"
+      v-model="showSidebar"
+      width="175px"
+      clipped
+      app
+    >
+      <v-list nav dense>
+        <v-list-item v-for="nav in links" :key="nav.title" link :to="nav.to">
+          <v-list-item-icon>
+            <v-icon>{{ nav.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ nav.title | titleCase }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      showSidebar: false,
       links: [],
       baseLinks: [
         {
