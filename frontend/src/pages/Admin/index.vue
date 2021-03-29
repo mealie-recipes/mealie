f8436584402acf64978064642cd6c011ba2b3a9e
hay-kot <hay-kot@pm.me>
2021-03-28 16:11:41 -0800

    remove old footer

--- frontend/src/pages/Admin/index.vue
+++ frontend/src/pages/Admin/index.vue
@@ -6,25 +6,6 @@
       </v-slide-x-transition>
       <AdminSidebar />
     </v-container>
-    <!-- <v-footer relative padding>
-      <div class="flex text-center" cols="12">
-        {{ $t("settings.current") }}
-        {{ version }} |
-        {{ $t("settings.latest") }}
-        {{ latestVersion }}
-        ·
-        <a href="https://hay-kot.github.io/mealie/" target="_blank">
-          {{ $t("settings.explore-the-docs") }}
-        </a>
-        ·
-        <a
-          href="https://hay-kot.github.io/mealie/contributors/non-coders/"
-          target="_blank"
-        >
-          {{ $t("settings.contribute") }}
-        </a>
-      </div>
-    </v-footer> -->
   </div>
 </template>
 
