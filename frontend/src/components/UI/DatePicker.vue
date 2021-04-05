929b8eb774247174af9bde581ba7f5f2ccb2985c
Florian Dupret <florian.dupret@gmail.com>
2021-04-05 10:37:12 +0200

    Make first day of the week customizable
    New settings section 'Locale settings'
    New setting 'First day of week'
    New date picker reusable UI that uses the new setting
    Meal planner now uses this new date picker

---
+++ frontend/src/components/UI/DatePicker.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-date-picker
+        :first-day-of-week="firstDayOfWeek"
+        v-on="$listeners"
+    ></v-date-picker>
+</template>
+
+<script>
+import { api } from "@/api";
+
+export default {
+  data() {
+    return {
+       firstDayOfWeek: 0,
+    };
+  },
+  mounted() {
+    this.getOptions();
+  },
+
+  methods: {
+    async getOptions() {
+      this.firstDayOfWeek = await api.siteSettings.get().firstDayOfWeek;
+    },
+  },
+}
+</script>
+
+<style>
+</style>
