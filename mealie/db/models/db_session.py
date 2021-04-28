f65f7183334855e2062a01a8857c0541a057ac2a
hay-kot <hay-kot@pm.me>
2021-04-27 16:09:41 -0800

    add thread_safe - required for sqlite

--- mealie/db/models/db_session.py
+++ mealie/db/models/db_session.py
@@ -1,16 +1,16 @@
-from pathlib import Path
-
 import sqlalchemy as sa
-from mealie.db.models.model_base import SqlAlchemyBase
 from sqlalchemy.orm import sessionmaker
 
 
 def sql_global_init(db_url: str):
+    thread_safe = True
+    if "sqlite" in db_url:
+        thread_safe = False
+
     engine = sa.create_engine(
         db_url,
         echo=False,
+        connect_args={"check_same_thread": thread_safe},
     )
 
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-    return SessionLocal
+    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
