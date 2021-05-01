52e5e9da5d4e6bf8bc9888811982c4344ed1af9a
sephrat <34862846+sephrat@users.noreply.github.com>
2021-05-01 23:35:46 +0200

    Remove former profile image upon new upload (#381)
    * Remove former profile image upon new upload
    
    * Fix test

--- tests/integration_tests/test_user_routes.py
+++ tests/integration_tests/test_user_routes.py
@@ -1,6 +1,8 @@
 import json
+from pathlib import Path
 
 from fastapi.testclient import TestClient
+from mealie.core.config import app_dirs
 from mealie.schema.user import UserOut
 from pytest import fixture
 from tests.app_routes import AppRoutes
@@ -84,3 +86,25 @@ def test_delete_user(api_client: TestClient, api_routes: AppRoutes, token):
     response = api_client.delete(api_routes.users_id(2), headers=token)
 
     assert response.status_code == 200
+
+
+def test_update_user_image(
+    api_client: TestClient, api_routes: AppRoutes, test_image_jpg: Path, test_image_png: Path, token
+):
+    response = api_client.post(
+        api_routes.users_id_image(2), files={"profile_image": test_image_jpg.open("rb")}, headers=token
+    )
+
+    assert response.status_code == 200
+
+    response = api_client.post(
+        api_routes.users_id_image(2), files={"profile_image": test_image_png.open("rb")}, headers=token
+    )
+
+    assert response.status_code == 200
+
+    directory = app_dirs.USER_DIR.joinpath("2")
+    assert directory.joinpath("profile_image.png").is_file()
+
+    # Old profile images are removed
+    assert 1 == len([file for file in directory.glob("profile_image.*") if file.is_file()])
