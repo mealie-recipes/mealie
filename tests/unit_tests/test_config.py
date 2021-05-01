ff5439c62f9aa630d7cd70957d4b8b47eb8aaad8
hay-kot <hay-kot@pm.me>
2021-05-01 12:50:16 -0800

    set url

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -49,15 +49,20 @@ def test_non_default_settings(monkeypatch):
     assert app_settings.DOCS_URL is None
 
 
+def test_default_connection_args():
+    app_settings = AppSettings()
+    assert re.match(r"sqlite:////.*mealie/dev/data/mealie_v0.5.0.db", app_settings.DB_URL)
+
+
 def test_default_connection_args(monkeypatch):
-    monkeypatch.setenv("DB_ENGINE", "sqlite")
+    monkeypatch.setenv("DB_ENGINE", "postgres")
     app_settings = AppSettings()
     assert re.match(r"sqlite:////.*mealie/dev/data/mealie_v0.5.0.db", app_settings.DB_URL)
 
 
 def test_pg_connection_args(monkeypatch):
     monkeypatch.setenv("DB_ENGINE", "postgres")
-    monkeypatch.setenv("POSTGRES_SERVER", "mealie")
+    monkeypatch.setenv("POSTGRES_SERVER", "postgres")
     app_settings = AppSettings()
     assert app_settings.DB_URL == "postgresql://mealie:mealie@postgres:5432/mealie"
 
