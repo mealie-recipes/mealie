5921922472ea87e9f1caa951b3cdae10bfb2b7a6
hay-kot <hay-kot@pm.me>
2021-05-06 11:19:05 -0800

    add profile link

--- frontend/src/components/UI/TheSidebar.vue
+++ frontend/src/components/UI/TheSidebar.vue
@@ -2,7 +2,7 @@
   <div>
     <v-navigation-drawer v-model="showSidebar" width="180px" clipped app>
       <template v-slot:prepend>
-        <v-list-item two-line v-if="isLoggedIn">
+        <v-list-item two-line v-if="isLoggedIn" to="/admin/profile">
           <v-list-item-avatar color="accent" class="white--text">
             <img :src="userProfileImage" v-if="!hideImage" @error="hideImage = true" />
             <div v-else>
