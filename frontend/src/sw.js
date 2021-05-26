96368c8a110f85c62ceacfd58a87bec433955baa
Weng Tad <wengtad93@gmail.com>
2021-05-26 17:45:45 +0800

    add PWA

---
+++ frontend/src/sw.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef, no-underscore-dangle, no-restricted-globals */
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener("message", e => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case "skipWaiting":
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
+// workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
+
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const manifestJSON = require("./public/manifest.json");
 module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
@@ -24,6 +25,26 @@ module.exports = {
       alias: {
         "@": path.resolve("src"),
       },
+    },
+  },
+  pwa: {
+    name: manifestJSON.short_name,
+    themeColor: manifestJSON.theme_color,
+    msTileColor: manifestJSON.background_color,
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    iconPaths: {
+      favicon32: "./public/img/icons/favicon-32x32.png",
+      favicon16: "./public/img/icons/favicon-16x16.png",
+      appleTouchIcon: "./public/img/icons/apple-touch-icon-152x152.png",
+      maskIcon: "./public/img/icons/safari-pinned-tab.svg",
+      msTileImage: "./public/img/icons/msapplication-icon-144x144.png",
+    },
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
     },
   },
 };
