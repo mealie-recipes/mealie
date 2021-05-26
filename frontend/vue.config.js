b4fcd8196d7eb0356a3d1716e59a6bce1650a3af
Weng Tad <wengtad93@gmail.com>
2021-05-26 18:28:18 +0800

    cleanup

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -34,13 +34,6 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
 
-    iconPaths: {
-      favicon32: "./public/img/icons/favicon-32x32.png",
-      favicon16: "./public/img/icons/favicon-16x16.png",
-      appleTouchIcon: "./public/img/icons/apple-touch-icon-152x152.png",
-      maskIcon: "./public/img/icons/safari-pinned-tab.svg",
-      msTileImage: "./public/img/icons/msapplication-icon-144x144.png",
-    },
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "./src/sw.js",
