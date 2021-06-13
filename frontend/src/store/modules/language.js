a5b93630c8322e7d18578809640ce9c6ec0dc891
Alex <66680498+CMBoii@users.noreply.github.com>
2021-06-13 20:35:34 +0200

    Add Spanish language (#510)
    * New translations en-US.json (Spanish)
    
    * Add Spanish language
    
    Co-authored-by: Hayden <64056131+hay-kot@users.noreply.github.com>

--- frontend/src/store/modules/language.js
+++ frontend/src/store/modules/language.js
@@ -14,6 +14,10 @@ const state = {
       value: "de-DE",
     },
     {
+      name: "Español (Spanish)",
+      value: "es-ES",
+    },
+    {
       name: "Français (French)",
       value: "fr-FR",
     },
