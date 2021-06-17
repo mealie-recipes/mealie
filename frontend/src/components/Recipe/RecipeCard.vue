94133f3864bc55c2054e291af221cb8626390f33
hay-kot <hay-kot@pm.me>
2021-06-16 19:22:05 -0800

    request correct image

--- frontend/src/components/Recipe/RecipeCard.vue
+++ frontend/src/components/Recipe/RecipeCard.vue
@@ -7,7 +7,7 @@
       @click="$emit('click')"
       min-height="275"
     >
-      <CardImage icon-size="200" :slug="slug" :image-version="image">
+      <CardImage icon-size="200" :slug="slug" small :image-version="image">
         <v-expand-transition v-if="description">
           <div v-if="hover" class="d-flex transition-fast-in-fast-out secondary v-card--reveal  " style="height: 100%;">
             <v-card-text class="v-card--text-show white--text">
