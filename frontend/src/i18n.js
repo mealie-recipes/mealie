93e6c0c41cbe3501f06b144a69ec3035667c2ec4
sephrat <34862846+sephrat@users.noreply.github.com>
2021-06-18 14:52:27 +0200

    Lazy load translations (#561)
    * Lazy load translations
    
    * Lazy load dateTimeFormats

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,33 +1,44 @@
 import Vue from "vue";
 import VueI18n from "vue-i18n";
+import Vuetify from "@/plugins/vuetify";
+import axios from 'axios';
 
 Vue.use(VueI18n);
 
-function parseLocaleFiles(locales) {
-  const messages = {};
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
+const i18n = new VueI18n();
+
+export default i18n;
+
+const loadedLanguages = [];
+
+function setI18nLanguage (lang) {
+  i18n.locale = lang;
+  Vuetify.framework.lang.current = lang;
+  axios.defaults.headers.common['Accept-Language'] = lang
+  document.querySelector('html').setAttribute('lang', lang)
+  return lang
+}
+
+export function loadLanguageAsync(lang) {
+    // If the same language
+  if (i18n.locale === lang) {
+    return Promise.resolve(setI18nLanguage(lang))
+  }
+
+  // If the language was already loaded
+  if (loadedLanguages.includes(lang)) {
+    return Promise.resolve(setI18nLanguage(lang))
+  }
+
+  const messages = import(`./locales/messages/${lang}.json`);
+  const dateTimeFormats = import(`./locales/dateTimeFormats/${lang}.json`);
+  
+  return Promise.all([messages, dateTimeFormats]).then(
+    values  => {
+      i18n.setLocaleMessage(lang, values[0].default)
+      i18n.setDateTimeFormat(lang, values[1].default)
+      loadedLanguages.push(lang)
+      return setI18nLanguage(lang)
     }
-  });
-  return messages;
+  ) 
 }
-
-function loadLocaleMessages() {
-  const locales = require.context("./locales/messages", true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  return parseLocaleFiles(locales);
-}
-
-function loadDateTimeFormats() {
-  const locales = require.context("./locales/dateTimeFormats", true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  return parseLocaleFiles(locales);
-}
-
-export default new VueI18n({
-  locale: "en-US",
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en-US",
-  messages: loadLocaleMessages(),
-  dateTimeFormats: loadDateTimeFormats(),
-});
