79b3985a49d87914fa38545d8ceeeef1cd5316a3
hay-kot <hay-kot@pm.me>
2021-07-31 13:56:59 -0800

    add composition api

---
+++ frontend/src/installCompAPI.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
+Vue.use(VueCompositionAPI);
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,5 @@
+import "./installCompAPI"; // Must Be First
+
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
