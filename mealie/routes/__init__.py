5ace086f23de10ea7356f56a367d2f659bd80f5c
hay-kot <hay-kot@pm.me>
2021-09-02 11:08:51 -0800

    refactor(backend): :recycle: split unrelated routes into clearer router paths
    Add an /app and /admin router base paths to split previously grouped public/admin data into different paths. Part of a longer migration to move 'admin' operations under the admin path.

--- mealie/routes/__init__.py
+++ mealie/routes/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from . import auth, categories, groups, recipe, shopping_lists, tags, unit_and_foods, users
+from . import admin, app, auth, categories, groups, recipe, shopping_lists, tags, unit_and_foods, users
 
 router = APIRouter(prefix="/api")
 
+router.include_router(app.router)
 router.include_router(auth.router)
 router.include_router(users.router)
 router.include_router(groups.router)
@@ -11,5 +12,5 @@ router.include_router(recipe.router)
 router.include_router(unit_and_foods.router)
 router.include_router(categories.router)
 router.include_router(tags.router)
-
 router.include_router(shopping_lists.router)
+router.include_router(admin.router)
---
+++ mealie/routes/admin/admin_log.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter
+
+from mealie.core.root_logger import LOGGER_FILE
+from mealie.core.security import create_file_token
+
+router = APIRouter(prefix="/logs")
+
+
+@router.get("/{num}")
+async def get_log(num: int):
+    """ Doc Str """
+    with open(LOGGER_FILE, "rb") as f:
+        log_text = tail(f, num)
+    return log_text
+
+
+@router.get("")
+async def get_log_file():
+    """ Returns a token to download a file """
+    return {"fileToken": create_file_token(LOGGER_FILE)}
+
+
+def tail(f, lines=20):
+    total_lines_wanted = lines
+
+    BLOCK_SIZE = 1024
+    f.seek(0, 2)
+    block_end_byte = f.tell()
+    lines_to_go = total_lines_wanted
+    block_number = -1
+    blocks = []
+    while lines_to_go > 0 and block_end_byte > 0:
+        if block_end_byte - BLOCK_SIZE > 0:
+            f.seek(block_number * BLOCK_SIZE, 2)
+            blocks.append(f.read(BLOCK_SIZE))
+        else:
+            f.seek(0, 0)
+            blocks.append(f.read(block_end_byte))
+        lines_found = blocks[-1].count(b"\n")
+        lines_to_go -= lines_found
+        block_end_byte -= BLOCK_SIZE
+        block_number -= 1
+    all_read_text = b"".join(reversed(blocks))
+    return b"/n".join(all_read_text.splitlines()[-total_lines_wanted:])
