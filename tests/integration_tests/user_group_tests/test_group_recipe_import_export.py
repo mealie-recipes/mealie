36f8916aacf9de1b2c780680060a1c6f99882f18
Hayden <hay-kot@pm.me>
2021-10-02 11:27:45 -0800

    comment out stub tests

--- tests/integration_tests/user_group_tests/test_group_recipe_import_export.py
+++ tests/integration_tests/user_group_tests/test_group_recipe_import_export.py
@@ -1,15 +1,15 @@
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 
-from tests.utils.fixture_schemas import TestUser
-
-
-class Routes:
-    base = "/api/groups/manage/data"  # Not sure if this is a good url?!?!?
+# from tests.utils.fixture_schemas import TestUser
 
 
-def test_recipe_export(api_client: TestClient, unique_user: TestUser) -> None:
-    assert False
+# class Routes:
+#     base = "/api/groups/manage/data"  # Not sure if this is a good url?!?!?
 
 
-def test_recipe_import(api_client: TestClient, unique_user: TestUser) -> None:
-    assert False
+# def test_recipe_export(api_client: TestClient, unique_user: TestUser) -> None:
+#     assert False
+
+
+# def test_recipe_import(api_client: TestClient, unique_user: TestUser) -> None:
+#     assert False
