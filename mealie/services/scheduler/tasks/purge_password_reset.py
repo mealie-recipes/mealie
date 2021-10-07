317b3ff12746439ee50691ac37abe8b9cd44af56
Hayden <hay-kot@pm.me>
2021-10-07 09:17:28 -0800

    purge password schedule

--- mealie/services/scheduler/tasks/purge_password_reset.py
+++ mealie/services/scheduler/tasks/purge_password_reset.py
@@ -1,11 +1,20 @@
+import datetime
+
 from mealie.core import root_logger
+from mealie.db.db_setup import create_session
+from mealie.db.models.users.password_reset import PasswordResetModel
 
 logger = root_logger.get_logger()
 
-MAX_DAYS_OLD = 4
+MAX_DAYS_OLD = 2
 
 
 def purge_password_reset_tokens():
     """Purges all events after x days"""
     logger.info("purging password reset tokens")
+    limit = datetime.datetime.now() - datetime.timedelta(days=MAX_DAYS_OLD)
+    session = create_session()
+    session.query(PasswordResetModel).filter(PasswordResetModel.created_at <= limit).delete()
+    session.commit()
+    session.close()
     logger.info("password reset tokens purges")
