d54ceff7f830fc32801ba2e2910fcf8ff8fc7f58
Christian Hollinger <christian.hollinger@otter-in-a-suit.com>
2021-10-16 12:29:04 -0400

    Batched database imports

--- mealie/services/migrations/csv.py
+++ mealie/services/migrations/csv.py
@@ -21,22 +21,34 @@ class CsvMigration(MigrationBase):
 def migrate(session: Session, csv_path: Path) -> list[MigrationImport]:
     csv_migration = CsvMigration(migration_file=csv_path, session=session)
 
+    batch_size = 10
     all_recipes = []
+    c = 0
     with csv_migration.temp_dir as dir:
         csv_dir = Path(dir)
         for f in csv_dir.glob("*.*sv"):
             for row in csv_migration.csv_reader(csv_file=f):
-                print(row)
                 # Scrape the recipe
-                recipe = create_from_url(row["url"])
+                try:
+                    recipe = create_from_url(row["url"])
+                except Exception:
+                    continue
                 if not recipe or recipe.name is None:
                     continue
+                # Keep count
+                c += 1
                 # Set the tags and categories
                 recipe.tags = row["tags"]
                 recipe.recipe_category = row["categories"]
                 # Save it
                 all_recipes.append(recipe)
-            # Import
-            csv_migration.import_recipes_to_database(all_recipes)
-
+                # Flush the Buffer
+                if c >= batch_size:
+                    csv_migration.import_recipes_to_database(all_recipes)
+                    # Clear the buffer
+                    all_recipes = []
+                    c = 0
+    # Import the rest
+    csv_migration.import_recipes_to_database(all_recipes)
+    # If this were a generator, we could return partial results
     return csv_migration.migration_report
