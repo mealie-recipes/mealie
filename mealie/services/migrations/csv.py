b6ad8672750b2c81f6220bcf7ec7d8788b7c88d9
Christian Hollinger <christian.hollinger@otter-in-a-suit.com>
2021-10-19 20:51:31 -0400

    Limiting the no of recipes and returning a pseudo-error

--- mealie/services/migrations/csv.py
+++ mealie/services/migrations/csv.py
@@ -18,7 +18,7 @@ class CsvMigration(MigrationBase):
     ]
 
 
-def migrate(session: Session, csv_path: Path) -> list[MigrationImport]:
+def migrate(session: Session, csv_path: Path, max_rows=10) -> list[MigrationImport]:
     csv_migration = CsvMigration(migration_file=csv_path, session=session)
 
     batch_size = 10
@@ -27,7 +27,11 @@ def migrate(session: Session, csv_path: Path) -> list[MigrationImport]:
     with csv_migration.temp_dir as dir:
         csv_dir = Path(dir)
         for f in csv_dir.glob("*.*sv"):
-            for row in csv_migration.csv_reader(csv_file=f):
+            # TODO: the generator gets wrapped in a list to provide feedback on the import size,
+            # as we currently need to limit the max. imports
+            cur_csvs = list(csv_migration.csv_reader(csv_file=f))
+            # Temporarily check the length
+            for row in cur_csvs[:max_rows]:
                 # Scrape the recipe
                 try:
                     recipe = create_from_url(row["url"])
@@ -49,6 +53,11 @@ def migrate(session: Session, csv_path: Path) -> list[MigrationImport]:
                     all_recipes = []
                     c = 0
     # Import the rest
-    csv_migration.import_recipes_to_database(all_recipes)
+    if all_recipes:
+        csv_migration.import_recipes_to_database(all_recipes)
+    if len(cur_csvs) > max_rows:
+        # TODO: this needs to be removed w/ the async queue
+        err = f"Can only import {max_rows} recipes at once. Please reduce the import size per CSV!"
+        csv_migration.migration_report.append(MigrationImport(slug="", name=err, status=False, exception=err))
     # If this were a generator, we could return partial results
     return csv_migration.migration_report
