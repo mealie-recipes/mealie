de5bea6f73db9edbfebe3791e239f4951d17e653
Hayden <64056131+hay-kot@users.noreply.github.com>
2021-10-30 12:13:54 -0800

    Bug/minor fixes (#762)
    * Set web concurrency to 1
    
    * bump versions
    
    * update release notes
    
    Co-authored-by: Hayden <hay-kot@pm.me>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
     <v-main>
       <v-banner v-if="demo" sticky>
         <div class="text-center">
-          <b> This is a Demo of the v0.5.0 (BETA) </b> | Username: changeme@email.com | Password: demo
+          <b> This is a Demo of the v0.5.4 (BETA) </b> | Username: changeme@email.com | Password: demo
         </div>
       </v-banner>
       <GlobalSnackbar />
