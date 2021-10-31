561c80ddea7ba81d72a32c83c72713231a997d1d
Hayden <hay-kot@pm.me>
2021-10-31 13:41:16 -0800

    lazy load json editor

---
+++ frontend/components/global/RecipeJsonEditor.vue
@@ -0,0 +1,22 @@
+<template>
+  <VJsoneditor v-model="value" height="1500px" :options="options" :attrs="$attrs"></VJsoneditor>
+</template>
+
+<script>
+import VJsoneditor from "v-jsoneditor";
+
+export default {
+  components: { VJsoneditor },
+  props: {
+    value: {
+      type: Object,
+      default: () => ({}),
+    },
+    options: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
