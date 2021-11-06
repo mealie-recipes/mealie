377b955b13e0bb7c4f0220cd03f3dc926285f0e9
Hayden K <hay-kot@pm.me>
2021-11-06 11:04:24 -0800

    feat(frontend): :sparkles: add scale, and back to recipe button + hide ingredients if none

--- frontend/pages/recipe/_slug/cook.vue
+++ frontend/pages/recipe/_slug/cook.vue
@@ -8,12 +8,30 @@
     <v-card-title>
       <h1 class="headline">{{ recipe.name }}</h1>
     </v-card-title>
+
     <v-stepper v-model="activeStep" flat>
       <v-toolbar class="ma-1 elevation-2 rounded">
         <v-toolbar-title class="headline">
           Step {{ activeStep }} of {{ recipe.recipeInstructions.length }}</v-toolbar-title
         >
       </v-toolbar>
+      <div class="d-flex mt-3 px-2">
+        <BaseButton color="primary" @click="$router.go(-1)">
+          <template #icon> {{ $globals.icons.arrowLeftBold }}</template>
+          To Recipe
+        </BaseButton>
+        <v-btn rounded icon color="primary" class="ml-auto" small @click="scale > 1 ? scale-- : null">
+          <v-icon>
+            {{ $globals.icons.minus }}
+          </v-icon>
+        </v-btn>
+        <v-btn rounded color="primary" small> Scale: {{ scale }} </v-btn>
+        <v-btn rounded icon color="primary" small @click="scale++">
+          <v-icon>
+            {{ $globals.icons.createAlt }}
+          </v-icon>
+        </v-btn>
+      </div>
       <v-stepper-items>
         <template v-for="(step, index) in recipe.recipeInstructions">
           <v-stepper-content :key="index + 1 + '-content'" :step="index + 1" class="pa-0 mt-2 elevation-0">
@@ -21,11 +39,17 @@
               <v-card-text>
                 <h2 class="mb-4">{{ $t("recipe.instructions") }}</h2>
                 <VueMarkdown :source="step.text"> </VueMarkdown>
-                <v-divider></v-divider>
-                <h2 class="mb-4 mt-4">{{ $t("recipe.ingredients") }}</h2>
-                <div v-for="ing in step.ingredientReferences" :key="ing.referenceId">
-                  {{ getIngredientByRefId(ing.referenceId).note }}
-                </div>
+                <template v-if="step.ingredientReferences.length > 0">
+                  <v-divider></v-divider>
+                  <div>
+                    <h2 class="mb-4 mt-4">{{ $t("recipe.ingredients") }}</h2>
+                    <div
+                      v-for="ing in step.ingredientReferences"
+                      :key="ing.referenceId"
+                      v-html="getIngredientByRefId(ing.referenceId)"
+                    ></div>
+                  </div>
+                </template>
               </v-card-text>
             </v-card>
             <v-card-actions class="justify-center">
@@ -33,6 +57,7 @@
                 <template #icon> {{ $globals.icons.arrowLeftBold }}</template>
                 Back
               </BaseButton>
+
               <BaseButton
                 icon-right
                 :disabled="index + 1 == recipe.recipeInstructions.length"
@@ -55,23 +80,35 @@ import { defineComponent, useRoute, ref } from "@nuxtjs/composition-api";
 // @ts-ignore
 import VueMarkdown from "@adapttive/vue-markdown";
 import { useStaticRoutes } from "~/composables/api";
-import { useRecipe } from "~/composables/recipes";
+import { parseIngredientText, useRecipe } from "~/composables/recipes";
+
 export default defineComponent({
   components: { VueMarkdown },
   setup() {
     const route = useRoute();
     const slug = route.value.params.slug;
     const activeStep = ref(1);
+    const scale = ref(1);
 
     const { recipe } = useRecipe(slug);
 
     const { recipeImage } = useStaticRoutes();
 
     function getIngredientByRefId(refId: String) {
-      return recipe.value?.recipeIngredient.find((ing) => ing.referenceId === refId) || "";
+      if (!recipe.value) {
+        return;
+      }
+
+      const ing = recipe?.value.recipeIngredient.find((ing) => ing.referenceId === refId) || "";
+      if (ing === "") {
+        return "";
+      }
+
+      return parseIngredientText(ing, recipe?.value?.settings?.disableAmount || false, scale.value);
     }
 
     return {
+      scale,
       getIngredientByRefId,
       activeStep,
       slug,
