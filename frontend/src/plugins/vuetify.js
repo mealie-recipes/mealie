d5dc47c60b382c4f31fd5ea90cd4098e445dace7
Florian Dupret <34862846+sephrat@users.noreply.github.com>
2021-11-06 09:19:35 +0100

    Add support for new languages
    Catalan, Danish, Norwegian and Russian

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,6 +3,7 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
+import ca from "vuetify/es5/locale/ca";
 import de from "vuetify/es5/locale/de";
 import en from "vuetify/es5/locale/en";
 import es from "vuetify/es5/locale/es";
@@ -10,7 +11,9 @@ import fr from "vuetify/es5/locale/fr";
 import hu from "vuetify/es5/locale/hu";
 import it from "vuetify/es5/locale/it";
 import nl from "vuetify/es5/locale/nl";
+import no from "vuetify/es5/locale/no";
 import pl from "vuetify/es5/locale/pl";
+import ru from "vuetify/es5/locale/ru";
 import sk from "vuetify/es5/locale/sk";
 import sv from "vuetify/es5/locale/sv";
 import zhHans from "vuetify/es5/locale/zh-Hans";
@@ -44,6 +47,8 @@ const vuetify = new Vuetify({
   },
   lang: {
     locales: {
+      "ca-ES": ca,
+      "da-DK": en, // language not supported by Vuetify
       "de-DE": de,
       "en-US": en,
       "en-GB": en,
@@ -52,7 +57,9 @@ const vuetify = new Vuetify({
       "hu-HU": hu,
       "it-IT": it,
       "nl-NL": nl,
+      "no-NO": no,
       "pl-PL": pl,
+      "ru-RU": ru,
       "sk-SK": sk,
       "sv-SE": sv,
       "zh-CN": zhHans,
