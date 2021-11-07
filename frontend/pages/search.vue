d76299953079fd9700d98d93db8768c92e3c6152
hay-kot <hay-kot@pm.me>
2021-11-06 18:32:24 -0800

    add additional search fields

--- frontend/pages/search.vue
+++ frontend/pages/search.vue
@@ -1,98 +1,109 @@
 <template>
-  <v-container>
-    <v-row dense>
-      <v-col>
-        <v-text-field
-          v-model="searchString"
-          outlined
-          color="primary accent-3"
-          :placeholder="$t('search.search-placeholder')"
-          :append-icon="$globals.icons.search"
-        >
-        </v-text-field>
-      </v-col>
-      <v-col cols="12" md="2" sm="12">
-        <v-text-field v-model="maxResults" class="mt-0 pt-0" :label="$t('search.max-results')" type="number" outlined />
-      </v-col>
-    </v-row>
+  <v-container fluid>
+    <v-container fluid class="pa-0">
+      <v-row dense>
+        <v-col>
+          <v-text-field
+            v-model="searchString"
+            outlined
+            autofocus
+            color="primary accent-3"
+            :placeholder="$t('search.search-placeholder')"
+            :prepend-inner-icon="$globals.icons.search"
+            clearable
+          >
+          </v-text-field>
+        </v-col>
+        <v-col cols="12" md="2" sm="12">
+          <v-text-field
+            v-model="maxResults"
+            class="mt-0 pt-0"
+            :label="$t('search.max-results')"
+            type="number"
+            outlined
+          />
+        </v-col>
+      </v-row>
 
-    <ToggleState>
-      <template #activator="{ state, toggle }">
-        <v-switch :value="state" color="info" class="ma-0 pa-0" label="Advanced" @input="toggle" @click="toggle">
-          Advanced
-        </v-switch>
-      </template>
-      <template #default="{ state }">
-        <v-expand-transition>
-          <v-row v-show="state" dense class="my-0 dense flex-row align-center justify-space-around">
-            <v-col>
-              <h3 class="pl-2 text-center headline">
-                {{ $t("category.category-filter") }}
-              </h3>
-              <RecipeSearchFilterSelector class="mb-1" @update="updateCatParams" />
-              <RecipeCategoryTagSelector
-                v-model="includeCategories"
-                :solo="true"
-                :dense="false"
-                :return-object="false"
-              />
-            </v-col>
-            <v-col>
-              <h3 class="pl-2 text-center headline">
-                {{ $t("search.tag-filter") }}
-              </h3>
-              <RecipeSearchFilterSelector class="mb-1" @update="updateTagParams" />
-              <RecipeCategoryTagSelector
-                v-model="includeTags"
-                :solo="true"
-                :dense="false"
-                :return-object="false"
-                :tag-selector="true"
-              />
-            </v-col>
-            <v-col>
-              <h3 class="pl-2 text-center headline">Food Filter</h3>
-              <RecipeSearchFilterSelector class="mb-1" @update="updateFoodParams" />
-              <v-autocomplete
-                v-model="includeFoods"
-                hide-details
-                chips
-                deletable-chips
-                solo
-                multiple
-                :items="foods || []"
-                item-text="name"
-                class="mx-1 py-0 mb-8"
-                :prepend-inner-icon="$globals.icons.foods"
-                label="Choose Food"
-              >
-                <template #selection="data">
-                  <v-chip
-                    :key="data.index"
-                    class="ma-1"
-                    :input-value="data.selected"
-                    close
-                    label
-                    color="accent"
-                    dark
-                    @click:close="includeFoods.splice(data.index, 1)"
-                  >
-                    {{ data.item.name || data.item }}
-                  </v-chip>
-                </template>
-              </v-autocomplete>
-            </v-col>
-          </v-row>
-        </v-expand-transition>
-      </template>
-    </ToggleState>
-
-    <RecipeCardSection
-      class="mt-n5"
-      :title-icon="$globals.icons.magnify"
-      :recipes="showRecipes.slice(0, maxResults)"
-      @sort="assignFuzzy"
-    />
+      <ToggleState>
+        <template #activator="{ state, toggle }">
+          <v-switch :value="state" color="info" class="ma-0 pa-0" label="Advanced" @input="toggle" @click="toggle">
+            Advanced
+          </v-switch>
+        </template>
+        <template #default="{ state }">
+          <v-expand-transition>
+            <v-row v-show="state" dense class="my-0 dense flex-row align-center justify-space-around">
+              <v-col>
+                <h3 class="pl-2 text-center headline">
+                  {{ $t("category.category-filter") }}
+                </h3>
+                <RecipeSearchFilterSelector class="mb-1" @update="updateCatParams" />
+                <RecipeCategoryTagSelector
+                  v-model="includeCategories"
+                  :solo="true"
+                  :dense="false"
+                  :return-object="false"
+                />
+              </v-col>
+              <v-col>
+                <h3 class="pl-2 text-center headline">
+                  {{ $t("search.tag-filter") }}
+                </h3>
+                <RecipeSearchFilterSelector class="mb-1" @update="updateTagParams" />
+                <RecipeCategoryTagSelector
+                  v-model="includeTags"
+                  :solo="true"
+                  :dense="false"
+                  :return-object="false"
+                  :tag-selector="true"
+                />
+              </v-col>
+              <v-col>
+                <h3 class="pl-2 text-center headline">Food Filter</h3>
+                <RecipeSearchFilterSelector class="mb-1" @update="updateFoodParams" />
+                <v-autocomplete
+                  v-model="includeFoods"
+                  hide-details
+                  chips
+                  deletable-chips
+                  solo
+                  multiple
+                  :items="foods || []"
+                  item-text="name"
+                  class="mx-1 py-0 mb-8"
+                  :prepend-inner-icon="$globals.icons.foods"
+                  label="Choose Food"
+                >
+                  <template #selection="data">
+                    <v-chip
+                      :key="data.index"
+                      class="ma-1"
+                      :input-value="data.selected"
+                      close
+                      label
+                      color="accent"
+                      dark
+                      @click:close="includeFoods.splice(data.index, 1)"
+                    >
+                      {{ data.item.name || data.item }}
+                    </v-chip>
+                  </template>
+                </v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-expand-transition>
+        </template>
+      </ToggleState>
+    </v-container>
+    <v-container>
+      <RecipeCardSection
+        class="mt-n5"
+        :title-icon="$globals.icons.magnify"
+        :recipes="showRecipes.slice(0, maxResults)"
+        @sort="assignFuzzy"
+      />
+    </v-container>
   </v-container>
 </template>
 
@@ -160,7 +171,7 @@ export default defineComponent({
         findAllMatches: true,
         maxPatternLength: 32,
         minMatchCharLength: 2,
-        keys: ["name", "description"],
+        keys: ["name", "description", "recipeIngredient.note", "recipeIngredient.food.name"],
       },
     });
 
