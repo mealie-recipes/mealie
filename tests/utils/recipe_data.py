be41966733ef921cc2224e72c20fdfe1292d0f49
Hayden <64056131+hay-kot@users.noreply.github.com>
2021-12-08 13:50:33 -0900

    remove hard-coded paths

--- tests/utils/recipe_data.py
+++ tests/utils/recipe_data.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from pathlib import Path
 
-from tests.test_config import TEST_HTML_DIR
+from tests import data as test_data
 
 
 @dataclass
@@ -11,10 +10,7 @@ class RecipeSiteTestCase:
     expected_slug: str
     num_ingredients: int
     num_steps: int
-
-    @property
-    def html_file(self) -> Path:
-        return TEST_HTML_DIR / self.html
+    html_file: str
 
 
 def get_recipe_test_cases():
@@ -22,6 +18,7 @@ def get_recipe_test_cases():
         RecipeSiteTestCase(
             url="https://www.seriouseats.com/taiwanese-three-cup-chicken-san-bei-gi-recipe",
             html="taiwanese-three-cup-chicken-san-bei-gi-recipe.html",
+            html_file=test_data.html_taiwanese_three_cup_chicken_san_bei_gi_recipe,
             expected_slug="taiwanese-three-cup-chicken-san-bei-ji-recipe",
             num_ingredients=10,
             num_steps=3,
@@ -29,6 +26,7 @@ def get_recipe_test_cases():
         RecipeSiteTestCase(
             url="https://www.rezeptwelt.de/backen-herzhaft-rezepte/schinken-kaese-waffeln-ohne-viel-schnickschnack/4j0bkiig-94d4d-106529-cfcd2-is97x2ml",
             html="schinken-kase-waffeln-ohne-viel-schnickschnack.html",
+            html_file=test_data.html_schinken_kase_waffeln_ohne_viel_schnickschnack,
             expected_slug="schinken-kase-waffeln-ohne-viel-schnickschnack",
             num_ingredients=7,
             num_steps=1,  # Malformed JSON Data, can't parse steps just get one string
@@ -36,6 +34,7 @@ def get_recipe_test_cases():
         RecipeSiteTestCase(
             url="https://cookpad.com/us/recipes/5544853-sous-vide-smoked-beef-ribs",
             html="sous-vide-smoked-beef-ribs.html",
+            html_file=test_data.html_sous_vide_smoked_beef_ribs,
             expected_slug="sous-vide-smoked-beef-ribs",
             num_ingredients=7,
             num_steps=12,
@@ -43,6 +42,7 @@ def get_recipe_test_cases():
         RecipeSiteTestCase(
             url="https://www.greatbritishchefs.com/recipes/jam-roly-poly-recipe",
             html="jam-roly-poly-with-custard.html",
+            html_file=test_data.html_jam_roly_poly_with_custard,
             expected_slug="jam-roly-poly-with-custard",
             num_ingredients=13,
             num_steps=9,
@@ -50,6 +50,7 @@ def get_recipe_test_cases():
         RecipeSiteTestCase(
             url="https://recipes.anovaculinary.com/recipe/sous-vide-shrimp",
             html="sous-vide-shrimp.html",
+            html_file=test_data.html_sous_vide_shrimp,
             expected_slug="sous-vide-shrimp",
             num_ingredients=5,
             num_steps=0,
@@ -57,6 +58,7 @@ def get_recipe_test_cases():
         RecipeSiteTestCase(
             url="https://www.bonappetit.com/recipe/detroit-style-pepperoni-pizza",
             html="detroit-style-pepperoni-pizza.html",
+            html_file=test_data.html_detroit_style_pepperoni_pizza,
             expected_slug="detroit-style-pepperoni-pizza",
             num_ingredients=8,
             num_steps=5,
