964aa09f52064917a3dcaea8698e7751cbc8da7d
Hayden <64056131+hay-kot@users.noreply.github.com>
2021-12-11 21:11:34 -0900

    add recipe avatar

---
+++ frontend/components/Domain/User/UserAvatar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-list-item-avatar v-if="list && userId">
+    <v-img :src="`/api/media/users/${userId}/profile.webp`" :alt="userId" @load="error = false" @error="error = true">
+    </v-img>
+  </v-list-item-avatar>
+  <v-avatar v-else-if="userId" :size="size">
+    <v-img :src="`/api/media/users/${userId}/profile.webp`" :alt="userId" @load="error = false" @error="error = true">
+    </v-img>
+  </v-avatar>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, reactive } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {
+    userId: {
+      type: String,
+      required: true,
+    },
+    list: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: Number,
+      default: 42,
+    },
+  },
+  setup() {
+    const state = reactive({
+      error: false,
+    });
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
