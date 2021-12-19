88b6d00d26e1c4b58f45889a8d5b7d260f485a00
Hayden <64056131+hay-kot@users.noreply.github.com>
2021-12-18 17:29:37 -0900

    force set content type

--- mealie/routes/media/media_user.py
+++ mealie/routes/media/media_user.py
@@ -12,13 +12,13 @@ in development mode. If you make changes, be sure to test the production contain
 router = APIRouter(prefix="/users")
 
 
-@router.get("/{user_id}/{file_name}")
+@router.get("/{user_id}/{file_name}", response_class=FileResponse)
 async def get_user_image(user_id: UUID4, file_name: str):
     """Takes in a recipe slug, returns the static image. This route is proxied in the docker image
     and should not hit the API in production"""
     recipe_image = PrivateUser.get_directory(user_id) / file_name
 
     if recipe_image.exists():
-        return FileResponse(recipe_image)
+        return FileResponse(recipe_image, media_type="image/webp")
     else:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
