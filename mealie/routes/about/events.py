5c353bee3f51cee450a8b02487461386e98c5b13
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-10 10:15:19 -0900

    convert events api to controller

--- mealie/routes/about/events.py
+++ mealie/routes/about/events.py
@@ -1,35 +1,31 @@
 from fastapi import Depends
-from sqlalchemy.orm.session import Session
 
-from mealie.core.root_logger import get_logger
-from mealie.db.db_setup import generate_session
-from mealie.repos.all_repositories import get_repositories
+from mealie.repos.all_repositories import generate_repositories
+from mealie.repos.repository_factory import AllRepositories
+from mealie.routes._base.controller import controller
 from mealie.routes.routers import AdminAPIRouter
 from mealie.schema.events import EventsOut
 
 router = AdminAPIRouter(prefix="/events")
 
-logger = get_logger()
 
+@controller(router)
+class EventsController:
+    repos: AllRepositories = Depends(generate_repositories)
 
-@router.get("", response_model=EventsOut)
-async def get_events(session: Session = Depends(generate_session)):
-    """Get event from the Database"""
-    db = get_repositories(session)
+    @router.get("", response_model=EventsOut)
+    async def get_events(self):
+        """Get event from the Database"""
 
-    return EventsOut(total=db.events.count_all(), events=db.events.get_all(order_by="time_stamp"))
+        return EventsOut(total=self.repos.events.count_all(), events=self.repos.events.get_all(order_by="time_stamp"))
 
+    @router.delete("")
+    async def delete_events(self):
+        """Get event from the Database"""
+        self.repos.events.delete_all()
+        return {"message": "All events deleted"}
 
-@router.delete("")
-async def delete_events(session: Session = Depends(generate_session)):
-    """Get event from the Database"""
-    db = get_repositories(session)
-    db.events.delete_all()
-    return {"message": "All events deleted"}
-
-
-@router.delete("/{id}")
-async def delete_event(id: int, session: Session = Depends(generate_session)):
-    """Delete event from the Database"""
-    db = get_repositories(session)
-    return db.events.delete(id)
+    @router.delete("/{id}")
+    async def delete_event(self, id: int):
+        """Delete event from the Database"""
+        return self.repos.events.delete(id)
