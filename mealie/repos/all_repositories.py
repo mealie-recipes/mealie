fc3d2bf363456c055562ce229298ac846a7e87b0
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-10 10:15:12 -0900

    add dependency injection for get_repositories

--- mealie/repos/all_repositories.py
+++ mealie/repos/all_repositories.py
@@ -1,6 +1,13 @@
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
+from mealie.db.db_setup import generate_session
+
 from .repository_factory import AllRepositories
+
+
+def generate_repositories(session: Session = Depends(generate_session)):
+    return AllRepositories(session)
 
 
 def get_repositories(session: Session):
