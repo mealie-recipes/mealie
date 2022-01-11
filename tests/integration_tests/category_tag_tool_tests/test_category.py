2fa81418612d8ce44f3278168e706b8b02520a31
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-10 16:10:22 -0900

    add category/tag/tool tests

--- tests/integration_tests/category_tag_tool_tests/test_category.py
+++ tests/integration_tests/category_tag_tool_tests/test_category.py
@@ -3,12 +3,13 @@ from dataclasses import dataclass
 import pytest
 from fastapi.testclient import TestClient
 
+from mealie.schema.static import recipe_keys
 from tests.utils.factories import random_string
 from tests.utils.fixture_schemas import TestUser
 
 
 class Routes:
-    base = "/api/tools"
+    base = "/api/categories"
     recipes = "/api/recipes"
 
     def item(item_id: int) -> str:
@@ -19,16 +20,15 @@ class Routes:
 
 
 @dataclass
-class TestRecipeTool:
+class TestRecipeCategory:
     id: int
     name: str
     slug: str
-    on_hand: bool
     recipes: list
 
 
 @pytest.fixture(scope="function")
-def tool(api_client: TestClient, unique_user: TestUser) -> TestRecipeTool:
+def category(api_client: TestClient, unique_user: TestUser) -> TestRecipeCategory:
     data = {"name": random_string(10)}
 
     response = api_client.post(Routes.base, json=data, headers=unique_user.token)
@@ -37,84 +37,71 @@ def tool(api_client: TestClient, unique_user: TestUser) -> TestRecipeTool:
 
     as_json = response.json()
 
-    yield TestRecipeTool(
+    yield TestRecipeCategory(
         id=as_json["id"],
         name=data["name"],
         slug=as_json["slug"],
-        on_hand=as_json["onHand"],
         recipes=[],
     )
 
     try:
-        response = api_client.delete(Routes.item(response.json()["id"]), headers=unique_user.token)
+        response = api_client.delete(Routes.item(response.json()["slug"]), headers=unique_user.token)
     except Exception:
         pass
 
 
-def test_create_tool(api_client: TestClient, unique_user: TestUser):
+def test_create_category(api_client: TestClient, unique_user: TestUser):
     data = {"name": random_string(10)}
-
     response = api_client.post(Routes.base, json=data, headers=unique_user.token)
     assert response.status_code == 201
 
 
-def test_read_tool(api_client: TestClient, tool: TestRecipeTool, unique_user: TestUser):
-    response = api_client.get(Routes.item(tool.id), headers=unique_user.token)
+def test_read_category(api_client: TestClient, category: TestRecipeCategory, unique_user: TestUser):
+    response = api_client.get(Routes.item(category.slug), headers=unique_user.token)
     assert response.status_code == 200
 
     as_json = response.json()
+    assert as_json["id"] == category.id
+    assert as_json["name"] == category.name
 
-    assert as_json["id"] == tool.id
-    assert as_json["name"] == tool.name
 
-
-def test_update_tool(api_client: TestClient, tool: TestRecipeTool, unique_user: TestUser):
+def test_update_category(api_client: TestClient, category: TestRecipeCategory, unique_user: TestUser):
     update_data = {
-        "id": tool.id,
+        "id": category.id,
         "name": random_string(10),
-        "slug": tool.slug,
-        "on_hand": True,
+        "slug": category.slug,
     }
 
-    response = api_client.put(Routes.item(tool.id), json=update_data, headers=unique_user.token)
+    response = api_client.put(Routes.item(category.slug), json=update_data, headers=unique_user.token)
     assert response.status_code == 200
 
     as_json = response.json()
-
-    assert as_json["id"] == tool.id
+    assert as_json["id"] == category.id
     assert as_json["name"] == update_data["name"]
 
 
-def test_delete_tool(api_client: TestClient, tool: TestRecipeTool, unique_user: TestUser):
-    response = api_client.delete(Routes.item(tool.id), headers=unique_user.token)
+def test_delete_category(api_client: TestClient, category: TestRecipeCategory, unique_user: TestUser):
+    response = api_client.delete(Routes.item(category.slug), headers=unique_user.token)
     assert response.status_code == 200
 
 
-def test_recipe_tool_association(api_client: TestClient, tool: TestRecipeTool, unique_user: TestUser):
+def test_recipe_category_association(api_client: TestClient, category: TestRecipeCategory, unique_user: TestUser):
     # Setup Recipe
     recipe_data = {"name": random_string(10)}
-
     response = api_client.post(Routes.recipes, json=recipe_data, headers=unique_user.token)
-
     slug = response.json()
-
     assert response.status_code == 201
 
     # Get Recipe Data
     response = api_client.get(Routes.recipe(slug), headers=unique_user.token)
-
     as_json = response.json()
-
-    as_json["tools"] = [{"id": tool.id, "name": tool.name, "slug": tool.slug}]
+    as_json[recipe_keys.recipe_category] = [{"id": category.id, "name": category.name, "slug": category.slug}]
 
     # Update Recipe
     response = api_client.put(Routes.recipe(slug), json=as_json, headers=unique_user.token)
-
     assert response.status_code == 200
 
     # Get Recipe Data
     response = api_client.get(Routes.recipe(slug), headers=unique_user.token)
-
     as_json = response.json()
-
-    assert as_json["tools"][0]["id"] == tool.id
+    assert as_json[recipe_keys.recipe_category][0]["slug"] == category.slug
