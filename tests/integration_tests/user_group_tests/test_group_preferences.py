fad643f8fba5f11b7719db7d2f7cb418cf379d96
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-11 16:56:01 -0900

    add additional group test coverage

--- tests/integration_tests/user_group_tests/test_group_preferences.py
+++ tests/integration_tests/user_group_tests/test_group_preferences.py
@@ -12,7 +12,6 @@ class Routes:
 
 def test_get_preferences(api_client: TestClient, unique_user: TestUser) -> None:
     response = api_client.get(Routes.preferences, headers=unique_user.token)
-
     assert response.status_code == 200
 
     preferences = response.json()
