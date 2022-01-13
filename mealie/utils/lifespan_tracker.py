8ee690b0b60438f3fceecc177f57a86eb39c4fb3
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-12 20:31:08 -0900

    ignore prints in flake8

--- mealie/utils/lifespan_tracker.py
+++ mealie/utils/lifespan_tracker.py
@@ -7,16 +7,16 @@ import time
 def log_lifetime(cls):
     class LifeTimeClass(cls):
         def __init__(self, *args, **kwargs):
-            print(f"Creating an instance of {cls.__name__}")  # noqa
+            print(f"Creating an instance of {cls.__name__}")  # noqa: T001
             self.__lifespan_timer_start = time.perf_counter()
 
             super().__init__(*args, **kwargs)
 
         def __del__(self):
             toc = time.perf_counter()
-            print(f"Downloaded the tutorial in {toc - self.__lifespan_timer_start:0.4f} seconds")
+            print(f"Downloaded the tutorial in {toc - self.__lifespan_timer_start:0.4f} seconds")  # noqa: T001
 
-            print(f"Deleting an instance of {cls.__name__}")  # noqa
+            print(f"Deleting an instance of {cls.__name__}")  # noqa: T001
 
             try:
                 super().__del__()
