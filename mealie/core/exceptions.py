920bee9c29bfedb8260847987ae65200a1850f4d
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-13 10:24:59 -0900

    centralize error handling

--- mealie/core/exceptions.py
+++ mealie/core/exceptions.py
@@ -1,3 +1,8 @@
+from sqlite3 import IntegrityError
+
+from mealie.lang.providers import AbstractLocaleProvider
+
+
 class PermissionDenied(Exception):
     """
     This exception is raised when a user tries to access a resource that they do not have permission to access.
@@ -12,3 +17,15 @@ class NoEntryFound(Exception):
     """
 
     pass
+
+
+def mealie_registered_exceptions(t: AbstractLocaleProvider) -> dict:
+    """
+    This function returns a dictionary of all the globally registered exceptions in the Mealie application.
+    """
+
+    return {
+        PermissionDenied: t.t("exceptions.permission-denied"),
+        NoEntryFound: t.t("exceptions.no-entry-found"),
+        IntegrityError: t.t("exceptions.integrity-error"),
+    }
