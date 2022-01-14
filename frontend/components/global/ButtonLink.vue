ea8e9e77e6e13ee5fa9748fd7a00543311860f5b
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-13 16:19:35 -0900

    init button link for common styles

---
+++ frontend/components/global/ButtonLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-btn outlined class="rounded-xl my-1 mx-1" :to="to">
+      <v-icon left v-if="icon != ''">
+        {{ icon }}
+      </v-icon>
+      {{ text }}
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      default: "Link",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
