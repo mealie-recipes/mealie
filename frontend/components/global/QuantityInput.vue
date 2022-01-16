3d7d5a8644592a6af6231390934c955dd8355df4
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-15 17:05:15 -0900

    init global utility components

---
+++ frontend/components/global/QuantityInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex align-center" style="max-width: 60px">
+    <v-text-field
+      v-model.number="quantity"
+      hide-details
+      label="Qty"
+      :min="min"
+      :max="max"
+      type="number"
+      class="rounded-xl"
+      small
+      text
+    >
+    </v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "VInputNumber",
+  props: {
+    label: {
+      type: String,
+      default: "Qty",
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 9999,
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    step: {
+      type: Number,
+      default: 1,
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup(props, context) {
+    const quantity = computed({
+      get: () => {
+        return Number(props.value);
+      },
+      set: (val) => {
+        context.emit("input", val);
+      },
+    });
+
+    return {
+      quantity,
+    };
+  },
+});
+</script>
