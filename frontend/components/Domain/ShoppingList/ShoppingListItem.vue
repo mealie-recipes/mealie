95f1a7359d59f2fcd47c2bc19fe5562d0c15aa18
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-15 17:36:47 -0900

    breakout item editor

--- frontend/components/Domain/ShoppingList/ShoppingListItem.vue
+++ frontend/components/Domain/ShoppingList/ShoppingListItem.vue
@@ -1,17 +1,26 @@
 <template>
-  <div v-if="!edit" class="small-checkboxes d-flex justify-space-between align-center">
-    <v-checkbox v-model="listItem.checked" hide-details dense :label="listItem.note" @change="$emit('checked')">
+  <div v-if="!edit" class="d-flex justify-space-between align-center">
+    <v-checkbox
+      v-model="listItem.checked"
+      color="null"
+      hide-details
+      dense
+      :label="listItem.note"
+      @change="$emit('checked')"
+    >
       <template #label>
         <div>
           {{ listItem.quantity }} <v-icon size="16" class="mx-1"> {{ $globals.icons.close }} </v-icon>
-          {{ listItem.note }}
+          <span :class="listItem.checked ? 'strike-through' : ''">
+            {{ listItem.note }}
+          </span>
         </div>
       </template>
     </v-checkbox>
     <v-chip v-if="listItem.label" class="ml-auto mt-2" small label>
       {{ listItem.label.name }}
     </v-chip>
-    <div>
+    <div style="min-width: 72px">
       <v-menu offset-x left min-width="125px">
         <template #activator="{ on, attrs }">
           <v-btn small class="ml-2 mt-2 handle" icon v-bind="attrs" v-on="on">
@@ -33,62 +42,23 @@
       </v-btn>
     </div>
   </div>
-  <div v-else class="my-1">
-    <v-card outlined>
-      <v-card-text>
-        <v-textarea v-model="listItem.note" hide-details label="Note" rows="1" auto-grow></v-textarea>
-        <div style="max-width: 300px" class="mt-3">
-          <v-autocomplete
-            v-model="listItem.labelId"
-            name=""
-            :items="labels"
-            item-value="id"
-            hide-details
-            item-text="name"
-            clearable
-            :prepend-inner-icon="$globals.icons.tags"
-          >
-          </v-autocomplete>
-        </div>
-      </v-card-text>
-    </v-card>
-    <v-card-actions class="ma-0 pt-0 pb-1 justify-end">
-      <BaseButtonGroup
-        :buttons="[
-          {
-            icon: $globals.icons.delete,
-            text: $t('general.delete'),
-            event: 'delete',
-          },
-          {
-            icon: $globals.icons.close,
-            text: $t('general.cancel'),
-            event: 'cancel',
-          },
-          {
-            icon: $globals.icons.foods,
-            text: 'Toggle Food',
-            event: 'toggle-foods',
-          },
-          {
-            icon: $globals.icons.save,
-            text: $t('general.save'),
-            event: 'save',
-          },
-        ]"
-        @save="save"
-        @cancel="edit = !edit"
-        @delete="$emit('general.delete')"
-        @toggle-foods="listItem.isFood = !listItem.isFood"
-      />
-    </v-card-actions>
+  <div v-else class="mb-1 mt-6">
+    <ShoppingListItemEditor
+      v-model="listItem"
+      :labels="labels"
+      @save="save"
+      @cancel="edit = !edit"
+      @delete="$emit('delete')"
+      @toggle-foods="listItem.isFood = !listItem.isFood"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref } from "@nuxtjs/composition-api";
-import { Label } from "~/api/class-interfaces/group-multiple-purpose-labels";
-import { ShoppingListItemCreate } from "~/api/class-interfaces/group-shopping-lists";
+import ShoppingListItemEditor from "./ShoppingListItemEditor.vue";
+import { ShoppingListItemCreate } from "~/types/api-types/group";
+import { MultiPurposeLabelOut } from "~/types/api-types/labels";
 
 interface actions {
   text: string;
@@ -111,13 +81,14 @@ const contextMenu: actions[] = [
 ];
 
 export default defineComponent({
+  components: { ShoppingListItemEditor },
   props: {
     value: {
       type: Object as () => ShoppingListItemCreate,
       required: true,
     },
     labels: {
-      type: Array as () => Label[],
+      type: Array as () => MultiPurposeLabelOut[],
       required: true,
     },
   },
@@ -168,3 +139,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="css">
+.strike-through {
+  text-decoration: line-through !important;
+}
+</style>
---
+++ frontend/components/Domain/ShoppingList/ShoppingListItemEditor.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <v-card outlined>
+      <v-card-text class="pb-3 pt-1">
+        <div class="d-md-flex align-center" style="gap: 20px">
+          <v-textarea v-model="listItem.note" hide-details label="Note" rows="1" auto-grow></v-textarea>
+        </div>
+        <div class="d-flex align-end" style="gap: 20px">
+          <div>
+            <QuantityInput v-model="listItem.quantity" />
+          </div>
+          <div style="max-width: 300px" class="mt-3 mr-auto">
+            <v-autocomplete
+              v-model="listItem.labelId"
+              name=""
+              :items="labels"
+              item-value="id"
+              label="Label"
+              hide-details
+              item-text="name"
+              clearable
+              :prepend-inner-icon="$globals.icons.tags"
+            >
+            </v-autocomplete>
+          </div>
+          <v-menu
+            v-if="listItem.recipeReferences && listItem.recipeReferences.length > 0"
+            open-on-hover
+            offset-y
+            left
+            top
+          >
+            <template #activator="{ on, attrs }">
+              <v-icon class="mt-auto" icon v-bind="attrs" color="warning" v-on="on">
+                {{ $globals.icons.alert }}
+              </v-icon>
+            </template>
+            <v-card max-width="350px" class="left-warning-border">
+              <v-card-text>
+                This item is linked to one or more recipe. Adjusting the units or foods will yield unexpected results
+                when adding or removing the recipe from this list.
+              </v-card-text>
+            </v-card>
+          </v-menu>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card-actions class="ma-0 pt-0 pb-1 justify-end">
+      <BaseButtonGroup
+        :buttons="[
+          {
+            icon: $globals.icons.delete,
+            text: $t('general.delete'),
+            event: 'delete',
+          },
+          {
+            icon: $globals.icons.close,
+            text: $t('general.cancel'),
+            event: 'cancel',
+          },
+          {
+            icon: $globals.icons.foods,
+            text: 'Toggle Food',
+            event: 'toggle-foods',
+          },
+          {
+            icon: $globals.icons.save,
+            text: $t('general.save'),
+            event: 'save',
+          },
+        ]"
+        @save="$emit('save')"
+        @cancel="$emit('cancel')"
+        @delete="$emit('delete')"
+        @toggle-foods="listItem.isFood = !listItem.isFood"
+      />
+    </v-card-actions>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "@nuxtjs/composition-api";
+import { ShoppingListItemCreate, ShoppingListItemOut } from "~/types/api-types/group";
+import { MultiPurposeLabelOut } from "~/types/api-types/labels";
+
+export default defineComponent({
+  props: {
+    value: {
+      type: Object as () => ShoppingListItemCreate | ShoppingListItemOut,
+      required: true,
+    },
+    labels: {
+      type: Array as () => MultiPurposeLabelOut[],
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const listItem = computed({
+      get: () => {
+        return props.value;
+      },
+      set: (val) => {
+        context.emit("input", val);
+      },
+    });
+    return {
+      listItem,
+    };
+  },
+  head: {
+    title: "vbase-nuxt",
+  },
+});
+</script>
