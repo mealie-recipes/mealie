775b77e34e524ac18194f1b065fdc709170881cb
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-02-07 13:01:16 -0900

    add plan rules schema

---
+++ mealie/schema/meal_plan/plan_rules.py
@@ -0,0 +1,56 @@
+import datetime
+from enum import Enum
+
+from fastapi_camelcase import CamelModel
+from pydantic import UUID4
+
+
+class Category(CamelModel):
+    id: int
+    slug: str
+
+    class Config:
+        orm_mode = True
+
+
+class PlanRulesDay(str, Enum):
+    monday = "monday"
+    tuesday = "tuesday"
+    wednesday = "wednesday"
+    thursday = "thursday"
+    friday = "friday"
+    saturday = "saturday"
+    sunday = "sunday"
+    unset = "unset"
+
+    @staticmethod
+    def from_date(date: datetime.date):
+        """Returns the enum value for the date passed in"""
+        try:
+            return PlanRulesDay[(date.strftime("%A").lower())]
+        except KeyError:
+            return PlanRulesDay.unset
+
+
+class PlanRulesType(str, Enum):
+    breakfast = "breakfast"
+    lunch = "lunch"
+    dinner = "dinner"
+    unset = "unset"
+
+
+class PlanRulesCreate(CamelModel):
+    day: PlanRulesDay = PlanRulesDay.unset
+    entry_type: PlanRulesType = PlanRulesType.unset
+    categories: list[Category] = []
+
+
+class PlanRulesSave(PlanRulesCreate):
+    group_id: UUID4
+
+
+class PlanRulesOut(PlanRulesSave):
+    id: UUID4
+
+    class Config:
+        orm_mode = True
