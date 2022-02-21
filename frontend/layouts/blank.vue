aea986d5458f7cabb4ab6f60a4487c770ba8104e
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-02-20 15:05:55 -0900

    remove default password and header

---
+++ frontend/layouts/blank.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app dark>
+    <TheSnackbar />
+
+    <v-main>
+      <v-scroll-x-transition>
+        <Nuxt />
+      </v-scroll-x-transition>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import TheSnackbar from "~/components/Layout/TheSnackbar.vue";
+export default defineComponent({
+  components: { TheSnackbar },
+});
+</script>
