a2b56393a9ddcca1d71479a3d6d58f827714c99f
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-03-15 17:17:29 -0800

    add banner on login page

--- frontend/layouts/blank.vue
+++ frontend/layouts/blank.vue
@@ -2,6 +2,12 @@
   <v-app dark>
     <TheSnackbar />
 
+    <v-banner v-if="isDemo" sticky>
+      <div class="text-center">
+        <b> This is a Demo for version: {{ version }} </b> | Username: changeme@email.com | Password: demo
+      </div>
+    </v-banner>
+
     <v-main>
       <v-scroll-x-transition>
         <Nuxt />
@@ -11,9 +17,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@nuxtjs/composition-api";
+import { computed, defineComponent } from "@nuxtjs/composition-api";
 import TheSnackbar from "~/components/Layout/TheSnackbar.vue";
+import { useAppInfo } from "~/composables/api";
 export default defineComponent({
   components: { TheSnackbar },
+  setup() {
+    const appInfo = useAppInfo();
+
+    const isDemo = computed(() => appInfo?.value?.demoStatus || false);
+
+    const version = computed(() => appInfo?.value?.version || "unknown");
+
+    return {
+      appInfo,
+      isDemo,
+      version,
+    };
+  },
 });
 </script>
