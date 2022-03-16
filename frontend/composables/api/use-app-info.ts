960378b213f78532991808826f7a695dd7b2fe4e
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-03-15 19:31:45 -0800

    hotfix: run fetch client side only

--- frontend/composables/api/use-app-info.ts
+++ frontend/composables/api/use-app-info.ts
@@ -1,11 +1,13 @@
-import { Ref, useAsync } from "@nuxtjs/composition-api";
-import { useAsyncKey } from "../use-utils";
+import { onMounted, ref, Ref } from "@nuxtjs/composition-api";
 import { AppInfo } from "~/types/api-types/admin";
 
 export function useAppInfo(): Ref<AppInfo | null> {
-  return useAsync(async () => {
-    // We use fetch here to reduce need for additional dependencies
+  const appInfo = ref<null | AppInfo>(null);
+
+  onMounted(async () => {
     const data = await fetch("/api/app/about").then((res) => res.json());
-    return data as AppInfo;
-  }, useAsyncKey());
+    appInfo.value = data as AppInfo;
+  });
+
+  return appInfo;
 }
