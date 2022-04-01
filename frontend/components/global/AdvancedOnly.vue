899377d8080e2f93fb71ea072a10b8e70ab12f7f
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-04-01 10:31:39 -0800

    add utility type to hide Advanced items

---
+++ frontend/components/global/AdvancedOnly.vue
@@ -0,0 +1,23 @@
+<template>
+  <div scoped-slot></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useContext } from "@nuxtjs/composition-api";
+
+/**
+ * Renderless component that only renders if the user is logged in.
+ * and has advanced options toggled.
+ */
+export default defineComponent({
+  setup(_, ctx) {
+    const { $auth } = useContext();
+
+    const r = $auth?.user?.advanced || false;
+
+    return () => {
+      return r ? ctx.slots.default?.() : null;
+    };
+  },
+});
+</script>
