76a766ad9f3b9ec4cb7cb6b8b7d1a9bdda049a92
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-05-25 19:14:53 -0800

    refactor UI for bulk importer

--- frontend/pages/recipe/create/bulk.vue
+++ frontend/pages/recipe/create/bulk.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <v-card flat>
+    <div flat>
       <v-card-title class="headline"> Recipe Bulk Importer </v-card-title>
       <v-card-text>
         The Bulk recipe importer allows you to import multiple recipes at once by queing the sites on the backend and
         running the task in the background. This can be useful when initially migrating to Mealie, or when you want to
         import a large number of recipes.
       </v-card-text>
-    </v-card>
+    </div>
     <section class="mt-2">
-      <v-row v-for="(bulkUrl, idx) in bulkUrls" :key="'bulk-url' + idx" class="my-1" dense>
+      <v-row v-for="(_, idx) in bulkUrls" :key="'bulk-url' + idx" class="my-1" dense>
         <v-col cols="12" xs="12" sm="12" md="12">
           <v-text-field
             v-model="bulkUrls[idx].url"
@@ -26,7 +26,7 @@
             class="rounded-lg"
           >
             <template #append>
-              <v-btn color="error" icon x-small @click="bulkUrls.splice(idx, 1)">
+              <v-btn style="margin-top: -2px" icon small @click="bulkUrls.splice(idx, 1)">
                 <v-icon>
                   {{ $globals.icons.delete }}
                 </v-icon>
@@ -34,38 +34,40 @@
             </template>
           </v-text-field>
         </v-col>
-        <v-col cols="12" xs="12" sm="6">
-          <RecipeOrganizerSelector
-            v-model="bulkUrls[idx].categories"
-            :items="allCategories || []"
-            selector-type="category"
-            :input-attrs="{
-              filled: true,
-              singleLine: true,
-              dense: true,
-              rounded: true,
-              class: 'rounded-lg',
-              hideDetails: true,
-              clearable: true,
-            }"
-          />
-        </v-col>
-        <v-col cols="12" xs="12" sm="6">
-          <RecipeOrganizerSelector
-            v-model="bulkUrls[idx].tags"
-            :items="allTags || []"
-            selector-type="tag"
-            :input-attrs="{
-              filled: true,
-              singleLine: true,
-              dense: true,
-              rounded: true,
-              class: 'rounded-lg',
-              hideDetails: true,
-              clearable: true,
-            }"
-          />
-        </v-col>
+        <template v-if="showCatTags">
+          <v-col cols="12" xs="12" sm="6">
+            <RecipeOrganizerSelector
+              v-model="bulkUrls[idx].categories"
+              :items="allCategories || []"
+              selector-type="category"
+              :input-attrs="{
+                filled: true,
+                singleLine: true,
+                dense: true,
+                rounded: true,
+                class: 'rounded-lg',
+                hideDetails: true,
+                clearable: true,
+              }"
+            />
+          </v-col>
+          <v-col cols="12" xs="12" sm="6">
+            <RecipeOrganizerSelector
+              v-model="bulkUrls[idx].tags"
+              :items="allTags || []"
+              selector-type="tag"
+              :input-attrs="{
+                filled: true,
+                singleLine: true,
+                dense: true,
+                rounded: true,
+                class: 'rounded-lg',
+                hideDetails: true,
+                clearable: true,
+              }"
+            />
+          </v-col>
+        </template>
       </v-row>
       <v-card-actions class="justify-end">
         <BaseButton
@@ -78,31 +80,55 @@
           Clear
         </BaseButton>
         <v-spacer></v-spacer>
-        <BaseButton color="info" @click="bulkUrls.push({ url: '', categories: [], tags: [] })">
-          <template #icon> {{ $globals.icons.createAlt }} </template> New
+        <BaseButton class="mr-1" color="info" @click="bulkUrls.push({ url: '', categories: [], tags: [] })">
+          <template #icon> {{ $globals.icons.createAlt }} </template>
+          New
         </BaseButton>
+        <RecipeDialogBulkAdd v-model="bulkDialog" @bulk-data="assignUrls" />
+      </v-card-actions>
+      <div class="px-1">
+        <v-checkbox v-model="showCatTags" hide-details label="Set Categories and Tags " />
+      </div>
+      <v-card-actions class="justify-end">
         <BaseButton :disabled="bulkUrls.length === 0 || lockBulkImport" @click="bulkCreate">
-          <template #icon> {{ $globals.icons.check }} </template> Submit
+          <template #icon> {{ $globals.icons.check }} </template>
+          Submit
         </BaseButton>
       </v-card-actions>
+    </section>
+    <section class="mt-12">
+      <BaseCardSectionTitle title="Bulk Imports"> </BaseCardSectionTitle>
+      <ReportTable :items="reports" @delete="deleteReport" />
     </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, toRefs, ref } from "@nuxtjs/composition-api";
+import { whenever } from "@vueuse/shared";
 import { useUserApi } from "~/composables/api";
 import { alert } from "~/composables/use-toast";
 import RecipeOrganizerSelector from "~/components/Domain/Recipe/RecipeOrganizerSelector.vue";
 import { useCategories, useTags } from "~/composables/recipes";
+import { ReportSummary } from "~/types/api-types/reports";
+import RecipeDialogBulkAdd from "~/components/Domain/Recipe/RecipeDialogBulkAdd.vue";
 
 export default defineComponent({
-  components: { RecipeOrganizerSelector },
+  components: { RecipeOrganizerSelector, RecipeDialogBulkAdd },
   setup() {
     const state = reactive({
       error: false,
       loading: false,
+      showCatTags: false,
+      bulkDialog: false,
     });
+
+    whenever(
+      () => !state.showCatTags,
+      () => {
+        console.log("showCatTags changed");
+      }
+    );
 
     const api = useUserApi();
 
@@ -122,6 +148,8 @@ export default defineComponent({
       } else {
         alert.error("Bulk import process has failed");
       }
+
+      fetchReports();
     }
 
     const { allTags, useAsyncGetAll: getAllTags } = useTags();
@@ -130,7 +158,37 @@ export default defineComponent({
     getAllTags();
     getAllCategories();
 
+    // =========================================================
+    // Reports
+
+    const reports = ref<ReportSummary[]>([]);
+
+    async function fetchReports() {
+      const { data } = await api.groupReports.getAll("bulk_import");
+      reports.value = data ?? [];
+    }
+
+    async function deleteReport(id: string) {
+      console.log(id);
+      const { response } = await api.groupReports.deleteOne(id);
+
+      if (response?.status === 200) {
+        fetchReports();
+      } else {
+        alert.error("Report deletion failed");
+      }
+    }
+
+    fetchReports();
+
+    function assignUrls(urls: string[]) {
+      bulkUrls.value = urls.map((url) => ({ url, categories: [], tags: [] }));
+    }
+
     return {
+      assignUrls,
+      reports,
+      deleteReport,
       allTags,
       allCategories,
       bulkCreate,
