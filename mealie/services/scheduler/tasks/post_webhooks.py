567093539c4de787c6ab788f176d2037bd64a2aa
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-06-17 13:13:29 -0800

    implement meal-plan data getter

--- mealie/services/scheduler/tasks/post_webhooks.py
+++ mealie/services/scheduler/tasks/post_webhooks.py
@@ -2,11 +2,12 @@ from datetime import datetime, timezone
 
 import requests
 from fastapi.encoders import jsonable_encoder
+from pydantic import UUID4
 from sqlalchemy.orm import Session
 
-from mealie.core import root_logger
 from mealie.db.db_setup import create_session
 from mealie.db.models.group.webhooks import GroupWebhooksModel
+from mealie.repos.all_repositories import get_repositories
 
 last_ran = datetime.now(timezone.utc)
 
@@ -31,12 +32,23 @@ def get_scheduled_webhooks(session: Session, bottom: datetime, top: datetime) ->
 def post_group_webhooks() -> None:
     global last_ran
     session = create_session()
-
-    logger = root_logger.get_logger()
     results = get_scheduled_webhooks(session, last_ran, datetime.now())
 
     last_ran = datetime.now(timezone.utc)
 
+    repos = get_repositories(session)
+
+    memo = {}
+
+    def get_meals(group_id: UUID4):
+        if group_id not in memo:
+            memo[group_id] = repos.meals.get_all(group_id=group_id)
+        return memo[group_id]
+
     for result in results:
-        logger.debug(f"posting webhooks for entry {result}")
-        requests.post(result.url, json=jsonable_encoder({"Hello": "World"}))  # TODO: add data to webhook
+        meals = get_meals(result.group_id)
+
+        if not meals:
+            continue
+
+        requests.post(result.url, json=jsonable_encoder(meals))
