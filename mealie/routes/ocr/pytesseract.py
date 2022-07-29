15a4046588514ab59f25f721d2566068a61a71f4
Miroito <alban.vachette@gmail.com>
2022-07-29 16:22:02 +0200

    Correct sloppy ocr classes

--- mealie/routes/ocr/pytesseract.py
+++ mealie/routes/ocr/pytesseract.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, File
 
 from mealie.routes._base import BaseUserController, controller
 from mealie.schema.ocr.ocr import OcrAssetReq, OcrTsvResponse
-from mealie.services.ocr.pytesseract import OCR
+from mealie.services.ocr.pytesseract import OcrService
 from mealie.services.recipe.recipe_data_service import RecipeDataService
 from mealie.services.recipe.recipe_service import RecipeService
 
@@ -11,21 +11,27 @@ router = APIRouter()
 
 @controller(router)
 class OCRController(BaseUserController):
+    def __init__(self):
+        self.ocr_service = OcrService()
+
     @router.post("/", response_model=str)
     def image_to_string(self, file: bytes = File(...)):
-        return OCR.image_to_string(file)
+        return self.ocr_service.image_to_string(file)
 
     @router.post("/file-to-tsv", response_model=list[OcrTsvResponse])
     def file_to_tsv(self, file: bytes = File(...)):
-        tsv = OCR.image_to_tsv(file)
-        return OCR.format_tsv_output(tsv)
+        tsv = self.ocr_service.image_to_tsv(file)
+        return self.ocr_service.format_tsv_output(tsv)
 
     @router.post("/asset-to-tsv", response_model=list[OcrTsvResponse])
     def asset_to_tsv(self, req: OcrAssetReq):
-        recipe = RecipeService._get_recipe(self, req.recipe_slug)
+        recipe_service = RecipeService(self.repos, self.user, self.group)
+        recipe = recipe_service._get_recipe(req.recipe_slug)
+        if recipe.id is None:
+            return []
         data_service = RecipeDataService(recipe.id, recipe.group_id)
         asset_path = data_service.dir_assets.joinpath(req.asset_name)
         file = open(asset_path, "rb")
-        tsv = OCR.image_to_tsv(file.read())
+        tsv = self.ocr_service.image_to_tsv(file.read())
 
-        return OCR.format_tsv_output(tsv)
+        return self.ocr_service.format_tsv_output(tsv)
--- mealie/services/ocr/pytesseract.py
+++ mealie/services/ocr/pytesseract.py
@@ -7,28 +7,30 @@ from mealie.schema.ocr.ocr import OcrTsvResponse
 from mealie.services._base_service import BaseService
 
 
-class OCR(BaseService):
+class OcrService(BaseService):
     """
     Class for ocr engines.
     """
 
-    def image_to_string(image_data):
+    def image_to_string(self, image_data):
         """
         Returns a plain text translation of an image
         """
         return pytesseract.image_to_string(Image.open(image_data))
 
-    def image_to_tsv(image_data):
+    def image_to_tsv(self, image_data, lang=None):
         """
         Returns tsv formatted output
         """
+        if lang is not None:
+            return pytesseract.image_to_data(Image.open(BytesIO(image_data)), lang=lang)
 
         return pytesseract.image_to_data(Image.open(BytesIO(image_data)))
 
-    def format_tsv_output(tsv: str) -> OcrTsvResponse:
+    def format_tsv_output(self, tsv: str) -> list[OcrTsvResponse]:
         lines = tsv.split("\n")
         titles = [t.strip() for t in lines[0].split("\t")]
-        response = []
+        response: list[OcrTsvResponse] = []
         #  len-1 because the last line is empty
         for i in range(1, len(lines) - 1):
             d = {}
