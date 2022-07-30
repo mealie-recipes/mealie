9ca704761efc8b56baa70a55b590687d94012d2c
Miroito <alban.vachette@gmail.com>
2022-07-30 11:49:47 +0200

    Rewrite safer tests

--- tests/unit_tests/services_tests/test_ocr_service.py
+++ tests/unit_tests/services_tests/test_ocr_service.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 
-from mealie.services.ocr.pytesseract import OCR
+from mealie.services.ocr.pytesseract import OcrService
+
+ocr_service = OcrService()
 
 
 def test_image_to_string():
-    result = OCR.image_to_string(open(Path("tests/data/images/test-ocr.png"), "rb"))
-    expected_result = open(Path("tests/data/text/test-ocr.txt"))
-    assert result == expected_result.read()
+    with open(Path("tests/data/images/test-ocr.png"), "rb") as image:
+        result = ocr_service.image_to_string(image)
+        with open(Path("tests/data/text/test-ocr.txt"), "r", encoding="utf-8") as expected_result:
+            assert result == expected_result.read()
 
 
 def test_image_to_tsv():
-    result = OCR.image_to_tsv(open(Path("tests/data/images/test-ocr.png"), "rb").read())
-    expected_result = open(Path("tests/data/text/test-ocr.tsv"))
-    assert result == expected_result.read()
+    with open(Path("tests/data/images/test-ocr.png"), "rb") as image:
+        result = ocr_service.image_to_tsv(image.read())
+        with open(Path("tests/data/text/test-ocr.tsv"), "r", encoding="utf-8") as expected_result:
+            assert result == expected_result.read()
