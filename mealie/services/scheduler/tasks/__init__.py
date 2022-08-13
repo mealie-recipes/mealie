555e513ad9eefdb0dc35db7bb4225fbfadb6b643
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-08-13 11:25:00 -0800

    implement scheduled task

--- mealie/services/scheduler/tasks/__init__.py
+++ mealie/services/scheduler/tasks/__init__.py
@@ -2,12 +2,14 @@ from .post_webhooks import post_group_webhooks
 from .purge_group_exports import purge_group_data_exports
 from .purge_password_reset import purge_password_reset_tokens
 from .purge_registration import purge_group_registration
+from .reset_locked_users import locked_user_reset
 
 __all__ = [
     "post_group_webhooks",
     "purge_password_reset_tokens",
     "purge_group_data_exports",
     "purge_group_registration",
+    "locked_user_reset",
 ]
 
 """
--- mealie/services/user_services/user_service.py
+++ mealie/services/user_services/user_service.py
@@ -13,6 +13,22 @@ class UserService(BaseService):
     def get_locked_users(self) -> list[PrivateUser]:
         return self.repos.users.get_locked_users()
 
+    def reset_locked_users(self) -> int:
+        """
+        Queriers that database for all locked users and resets their locked_at field to None
+        if more than the set time has passed since the user was locked
+        """
+        locked_users = self.get_locked_users()
+
+        unlocked = 0
+
+        for user in locked_users:
+            if not user.is_locked and user.locked_at is not None:
+                self.unlock_user(user)
+                unlocked += 1
+
+        return unlocked
+
     def lock_user(self, user: PrivateUser) -> PrivateUser:
         user.locked_at = datetime.now()
         return self.repos.users.update(user.id, user)
