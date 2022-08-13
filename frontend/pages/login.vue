e50afb9ac075f2eb61b3b871644ecf3fb791f59b
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-08-13 12:54:43 -0800

    use ignore instead of expect-error

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -157,10 +157,10 @@ export default defineComponent({
         // See https://github.com/nuxt-community/axios-module/issues/550
         // Import $axios from useContext()
         // if ($axios.isAxiosError(error) && error.response?.status === 401) {
-        // @ts-expect-error - see above
+        // @ts-ignore- see above
         if (error.response?.status === 401) {
           alert.error("Invalid Credentials");
-          // @ts-expect-error - see above
+          // @ts-ignore - see above
         } else if (error.response?.status === 423) {
           alert.error("Account Locked. Please try again later");
         } else {
