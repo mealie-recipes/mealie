23d1be9d106856feb65821beab74a4439b902967
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-08-12 20:10:10 -0800

    save increment to database

--- mealie/core/security/security.py
+++ mealie/core/security/security.py
@@ -103,6 +103,7 @@ def authenticate_user(session, email: str, password: str) -> PrivateUser | bool:
 
     elif not verify_password(password, user.password):
         user.login_attemps += 1
+        db.users.update(user.id, user)
 
         if user.login_attemps >= settings.SECURITY_MAX_LOGIN_ATTEMPS:
             user_service = UserService(db)
