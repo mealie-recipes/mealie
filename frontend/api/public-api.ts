b6a631e1182ee1c6be97d1d14b0acd49bde894ed
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-08-27 12:36:01 -0800

    public API endpoint

--- frontend/api/public-api.ts
+++ frontend/api/public-api.ts
@@ -1,11 +1,14 @@
 import { ValidatorsApi } from "./public/validators";
+import { ExploreApi } from "./public/explore";
 import { ApiRequestInstance } from "~/types/api";
 
 export class PublicApi {
   public validators: ValidatorsApi;
+  public explore: ExploreApi;
 
   constructor(requests: ApiRequestInstance) {
     this.validators = new ValidatorsApi(requests);
+    this.explore = new ExploreApi(requests);
 
     Object.freeze(this);
   }
