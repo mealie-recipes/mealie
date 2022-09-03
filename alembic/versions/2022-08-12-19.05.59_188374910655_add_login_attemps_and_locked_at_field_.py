965c85c8109cd422a2b6911eaa697e4c6fc5c2c3
Miroito <alban.vachette@gmail.com>
2022-09-03 18:30:02 +0200

    Rebasing alembic migrations sux

--- alembic/versions/2022-08-12-19.05.59_188374910655_add_login_attemps_and_locked_at_field_.py
+++ alembic/versions/2022-08-12-19.05.59_188374910655_add_login_attemps_and_locked_at_field_.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "188374910655"
-down_revision = "089bfa50d0ed"
+down_revision = "f30cf048c228"
 branch_labels = None
 depends_on = None
 
