4d7ca6d3d670888efe5da9ccf5ef0d8e59d2e36b
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2022-11-14 17:31:15 +0000

    formalized API call and added mobile-friendly view

--- frontend/components/Domain/Recipe/RecipeDialogTimeline.vue
+++ frontend/components/Domain/Recipe/RecipeDialogTimeline.vue
@@ -1,150 +1,195 @@
 <template>
-    <BaseDialog
-        v-model="dialog"
-        :title="`Timeline – ${recipeName}`"
-        :icon="$globals.icons.timelineText"
-        width="70%"
+  <BaseDialog
+    v-model="dialog"
+    :title="useMobileFormat ? 'Timeline' : `Timeline – ${recipeName}`"
+    :icon="$globals.icons.timelineText"
+    width="70%"
+  >
+    <!-- Desktop -->
+    <div v-if="!useMobileFormat">
+      <v-card
+        v-if="timelineEvents && timelineEvents.length"
+        height="fit-content"
+        max-height="70vh"
+        width="100%"
+        style="overflow-y: auto;"
       >
-        <v-card
-          v-if="timelineEvents && timelineEvents.length"
-          height="fit-content"
-          max-height="70vh"
-          width="100%"
-          style="overflow-y: auto;"
-        >
-          <v-timeline>
-            <v-timeline-item
-              v-for="event in timelineEvents"
-              :key="event.id"
-              class="px-3"
-              fill-dot
-              :icon="chooseEventIcon(event)"
-            >
-              <template #opposite>
-                <v-chip v-if="event.timestamp" label large>
-                  <v-icon class="mr-1"> {{ $globals.icons.calendar }} </v-icon>
+        <v-timeline>
+          <v-timeline-item
+            v-for="event in timelineEvents"
+            :key="event.id"
+            class="px-3"
+            fill-dot
+            :icon="chooseEventIcon(event)"
+          >
+            <template #opposite>
+              <v-chip v-if="event.timestamp" label large>
+                <v-icon class="mr-1"> {{ $globals.icons.calendar }} </v-icon>
+                {{ new Date(event.timestamp).toLocaleDateString($i18n.locale) }}
+              </v-chip>
+            </template>
+            <v-card>
+              <v-sheet>
+                <v-card-title>
+                  <div class="mr-3"><UserAvatar :user-id="event.userId" /></div>
+                  {{ event.subject }}
+                </v-card-title>
+                <v-card-text v-if="event.eventMessage">
+                  {{ event.eventMessage }}
+                </v-card-text>
+              </v-sheet>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card>
+      <v-card v-else>
+        <v-card-title class="justify-center">
+          Nothing on the timeline yet. Try making this recipe!
+        </v-card-title>
+      </v-card>
+    </div>
+
+    <!-- Mobile | Dense -->
+    <div v-else>
+      <v-card
+        v-if="timelineEvents && timelineEvents.length"
+        height="fit-content"
+        max-height="70vh"
+        width="100%"
+        style="overflow-y: auto;"
+      >
+        <v-timeline align-top dense>
+          <v-timeline-item
+            v-for="event in timelineEvents"
+            :key="event.id"
+            class="pr-3 mb-9"
+            fill-dot
+            small
+            :icon="chooseEventIcon(event)"
+          >
+            <v-row v-if="event.timestamp" class="mb-1">
+              <v-col align-self="center" sm="auto" cols="2" class="pa-0">
+                <UserAvatar :user-id="event.userId" />
+              </v-col>
+              <v-col align-self="center" class="ml-3 pa-0">
+                <v-chip label small>
+                  <v-icon> {{ $globals.icons.calendar }} </v-icon>
                   {{ new Date(event.timestamp).toLocaleDateString($i18n.locale) }}
                 </v-chip>
-              </template>
-              <v-card>
-                <v-sheet>
-                  <v-card-title>
-                    <div class="mr-3"><UserAvatar :user-id="event.userId" /></div>
-                    {{ event.subject }}
-                  </v-card-title>
-                  <v-card-text v-if="event.message">
-                    {{ event.message }}
-                  </v-card-text>
-                </v-sheet>
-              </v-card>
-            </v-timeline-item>
-          </v-timeline>
-        </v-card>
-        <v-card v-else>
-          <v-card-title class="justify-center">
-            Nothing on the timeline yet. Try making this recipe!
-          </v-card-title>
-        </v-card>
-      </BaseDialog>
-  </template>
+              </v-col>
+            </v-row>
+            <v-row class="mt-0">
+              <v-col class="pa-0">
+                <strong>{{ event.subject }}</strong>
+                <div v-if="event.eventMessage" class="text-caption">
+                  {{ event.eventMessage }}
+                </div>
+              </v-col>
+            </v-row>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card>
+      <v-card v-else>
+        <v-card-text class="justify-center pa-1">
+          Nothing on the timeline yet. Try making this recipe!
+        </v-card-text>
+      </v-card>
+    </div>
 
-  <script lang="ts">
-  import { computed, defineComponent, ref, useContext, } from "@nuxtjs/composition-api";
-  import { whenever } from "@vueuse/core";
-  import { RecipeTimelineEventOut } from "~/lib/api/types/recipe"
-  import { RequestResponse } from "~/lib/api/types/non-generated";
-  import UserAvatar from "~/components/Domain/User/UserAvatar.vue";
+  </BaseDialog>
+</template>
 
-  export default defineComponent({
-    components: { UserAvatar },
+<script lang="ts">
+import { computed, defineComponent, ref, useContext, } from "@nuxtjs/composition-api";
+import { whenever } from "@vueuse/core";
+import { useUserApi } from "~/composables/api";
+import { RecipeTimelineEventOut } from "~/lib/api/types/recipe"
+import UserAvatar from "~/components/Domain/User/UserAvatar.vue";
 
-    props: {
-      value: {
-        type: Boolean,
-        default: false,
-      },
-      slug: {
-        type: String,
-        default: "",
-      },
-      recipeName: {
-        type: String,
-        default: "",
-      },
+export default defineComponent({
+  components: { UserAvatar },
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
     },
-
-    setup(props, context) {
-      // V-Model Support
-      const dialog = computed({
-        get: () => {
-          return props.value;
-        },
-        set: (val) => {
-          context.emit("input", val);
-        },
-      });
-
-      whenever(
-        () => props.value,
-        () => {
-          refreshTimelineEvents();
-        }
-      );
-
-      const timelineEvents = ref([{}] as RecipeTimelineEventOut[])
-      const { $axios, $globals } = useContext();
-
-      function chooseEventIcon(event: RecipeTimelineEventOut) {
-        switch (event.eventType) {
-          case "comment":
-            return $globals.icons.commentTextMultiple;
-
-          case "info":
-            return $globals.icons.informationVariant;
-
-          case "system":
-            return $globals.icons.cog;
-
-          default:
-            return $globals.icons.informationVariant;
-        };
-      };
-
-      async function refreshTimelineEvents() {
-        // TODO: implement this properly when PR #1801 is merged
-        // TODO: switch message to eventMessage when PR #1801 is merged
-        // TODO: implement infinite scroll and paginate
-
-        console.error("Using temporary API implementation; not suitable for production!!!");
-
-        function getRequests() {
-          return {
-            async get<T>(url: string, params = {}): Promise<RequestResponse<any>> {
-              let error = null;
-              const response = await $axios.get<T>(url, params).catch((e) => {
-                error = e;
-              });
-              if (response != null) {
-                return { response, error, data: response?.data };
-              }
-              return { response: null, error, data: null };
-            }
-          };
-        }
-
-        const url = `/api/recipes/${props.slug}/timeline/events?orderBy=timestamp&orderDirection=asc&perPage=-1`
-        const response = await getRequests().get(url)
-        timelineEvents.value = response.data.items;
-      };
-
-      // preload events
-      refreshTimelineEvents();
-
-      return {
-        chooseEventIcon,
-        dialog,
-        timelineEvents,
-      };
+    slug: {
+      type: String,
+      default: "",
     },
-  });
-  </script>
+    recipeName: {
+      type: String,
+      default: "",
+    },
+  },
+
+  setup(props, context) {
+    const api = useUserApi();
+    const { $globals, $vuetify } = useContext();
+    const timelineEvents = ref([{}] as RecipeTimelineEventOut[])
+
+    const useMobileFormat = computed(() => {
+      return $vuetify.breakpoint.smAndDown;
+    });
+
+    // V-Model Support
+    const dialog = computed({
+      get: () => {
+        return props.value;
+      },
+      set: (val) => {
+        context.emit("input", val);
+      },
+    });
+
+    whenever(
+      () => props.value,
+      () => {
+        refreshTimelineEvents();
+      }
+    );
+
+    function chooseEventIcon(event: RecipeTimelineEventOut) {
+      switch (event.eventType) {
+        case "comment":
+          return $globals.icons.commentTextMultiple;
+
+        case "info":
+          return $globals.icons.informationVariant;
+
+        case "system":
+          return $globals.icons.cog;
+
+        default:
+          return $globals.icons.informationVariant;
+      };
+    };
+
+    async function refreshTimelineEvents() {
+      // TODO: implement infinite scroll and paginate instead of loading all events at once
+      const page = 1;
+      const perPage = -1;
+      const orderBy = "timestamp";
+      const orderDirection = "asc";
+
+      const response = await api.recipes.getAllTimelineEvents(props.slug, page, perPage, { orderBy, orderDirection })
+      if (!response?.data) {
+        return;
+      }
+
+      timelineEvents.value = response.data.items;
+    };
+
+    // preload events
+    refreshTimelineEvents();
+
+    return {
+      chooseEventIcon,
+      dialog,
+      timelineEvents,
+      useMobileFormat,
+    };
+  },
+});
+</script>
