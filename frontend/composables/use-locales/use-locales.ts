5568fdc01f40562ea2cffa08cc1513472100229a
sephrat <34862846+sephrat@users.noreply.github.com>
2023-01-18 16:51:46 +0100

    feat(lang): an attempt at localizing vuetify (WIP)

--- frontend/composables/use-locales/use-locales.ts
+++ frontend/composables/use-locales/use-locales.ts
@@ -2,7 +2,7 @@ import { computed, useContext } from "@nuxtjs/composition-api";
 import { LOCALES } from "./available-locales";
 
 export const useLocales = () => {
-  const { i18n } = useContext();
+  const { i18n, $vuetify } = useContext();
 
   const locale = computed<string>({
     get() {
@@ -10,6 +10,9 @@ export const useLocales = () => {
     },
     set(value) {
       i18n.setLocale(value);
+      // TODO: set vuetify language.
+      // $vuetify.lang.current = value; // this does not persist after window reload
+
       // Reload the page to update the language - not all strings are reactive
       window.location.reload();
     },
