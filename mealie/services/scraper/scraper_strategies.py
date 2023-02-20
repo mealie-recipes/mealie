05e2566c35d9f1877c9fe32949deb6810b2f36ac
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-02-19 18:43:52 -0600

    fix: recipe scraper image cleaning (#2139)
    * updated image cleaner
    enabled image cleaner
    added case for nested image dicts
    
    * refactored image cleaner to return a list of urls

--- mealie/services/scraper/scraper_strategies.py
+++ mealie/services/scraper/scraper_strategies.py
@@ -150,7 +150,7 @@ class RecipeScraperPackage(ABCScraperStrategy):
         recipe = Recipe(
             name=try_get_default(scraped_data.title, "name", "No Name Found", cleaner.clean_string),
             slug="",
-            image=try_get_default(None, "image", None),
+            image=try_get_default(None, "image", None, cleaner.clean_image),
             description=try_get_default(None, "description", "", cleaner.clean_string),
             nutrition=try_get_default(None, "nutrition", None, cleaner.clean_nutrition),
             recipe_yield=try_get_default(scraped_data.yields, "recipeYield", "1", cleaner.clean_string),
