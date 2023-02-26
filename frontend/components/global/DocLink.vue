5ad1ca63c6aba5248dfc15c5fdda09b0c0ef1b4c
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-02-26 11:51:31 -0900

    update inline docs on group page to clarify private/public settings

---
+++ frontend/components/global/DocLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-btn x-small :href="href" color="primary" target="_blank">
+    <v-icon left small>
+      {{ $globals.icons.folderOutline }}
+    </v-icon>
+    {{ $tc("about.docs") }}
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const href = computed(() => {
+      // TODO: dynamically set docs link based off env
+      return `https://nightly.mealie.io${props.link}`;
+    });
+
+    return { href };
+  },
+});
+</script>
