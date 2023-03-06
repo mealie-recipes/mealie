3144ab3468b1f10f1c636c54a580cbb70025b3ff
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-03-06 17:46:40 +0000

    switched recipe create to service added test for timeline event creation

--- tests/integration_tests/recipe_migration_tests/test_recipe_migrations.py
+++ tests/integration_tests/recipe_migration_tests/test_recipe_migrations.py
@@ -7,6 +7,7 @@ from fastapi.testclient import TestClient
 from mealie.schema.group.group_migration import SupportedMigrations
 from tests import data as test_data
 from tests.utils import api_routes
+from tests.utils.assertion_helpers import assert_derserialize
 from tests.utils.fixture_schemas import TestUser
 
 
@@ -58,3 +59,15 @@ def test_recipe_migration(api_client: TestClient, unique_user: TestUser, mig: Mi
 
     for item in response.json()["entries"]:
         assert item["success"]
+
+    # Validate Create Event
+    params = {"orderBy": "created_at", "orderDirection": "desc"}
+    response = api_client.get(api_routes.recipes, params=params, headers=unique_user.token)
+    query_data = assert_derserialize(response)
+    assert len(query_data["items"])
+    slug = query_data["items"][0]["slug"]
+
+    response = api_client.get(api_routes.recipes_slug_timeline_events(slug), headers=unique_user.token)
+    query_data = assert_derserialize(response)
+    events = query_data["items"]
+    assert len(events)
