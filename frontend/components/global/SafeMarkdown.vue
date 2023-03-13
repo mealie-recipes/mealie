a809fd09d9e5ce3c920d666032cce6a83606238b
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-03-13 21:32:27 +0000

    expanded safe html tags and attrs

--- frontend/components/global/SafeMarkdown.vue
+++ frontend/components/global/SafeMarkdown.vue
@@ -26,8 +26,16 @@ export default defineComponent({
 
       const sanitized = DOMPurify.sanitize(rawHtml, {
         // TODO: some more thought could be put into what is allowed and what isn't
-        ALLOWED_TAGS: ["img", "div", "p", "iframe"],
-        ADD_ATTR: ["src", "alt", "height", "width", "class", "allow", "title", "allowfullscreen", "frameborder", "scrolling"],
+        ALLOWED_TAGS: [
+                        "strong", "em", "b", "i", "u", "p", "code", "pre", "samp", "kbd", "var", "sub", "sup", "dfn", "cite",
+                        "small", "address", "hr", "br", "id", "div", "span", "h1", "h2", "h3", "h4", "h5", "h6",
+                        "ul", "ol", "li", "dl", "dt", "dd", "abbr", "a", "img", "blockquote", "iframe",
+                        "del", "ins", "table", "thead", "tbody", "tfoot", "tr", "th", "td", "colgroup",
+                      ],
+        ADD_ATTR: [
+                    "href", "src", "alt", "height", "width", "class", "allow", "title", "allowfullscreen", "frameborder",
+                    "scrolling", "style", "cite", "datetime", "name", "abbr", "target", "border",
+                  ],
       });
 
       return sanitized;
