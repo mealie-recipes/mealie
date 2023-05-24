430fc3162cc2d00d67b157110dde7b643a44ab21
Grygon <647846+Grygon@users.noreply.github.com>
2023-05-24 03:57:23 +0000

    Implement vertical scrolling

--- frontend/pages/group/mealplan/planner/view.vue
+++ frontend/pages/group/mealplan/planner/view.vue
@@ -1,45 +1,47 @@
 <template>
-  <div id="mealplan-container" ref="mealplan-container">
-    <button class="scroll-arrow scroll-prev" @click="scrollToNext(false)">&lt;&lt;&lt;</button>
+  <div id="mealplan-container" ref="mealplan-container" :class="{ 'flex-column': useMobileFormat, 'flex-row': !useMobileFormat  }">
+    <button v-if="useMobileFormat" class="scroll-arrow scroll-up" @click="scrollVert(false)">Up</button>
+    <button v-else class="scroll-arrow scroll-left" @click="scrollHoriz(false)">&lt;&lt;&lt;</button>
     <v-col
-      v-for="(day, index) in plan"
-      ref="days"
-      :key="index"
-      cols="12"
-      sm="12"
-      md="4"
-      lg="4"
-      xl="2"
-      class="col-borders my-1 d-flex flex-column"
+    v-for="(day, index) in plan"
+    ref="days"
+    :key="index"
+    cols="12"
+    sm="12"
+    md="4"
+    lg="4"
+    xl="2"
+    class="col-borders my-1 d-flex flex-column"
     >
-      <v-card class="mb-2 border-left-primary rounded-sm pa-2">
-        <p class="pl-2 mb-1">
-          {{ $d(day.date, "short") }}
+    <v-card class="mb-2 border-left-primary rounded-sm pa-2">
+      <p class="pl-2 mb-1">
+        {{ $d(day.date, "short") }}
+      </p>
+    </v-card>
+    <div v-for="section in day.sections" :key="section.title">
+      <div class="py-2 d-flex flex-column">
+        <div class="primary" style="width: 50px; height: 2.5px"></div>
+        <p class="text-overline my-0">
+          {{ section.title }}
         </p>
-      </v-card>
-      <div v-for="section in day.sections" :key="section.title">
-        <div class="py-2 d-flex flex-column">
-          <div class="primary" style="width: 50px; height: 2.5px"></div>
-          <p class="text-overline my-0">
-            {{ section.title }}
-          </p>
-        </div>
-
-        <RecipeCardMobile
-          v-for="mealplan in section.meals"
-          :key="mealplan.id"
-          :recipe-id="(mealplan.recipe && mealplan.recipe.id) ? mealplan.recipe.id : ''"
-          class="mb-2"
-          :route="mealplan.recipe ? true : false"
-          :rating="mealplan.recipe ? mealplan.recipe.rating : 0"
-          :slug="(mealplan.recipe ? mealplan.recipe.slug : mealplan.title) || 'Recipe'"
-          :description="(mealplan.recipe ? mealplan.recipe.description : mealplan.text) || 'No Description'"
-          :name="(mealplan.recipe ? mealplan.recipe.name : mealplan.title) || 'Recipe'"
-        />
       </div>
-    </v-col>
-    <button class="scroll-arrow scroll-next" @click="scrollToNext(true)">&gt;&gt;&gt;</button>
-  </div>
+
+      <RecipeCardMobile
+      v-for="mealplan in section.meals"
+      :key="mealplan.id"
+      :recipe-id="(mealplan.recipe && mealplan.recipe.id) ? mealplan.recipe.id : ''"
+      class="mb-2"
+      :route="mealplan.recipe ? true : false"
+      :rating="mealplan.recipe ? mealplan.recipe.rating : 0"
+      :slug="(mealplan.recipe ? mealplan.recipe.slug : mealplan.title) || 'Recipe'"
+      :description="(mealplan.recipe ? mealplan.recipe.description : mealplan.text) || 'No Description'"
+      :name="(mealplan.recipe ? mealplan.recipe.name : mealplan.title) || 'Recipe'"
+      />
+    </div>
+  </v-col>
+  <button v-if="useMobileFormat" class="scroll-arrow scroll-down" @click="scrollVert(true)">Down</button>
+  <button v-else class="scroll-arrow scroll-right" @click="scrollHoriz(true)">&gt;&gt;&gt;</button>
+</div>
 </template>
 
 
@@ -70,17 +72,21 @@ export default defineComponent({
       sections: DaySection[];
     };
 
-  const { i18n } = useContext();
+    const { $vuetify } = useContext();
+    const { i18n } = useContext();
+    const useMobileFormat = computed(() => {
+      return $vuetify.breakpoint.smAndDown;
+    });
 
     const plan = computed<Days[]>(() => {
       return props.mealplans.reduce((acc, day) => {
         const out: Days = {
           date: day.date,
           sections: [
-            { title: i18n.tc("meal-plan.breakfast"), meals: [] },
-            { title: i18n.tc("meal-plan.lunch"), meals: [] },
-            { title: i18n.tc("meal-plan.dinner"), meals: [] },
-            { title: i18n.tc("meal-plan.side"), meals: [] },
+          { title: i18n.tc("meal-plan.breakfast"), meals: [] },
+          { title: i18n.tc("meal-plan.lunch"), meals: [] },
+          { title: i18n.tc("meal-plan.dinner"), meals: [] },
+          { title: i18n.tc("meal-plan.side"), meals: [] },
           ],
         };
 
@@ -105,28 +111,62 @@ export default defineComponent({
       }, [] as Days[]);
     });
 
+
     return {
       plan,
+      useMobileFormat,
     };
   },
   methods: {
-    scrollToNext(forward: boolean) {
+    scrollHoriz(forward?: boolean) {
       if(this.$refs.days && this.$refs["mealplan-container"]) {
         const daysRefs = this.$refs.days as HTMLDivElement[];
         const containerRef = this.$refs["mealplan-container"] as HTMLDivElement
-        const containerOffset = containerRef.offsetLeft;
-        let scrollLeft: number[] = daysRefs.map((day) => day.offsetLeft - containerRef.scrollLeft - containerOffset);
+
+        const getOffset = (el: HTMLDivElement) => el.offsetLeft;
+
+        const getScroll = (el: HTMLDivElement) => el.scrollLeft;
+
+        const containerOffset = getOffset(containerRef);
+        let scrollDist: number[] = daysRefs.map((day) => getOffset(day) - getScroll(containerRef) - containerOffset);
+
         if(forward) {
-          scrollLeft = scrollLeft.filter(n => n > 0);
-        scrollLeft.sort((a,b) => a-b);
+          scrollDist = scrollDist.filter(n => n > 0);
+          scrollDist.sort((a,b) => a-b);
         } else {
-          scrollLeft = scrollLeft.filter(n => n < 0);
-        scrollLeft.sort((a,b) => b-a);
+          scrollDist = scrollDist.filter(n => n < 0);
+          scrollDist.sort((a,b) => b-a);
         }
+
         containerRef.scrollTo({
-          left: scrollLeft[0] + containerRef.scrollLeft ,
+          left: scrollDist[0] + getScroll(containerRef) ,
           behavior: "smooth"
-        });;
+        });
+      } else {
+        console.log("Unable to perform scrolling due to missing ref")
+      }
+    },
+    scrollVert(forward?: boolean) {
+      if(this.$refs.days && this.$refs["mealplan-container"]) {
+        const daysRefs = this.$refs.days as HTMLDivElement[];
+        const containerRef = this.$refs["mealplan-container"] as HTMLDivElement
+
+        const getOffset = (el: HTMLDivElement) => el.offsetTop ;
+
+        let scrollDist: number[] = daysRefs.map((day) => getOffset(day) - window.scrollY);
+
+        if(forward) {
+          scrollDist = scrollDist.filter(n => n > 0);
+          scrollDist.sort((a,b) => a-b);
+        } else {
+          scrollDist = scrollDist.filter(n => n < 0);
+          scrollDist.sort((a,b) => b-a);
+        }
+
+        window.scrollTo({
+          top: scrollDist[0] + window.scrollY ,
+          behavior: "smooth"
+        });
       } else {
         console.log("Unable to perform scrolling due to missing ref")
       }
@@ -138,22 +178,34 @@ export default defineComponent({
 <style>
 #mealplan-container {
   display: flex;
-  flex-direction: row;
   margin: -12px 12px -12px 12px;
   overflow: auto;
 }
 
-.scroll-arrow {
+.scroll-right {
   position: absolute;
   top: 200px;
+  right: 5px;
 }
 
-.scroll-next {
-  right: 5px
+.scroll-left {
+  position: absolute;
+  top: 200px;
+  left: 0px;
 }
 
-.scroll-prev {
-  left: 0px
+.scroll-up {
+  position: fixed;
+  top: 50px;
+  align-self: center;
+  z-index: 999;
+}
+
+.scroll-down {
+  position: fixed;
+  bottom: 0px;
+  align-self: center;
+  z-index: 999;
 }
 
 </style>
