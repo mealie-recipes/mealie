4350a16d7c1b17775f710eaaaac86309db86fb4c
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-08-21 17:33:09 +0000

    fixed alembic refs

--- alembic/versions/2023-08-15-16.25.07_bcfdad6b7355_remove_tool_name_and_slug_unique_.py
+++ alembic/versions/2023-08-15-16.25.07_bcfdad6b7355_remove_tool_name_and_slug_unique_.py
@@ -1,7 +1,7 @@
 """remove tool name and slug unique contraints
 
 Revision ID: bcfdad6b7355
-Revises: b3dbb554ba53
+Revises: 1825b5225403
 Create Date: 2023-08-15 16:25:07.058929
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "bcfdad6b7355"
-down_revision = "b3dbb554ba53"
+down_revision = "1825b5225403"
 branch_labels = None
 depends_on = None
 
