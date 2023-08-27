a6943f764028033fa0d3734f4d85356bf743491a
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-08-27 21:57:14 +0000

    added backend routes for public cookbooks

--- mealie/routes/explore/__init__.py
+++ mealie/routes/explore/__init__.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
 
-from . import controller_public_foods, controller_public_organizers, controller_public_recipes
+from . import (
+    controller_public_cookbooks,
+    controller_public_foods,
+    controller_public_organizers,
+    controller_public_recipes,
+)
 
 prefix = "/explore"
 
 router = APIRouter()
 
+router.include_router(controller_public_cookbooks.router, prefix=prefix, tags=["Explore: Cookbooks"])
 router.include_router(controller_public_foods.router, prefix=prefix, tags=["Explore: Foods"])
 router.include_router(controller_public_organizers.categories_router, prefix=prefix, tags=["Explore: Categories"])
 router.include_router(controller_public_organizers.tags_router, prefix=prefix, tags=["Explore: Tags"])
