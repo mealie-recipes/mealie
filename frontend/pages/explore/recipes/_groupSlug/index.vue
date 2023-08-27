6f64bcc252f9e3b78c283323a029f2d9ca07cef6
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-08-27 03:53:44 +0000

    force a 404 if the group slug is missing or invalid

--- frontend/pages/explore/recipes/_groupSlug/index.vue
+++ frontend/pages/explore/recipes/_groupSlug/index.vue
@@ -7,15 +7,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, useRoute } from "@nuxtjs/composition-api";
+import { defineComponent, useRoute, useRouter } from "@nuxtjs/composition-api";
+import { invoke } from "@vueuse/core";
 import RecipeExplorerPage from "~/components/Domain/Recipe/RecipeExplorerPage.vue";
+import { usePublicExploreApi } from "~/composables/api/api-client";
 
 export default defineComponent({
   components: { RecipeExplorerPage },
   layout: "basic",
   setup() {
     const route = useRoute();
-    const groupSlug = route.value.params.groupSlug;
+    const router = useRouter();
+    let groupSlug = route.value.params.groupSlug;
+    const api = usePublicExploreApi(groupSlug);
+
+    invoke(async () => {
+      if (!groupSlug) {
+        return;
+      }
+
+      // try to fetch one tag to make sure the group slug is valid
+      const { data } = await api.explore.tags.getAll(1, 1);
+      if (!data) {
+        // the group slug is invalid, so leave the page (this results in a 404)
+        router.push("/explore/recipes");
+      }
+    });
 
     return {
       groupSlug,
