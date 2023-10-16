4b04137a048431e3d0d62779a0ec401e5084f9ac
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-10-15 19:49:32 -0500

    fix: attempted Vuetify dark mode fix (#2638)

--- frontend/plugins/dark-mode.client.ts
+++ frontend/plugins/dark-mode.client.ts
@@ -5,9 +5,9 @@ const darkModePlugin: Plugin = ({ $vuetify }, _) => {
   const isDark = useDark();
 
   // Vuetify metadata is bugged and doesn't render dark mode fully when called immediately
-  // Adding a 100 millisecond delay fixes this problem
+  // Adding a delay fixes this problem
   // https://stackoverflow.com/questions/69399797/vuetify-darkmode-colors-wrong-after-page-reload
-  setTimeout(() => { $vuetify.theme.dark = isDark.value; }, 100);
+  setTimeout(() => { $vuetify.theme.dark = isDark.value; }, 200);
 };
 
 export default darkModePlugin;
