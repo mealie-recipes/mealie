d1219be79f78401f42e4e47de5d4898150d9a834
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-11-29 02:34:27 +0000

    redirect public user to default group

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,17 +4,28 @@
 
 <script lang="ts">
 import { computed, defineComponent, useContext, useRouter } from "@nuxtjs/composition-api";
+import { AppInfo } from "~/lib/api/types/admin";
+
 export default defineComponent({
   layout: "blank",
   setup() {
-    const { $auth } = useContext();
+    const { $auth, $axios } = useContext();
     const router = useRouter();
     const groupSlug = computed(() => $auth.user?.groupSlug);
+
+    async function redirectPublicUserToDefaultGroup() {
+      const { data } = await $axios.get<AppInfo>("/api/app/about");
+      if (data?.defaultGroupSlug) {
+        router.push(`/g/${data.defaultGroupSlug}`);
+      } else {
+        router.push("/login");
+      }
+    }
 
     if (groupSlug.value) {
       router.push(`/g/${groupSlug.value}`);
     } else {
-      router.push("/login");
+      redirectPublicUserToDefaultGroup();
     }
   }
 });
