aa3d2972d932b0c9f08874c2add7a75a3104c4db
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-12-11 15:05:48 +0000

    use id as default name to avoid collisions

--- mealie/db/fixes/fix_group_with_no_name.py
+++ mealie/db/fixes/fix_group_with_no_name.py
@@ -6,14 +6,13 @@ from mealie.core import root_logger
 from mealie.db.models.group import Group
 
 logger = root_logger.get_logger("init_db")
-DEFAULT_GROUP_NAME_BASE = "Group"
 
 
 def _do_fix(session: Session, group: Group, counter: int):
     if counter:
-        new_name = f"{DEFAULT_GROUP_NAME_BASE} ({counter})"
+        new_name = f"{group.id} ({counter})"
     else:
-        new_name = DEFAULT_GROUP_NAME_BASE
+        new_name = str(group.id)
 
     group.name = new_name
     group.slug = slugify(group.name)
@@ -28,7 +27,7 @@ def fix_group_with_no_name(session: Session):
 
     logger.info(
         f'{len(groups)} {"group" if len(groups) == 1 else "groups"} found with a missing name; '
-        f'applying default name "{DEFAULT_GROUP_NAME_BASE}"'
+        f"applying default name"
     )
 
     offset = 0
