48325f923154cdcb587b92cf4d7d0db67cdc871c
Carter Mintey <cmintey8@gmail.com>
2023-12-17 01:15:02 +0000

    e2e test setup

---
+++ tests/e2e/login.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('oidc-login', async ({ page }) => {
+    const claims = {
+        "sub": "testUser",
+        "email": "test@example.com",
+        "preferred_username": "test",
+        "name": "Test User"
+    }
+
+    await page.goto('http://localhost:9000/login');
+    await page.getByRole('button', { name: 'Login with OIDC' }).click();
+    await page.getByPlaceholder('Enter any user/subject').fill('testUser');
+    await page.getByPlaceholder('Optional claims JSON value,').fill(JSON.stringify(claims));
+    await page.getByRole('button', { name: 'Sign-in' }).click();
+    await expect(page.getByRole('button', { name: 'Create', exact: true })).toBeVisible();
+});
