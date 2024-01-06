265313919c0dddfb19dc6deda4293920a4163d3a
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2024-01-06 21:51:35 +0000

    Vue-ify and add documentation

--- frontend/composables/use-navigation-warning.ts
+++ frontend/composables/use-navigation-warning.ts
@@ -1,7 +1,22 @@
-export const activateNavigationWarning = () => {
+export function useNavigationWarning() {
+    return { activateNavigationWarning, deactivateNavigationWarning };
+}
+
+/**
+ * Displays a warning before the user navigates to another page
+ * e.g., by clicking a link (which isn't internal and rendered without page load),
+ * reloading the page,
+ * or closing the tab.
+ */
+const activateNavigationWarning = () => {
+    console.log("ACTIVATE");
     window.onbeforeunload = () => true;
 }
 
-export const deactivateNavigationWarning = () => {
+/**
+ * Disables the warning when navigating to a page
+ */
+const deactivateNavigationWarning = () => {
+    console.log("deACTIVATE");
     window.onbeforeunload = null;
 }
