9cc59e81d6e6d8398a6b7832cce4e00b8b215dd3
Kuchenpirat <24235032+Kuchenpirat@users.noreply.github.com>
2024-02-02 16:41:31 +0000

    add comment to group-only

--- frontend/middleware/group-only.ts
+++ frontend/middleware/group-only.ts
@@ -5,6 +5,7 @@ interface GroupOnlyRedirectParams {
 }
 
 export default function ({ $auth, route, redirect }: GroupOnlyRedirectParams) {
+  // this can only be used for routes that have a groupSlug parameter (e.g. /g/:groupSlug/...)
   if (route.params.groupSlug !== $auth.user.groupSlug) {
     redirect("/")
   }
