d83e2c4c380d7a7122af0014fb7ed322fc463d63
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-02-04 10:20:31 -0600

    fix types for text

--- frontend/components/global/CrudTable.vue
+++ frontend/components/global/CrudTable.vue
@@ -57,12 +57,12 @@
           :buttons="[
             {
               icon: $globals.icons.edit,
-              text: $t('general.edit'),
+              text: $tc('general.edit'),
               event: 'edit',
             },
             {
               icon: $globals.icons.delete,
-              text: $t('general.delete'),
+              text: $tc('general.delete'),
               event: 'delete',
             },
           ]"
