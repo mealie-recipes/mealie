f42114e96676d35b6dc59e3cfa80a655b964fe11
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-02-07 10:53:55 -0600

    feat: Redirect Logged Out Users to Default Group, If It's Public (#2772)
    * add default group slug to app info if public
    
    * redirect public user to default group
    
    * added tests
    
    ---------
    
    Co-authored-by: Kuchenpirat <24235032+Kuchenpirat@users.noreply.github.com>

--- mealie/routes/app/app_about.py
+++ mealie/routes/app/app_about.py
@@ -5,21 +5,30 @@ from mealie.core.config import get_app_settings
 from mealie.core.settings.static import APP_VERSION
 from mealie.db.db_setup import generate_session
 from mealie.db.models.users.users import User
+from mealie.repos.all_repositories import get_repositories
 from mealie.schema.admin.about import AppInfo, AppStartupInfo, AppTheme
 
 router = APIRouter(prefix="/about")
 
 
 @router.get("", response_model=AppInfo)
-def get_app_info():
+def get_app_info(session: Session = Depends(generate_session)):
     """Get general application information"""
     settings = get_app_settings()
+
+    repos = get_repositories(session)
+    default_group = repos.groups.get_by_name(settings.DEFAULT_GROUP)
+    if default_group and default_group.preferences and not default_group.preferences.private_group:
+        default_group_slug = default_group.slug
+    else:
+        default_group_slug = None
 
     return AppInfo(
         version=APP_VERSION,
         demo_status=settings.IS_DEMO,
         production=settings.PRODUCTION,
         allow_signup=settings.ALLOW_SIGNUP,
+        default_group_slug=default_group_slug,
     )
 
 
