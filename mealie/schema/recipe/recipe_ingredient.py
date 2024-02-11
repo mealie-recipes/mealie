9082289e3fa82654d7e0cd04289a21f8aab0ae68
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-02-11 21:28:45 +0000

    fixed serialization dropping food and unit ids

--- mealie/schema/recipe/recipe_ingredient.py
+++ mealie/schema/recipe/recipe_ingredient.py
@@ -266,6 +266,9 @@ class RecipeIngredient(RecipeIngredientBase):
     original_text: str | None = None
     disable_amount: bool = True
 
+    unit: IngredientUnit | None = None
+    food: IngredientFood | None = None
+
     # Ref is used as a way to distinguish between an individual ingredient on the frontend
     # It is required for the reorder and section titles to function properly because of how
     # Vue handles reactivity. ref may serve another purpose in the future.
