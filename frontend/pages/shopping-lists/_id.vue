a47a15273ef2ed260ead81fb99effb951f796a7b
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-04-16 15:37:16 +0000

    preserve item order if the user re-orders something

--- frontend/pages/shopping-lists/_id.vue
+++ frontend/pages/shopping-lists/_id.vue
@@ -279,6 +279,7 @@ export default defineComponent({
     const edit = ref(false);
     const reorderLabelsDialog = ref(false);
     const settingsDialog = ref(false);
+    const preserveItemOrder = ref(false);
 
     const route = useRoute();
     const groupSlug = computed(() => route.value.params.groupSlug || $auth.user?.groupSlug || "");
@@ -299,8 +300,19 @@ export default defineComponent({
       loadingCounter.value -= 1;
 
       // only update the list with the new value if we're not loading, to prevent UI jitter
-      if (!loadingCounter.value) {
-        shoppingList.value = newListValue;
+      if (loadingCounter.value) {
+        return;
+      }
+
+      shoppingList.value = newListValue;
+      updateListItemOrder();
+    }
+
+    function updateListItemOrder() {
+      if (!preserveItemOrder.value) {
+        groupAndSortListItemsByFood();
+        updateItemsByLabel();
+      } else {
         sortListItems();
         updateItemsByLabel();
       }
@@ -515,7 +527,7 @@ export default defineComponent({
 
       // setting this doesn't have any effect on the data since it's refreshed automatically, but it makes the ux feel smoother
       shoppingList.value.labelSettings = labelSettings;
-      updateItemsByLabel();
+      updateListItemOrder();
 
       loadingCounter.value += 1;
       const { data } = await userApi.shopping.lists.updateLabelSettings(shoppingList.value.id, labelSettings);
@@ -549,7 +561,7 @@ export default defineComponent({
       items: ShoppingListItemOut[];
     }
 
-    function sortListItems() {
+    function groupAndSortListItemsByFood() {
       if (!shoppingList.value?.listItems?.length) {
         return;
       }
@@ -590,6 +602,16 @@ export default defineComponent({
       });
 
       shoppingList.value.listItems = sortedItems;
+    }
+
+    function sortListItems() {
+      if (!shoppingList.value?.listItems?.length) {
+        return;
+      }
+
+      // sort by position ascending, then createdAt descending
+      // @ts-ignore none of these fields are undefined
+      shoppingList.value.listItems.sort((a, b) => (a.position > b.position || a.createdAt < b.createdAt ? 1 : -1))
     }
 
     function updateItemsByLabel() {
@@ -722,8 +744,7 @@ export default defineComponent({
         });
       }
 
-      sortListItems();
-      updateItemsByLabel();
+      updateListItemOrder();
 
       loadingCounter.value += 1;
       const { data } = await userApi.shopping.items.updateOne(item.id, item);
@@ -799,6 +820,9 @@ export default defineComponent({
         shoppingList.value.listItems = uncheckedItems.concat(listItems.value.checked);
       }
 
+      // since the user has manually reordered the list, we should preserve this order
+      preserveItemOrder.value = true;
+
       updateListItems();
     }
 
@@ -815,6 +839,9 @@ export default defineComponent({
       for (labelName in itemsByLabel.value) {
         allUncheckedItems.push(...itemsByLabel.value[labelName]);
       }
+
+      // since the user has manually reordered the list, we should preserve this order
+      preserveItemOrder.value = true;
 
       // save changes
       return updateIndexUnchecked(allUncheckedItems);
