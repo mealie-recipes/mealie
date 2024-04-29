d2f60b6fc61614185fcf59e6185f4af30bd37872
Nathan Easton <nathaneaston@nathaneaston.com>
2024-04-28 20:22:41 -0400

    Update user-agent

--- mealie/services/recipe/recipe_data_service.py
+++ mealie/services/recipe/recipe_data_service.py
@@ -10,7 +10,7 @@ from mealie.pkgs.safehttp.transport import AsyncSafeTransport
 from mealie.schema.recipe.recipe import Recipe
 from mealie.services._base_service import BaseService
 
-_FIREFOX_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"
+_FIREFOX_UA = ("Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0")
 
 
 async def gather_with_concurrency(n, *coros, ignore_exceptions=False):
--- mealie/services/scraper/scraper_strategies.py
+++ mealie/services/scraper/scraper_strategies.py
@@ -18,7 +18,7 @@ from mealie.services.scraper.scraped_extras import ScrapedExtras
 
 from . import cleaner
 
-_FIREFOX_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"
+_FIREFOX_UA = "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0"
 SCRAPER_TIMEOUT = 15
 
 
