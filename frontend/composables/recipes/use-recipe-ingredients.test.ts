9f7d74aecf0e87ecbdb8760bbceebc6f4f5d5dbe
Bart Kummel <bart@leap.ac>
2024-06-13 15:04:45 +0200

    fix: Make fractions in the ingredient list look a tiny bit nicer (#3725)

--- frontend/composables/recipes/use-recipe-ingredients.test.ts
+++ frontend/composables/recipes/use-recipe-ingredients.test.ts
@@ -31,13 +31,13 @@ describe(parseIngredientText.name, () => {
   test("ingredient text with fraction", () => {
     const ingredient = createRecipeIngredient({ quantity: 1.5, unit: { fraction: true, id: "1", name: "cup" } });
 
-    expect(parseIngredientText(ingredient, false, 1, true)).contain("1 <sup>1</sup>").and.to.contain("<sub>2</sub>");
+    expect(parseIngredientText(ingredient, false, 1, true)).contain("1<sup>1</sup>").and.to.contain("<sub>2</sub>");
   });
 
   test("ingredient text with fraction when unit is null", () => {
     const ingredient = createRecipeIngredient({ quantity: 1.5, unit: undefined });
 
-    expect(parseIngredientText(ingredient, false, 1, true)).contain("1 <sup>1</sup>").and.to.contain("<sub>2</sub>");
+    expect(parseIngredientText(ingredient, false, 1, true)).contain("1<sup>1</sup>").and.to.contain("<sub>2</sub>");
   });
 
   test("ingredient text with fraction no formatting", () => {
