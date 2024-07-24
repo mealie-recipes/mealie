370ef03e5f322bbc6ef6f1068180ebb59217c8da
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-24 20:20:53 +0000

    re-parametrized tests to use households

--- tests/integration_tests/public_explorer_tests/test_public_recipes.py
+++ tests/integration_tests/public_explorer_tests/test_public_recipes.py
@@ -1,5 +1,4 @@
 import random
-from dataclasses import dataclass
 from typing import Any
 
 import pytest
@@ -11,19 +10,13 @@ from tests.utils.factories import random_int, random_string
 from tests.utils.fixture_schemas import TestUser
 
 
-@dataclass(slots=True)
-class PublicRecipeTestCase:
-    private_group: bool
-    public_recipe: bool
-    status_code: int
-    error: str | None
-
-
-@pytest.mark.parametrize("is_private_group", [True, False], ids=["group_is_private", "group_is_public"])
+@pytest.mark.parametrize("is_private_group", [True, False])
+@pytest.mark.parametrize("is_private_household", [True, False])
 def test_get_all_public_recipes(
     api_client: TestClient,
     unique_user: TestUser,
     is_private_group: bool,
+    is_private_household: bool,
 ):
     database = unique_user.repos
 
@@ -32,13 +25,20 @@ def test_get_all_public_recipes(
     assert group and group.preferences
 
     group.preferences.private_group = is_private_group
-    group.preferences.recipe_public = not is_private_group
     database.group_preferences.update(group.id, group.preferences)
+
+    household = database.households.get_one(unique_user.household_id)
+    assert household and household.preferences
+
+    household.preferences.private_household = is_private_household
+    household.preferences.recipe_public = not is_private_household
+    database.household_preferences.update(household.id, household.preferences)
 
     default_recipes = database.recipes.create_many(
         [
             Recipe(
                 user_id=unique_user.user_id,
+                household_id=unique_user.household_id,
                 group_id=unique_user.group_id,
                 name=random_string(),
             )
@@ -62,8 +62,10 @@ def test_get_all_public_recipes(
     database.recipes.update_many(public_recipes + private_recipes)
 
     ## Query All Recipes
-    response = api_client.get(api_routes.explore_recipes_group_slug(group.slug))
-    if is_private_group:
+    response = api_client.get(
+        api_routes.explore_groups_group_slug_households_household_slug_recipes(group.slug, household.slug)
+    )
+    if is_private_group or is_private_household:
         assert response.status_code == 404
         return
 
@@ -108,63 +110,84 @@ def test_get_all_public_recipes_filtered(
     assert group and group.preferences
 
     group.preferences.private_group = False
-    group.preferences.recipe_public = True
     database.group_preferences.update(group.id, group.preferences)
+
+    household = database.households.get_one(unique_user.household_id)
+    assert household and household.preferences
+
+    household.preferences.private_household = False
+    household.preferences.recipe_public = True
+    database.household_preferences.update(household.id, household.preferences)
 
     assert random_recipe.settings
     random_recipe.settings.public = True
     database.recipes.update(random_recipe.slug, random_recipe.model_dump() | recipe_data)
 
     ## Query All Recipes
-    response = api_client.get(api_routes.explore_recipes_group_slug(group.slug), params={"queryFilter": query_filter})
+    response = api_client.get(
+        api_routes.explore_groups_group_slug_households_household_slug_recipes(group.slug, household.slug),
+        params={"queryFilter": query_filter},
+    )
     assert response.status_code == 200
     recipes_data = response.json()
     fetched_ids: set[str] = {recipe["id"] for recipe in recipes_data["items"]}
     assert should_fetch is (str(random_recipe.id) in fetched_ids)
 
 
-@pytest.mark.parametrize(
-    "test_case",
-    (
-        PublicRecipeTestCase(private_group=False, public_recipe=True, status_code=200, error=None),
-        PublicRecipeTestCase(private_group=True, public_recipe=True, status_code=404, error="group not found"),
-        PublicRecipeTestCase(private_group=False, public_recipe=False, status_code=404, error="recipe not found"),
-    ),
-    ids=("is public", "group private", "recipe private"),
-)
+@pytest.mark.parametrize("is_private_group", [True, False])
+@pytest.mark.parametrize("is_private_household", [True, False])
+@pytest.mark.parametrize("is_private_recipe", [True, False])
 def test_public_recipe_success(
     api_client: TestClient,
     unique_user: TestUser,
     random_recipe: Recipe,
-    test_case: PublicRecipeTestCase,
+    is_private_group: bool,
+    is_private_household: bool,
+    is_private_recipe: bool,
 ):
     database = unique_user.repos
 
+    ## Set Up Group
     group = database.groups.get_one(unique_user.group_id)
     assert group and group.preferences
 
-    group.preferences.private_group = test_case.private_group
-    group.preferences.recipe_public = not test_case.private_group
+    group.preferences.private_group = is_private_group
     database.group_preferences.update(group.id, group.preferences)
+
+    ## Set Up Household
+    household = database.households.get_one(unique_user.household_id)
+    assert household and household.preferences
+
+    household.preferences.private_household = is_private_household
+    household.preferences.recipe_public = not is_private_household
+    database.household_preferences.update(household.id, household.preferences)
 
     # Set Recipe `settings.public` attribute
     assert random_recipe.settings
-    random_recipe.settings.public = test_case.public_recipe
+    random_recipe.settings.public = not is_private_recipe
     database.recipes.update(random_recipe.slug, random_recipe)
 
     # Try to access recipe
     recipe_group = database.groups.get_by_slug_or_id(random_recipe.group_id)
+    recipe_household = database.households.get_by_slug_or_id(random_recipe.household_id)
     assert recipe_group
+    assert recipe_household
     response = api_client.get(
-        api_routes.explore_recipes_group_slug_recipe_slug(
+        api_routes.explore_groups_group_slug_households_household_slug_recipes_recipe_slug(
             recipe_group.slug,
+            recipe_household.slug,
             random_recipe.slug,
         )
     )
-    assert response.status_code == test_case.status_code
+    if is_private_group or is_private_household or is_private_recipe:
+        assert response.status_code == 404
+        if is_private_group:
+            assert response.json()["detail"] == "group not found"
+        elif is_private_household:
+            assert response.json()["detail"] == "household not found"
+        else:
+            assert response.json()["detail"] == "recipe not found"
 
-    if test_case.error:
-        assert response.json()["detail"] == test_case.error
         return
 
     as_json = response.json()
