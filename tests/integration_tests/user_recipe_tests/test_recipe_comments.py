bc7acaa9f7437b8df69a154adb44ca4304ae070d
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-25 03:55:06 +0000

    test fixes

--- tests/integration_tests/user_recipe_tests/test_recipe_comments.py
+++ tests/integration_tests/user_recipe_tests/test_recipe_comments.py
@@ -1,7 +1,10 @@
+from uuid import UUID
+
 import pytest
 from fastapi.testclient import TestClient
 from pydantic import UUID4
 
+from mealie.repos.repository_factory import AllRepositories
 from mealie.schema.recipe.recipe import Recipe
 from tests.utils import api_routes
 from tests.utils.factories import random_string
@@ -93,7 +96,20 @@ def test_delete_comment(api_client: TestClient, unique_recipe: Recipe, unique_us
     assert response.status_code == 404
 
 
-def test_admin_can_delete(api_client: TestClient, unique_recipe: Recipe, unique_user: TestUser, admin_user: TestUser):
+def test_admin_can_delete(
+    unfiltered_database: AllRepositories,
+    api_client: TestClient,
+    unique_recipe: Recipe,
+    unique_user: TestUser,
+    admin_user: TestUser,
+):
+    # Make sure admin belongs to same group/household as user
+    admin_data = unfiltered_database.users.get_one(admin_user.user_id)
+    assert admin_data
+    admin_data.group_id = UUID(unique_user.group_id)
+    admin_data.household_id = UUID(unique_user.household_id)
+    unfiltered_database.users.update(admin_user.user_id, admin_data)
+
     # Create Comment
     create_data = random_comment(unique_recipe.id)
     response = api_client.post(api_routes.comments, json=create_data, headers=unique_user.token)
