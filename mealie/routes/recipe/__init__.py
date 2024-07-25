01c5cc2c0ba85e9873c494bd2f2eefbb51aae5af
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-25 20:09:37 +0000

    removed unused routes

--- mealie/routes/recipe/__init__.py
+++ mealie/routes/recipe/__init__.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
 
-from . import all_recipe_routes, bulk_actions, comments, recipe_crud_routes, shared_routes, timeline_events
+from . import bulk_actions, comments, recipe_crud_routes, shared_routes, timeline_events
 
 prefix = "/recipes"
 
 router = APIRouter()
 
-router.include_router(all_recipe_routes.router, prefix=prefix, tags=["Recipe: Query All"])
 router.include_router(recipe_crud_routes.router_exports)
 router.include_router(recipe_crud_routes.router)
 router.include_router(comments.router, prefix=prefix, tags=["Recipe: Comments"])
