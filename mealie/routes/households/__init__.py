f37b61aedd8f2c2569565630d75bf475d946e905
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-30 18:47:23 +0000

    fix fastapi router order

--- mealie/routes/households/__init__.py
+++ mealie/routes/households/__init__.py
@@ -19,8 +19,10 @@ router.include_router(controller_group_notifications.router)
 router.include_router(controller_group_recipe_actions.router)
 router.include_router(controller_household_self_service.router)
 router.include_router(controller_invitations.router)
-router.include_router(controller_mealplan.router)
-router.include_router(controller_mealplan_rules.router)
 router.include_router(controller_shopping_lists.router)
 router.include_router(controller_shopping_lists.item_router)
 router.include_router(controller_webhooks.router)
+
+# mealplan_rules must be added before mealplan due to the way the routes are defined
+router.include_router(controller_mealplan_rules.router)
+router.include_router(controller_mealplan.router)
