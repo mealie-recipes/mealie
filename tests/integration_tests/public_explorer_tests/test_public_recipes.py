e1080c90588c233ff883495c2fcbc28798e325fb
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-30 22:31:36 +0000

    added test for recipe filter

--- tests/integration_tests/public_explorer_tests/test_public_recipes.py
+++ tests/integration_tests/public_explorer_tests/test_public_recipes.py
@@ -7,6 +7,7 @@ from pydantic import UUID4
 
 from mealie.schema.cookbook.cookbook import SaveCookBook
 from mealie.schema.recipe.recipe import Recipe
+from mealie.schema.recipe.recipe_category import TagSave
 from tests.utils import api_routes
 from tests.utils.factories import random_int, random_string
 from tests.utils.fixture_schemas import TestUser
@@ -241,3 +242,84 @@ def test_public_recipe_cookbook_filter(
         assert response.status_code == 404
     else:
         assert response.status_code == 200
+
+
+def test_public_recipe_cookbook_filter_with_recipes(api_client: TestClient, unique_user: TestUser, h2_user: TestUser):
+    database = unique_user.repos
+
+    # Create a public and private recipe with a known tag
+    group = database.groups.get_one(unique_user.group_id)
+    assert group and group.preferences
+
+    group.preferences.private_group = False
+    database.group_preferences.update(group.id, group.preferences)
+
+    household = database.households.get_one(unique_user.household_id)
+    assert household and household.preferences
+
+    household.preferences.private_household = False
+    household.preferences.recipe_public = True
+    database.household_preferences.update(household.id, household.preferences)
+
+    tag = database.tags.create(TagSave(name=random_string(), group_id=unique_user.group_id))
+    public_recipe, private_recipe = database.recipes.create_many(
+        Recipe(
+            user_id=unique_user.user_id,
+            group_id=unique_user.group_id,
+            name=random_string(),
+        )
+        for _ in range(2)
+    )
+
+    assert public_recipe.settings
+    public_recipe.settings.public = True
+    public_recipe.tags = [tag]
+
+    assert private_recipe.settings
+    private_recipe.settings.public = False
+    private_recipe.tags = [tag]
+
+    database.recipes.update_many([public_recipe, private_recipe])
+
+    # Create a recipe in another household that's public with the same known tag
+    other_database = h2_user.repos
+    other_household = other_database.households.get_one(h2_user.household_id)
+    assert other_household and other_household.preferences
+
+    other_household.preferences.private_household = False
+    other_household.preferences.recipe_public = True
+    other_database.household_preferences.update(household.id, household.preferences)
+
+    other_household_recipe = other_database.recipes.create(
+        Recipe(
+            user_id=h2_user.user_id,
+            group_id=h2_user.group_id,
+            name=random_string(),
+        )
+    )
+    assert other_household_recipe.settings
+    other_household_recipe.settings.public = True
+    other_household_recipe.tags = [tag]
+    other_database.recipes.update(other_household_recipe.slug, other_household_recipe)
+
+    # Create a public cookbook with tag
+    cookbook = database.cookbooks.create(
+        SaveCookBook(
+            name=random_string(),
+            group_id=unique_user.group_id,
+            household_id=unique_user.household_id,
+            public=True,
+            tags=[tag],
+        )
+    )
+
+    # Get the cookbook's recipes and make sure we only get the public recipe from the correct household
+    response = api_client.get(
+        api_routes.explore_groups_group_slug_recipes(unique_user.group_id), params={"cookbook": cookbook.id}
+    )
+    assert response.status_code == 200
+    recipe_ids: set[str] = {recipe["id"] for recipe in response.json()["items"]}
+    assert len(recipe_ids) == 1
+    assert str(public_recipe.id) in recipe_ids
+    assert str(private_recipe.id) not in recipe_ids
+    assert str(other_household_recipe.id) not in recipe_ids
