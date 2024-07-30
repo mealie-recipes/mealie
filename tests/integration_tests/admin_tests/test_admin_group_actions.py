3226c17d8b919e729e9cd71f16aa8e93b8e55c44
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-30 04:11:36 +0000

    added test

--- tests/integration_tests/admin_tests/test_admin_group_actions.py
+++ tests/integration_tests/admin_tests/test_admin_group_actions.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
 
+from mealie.core.config import get_app_settings
+from mealie.schema.household.household import HouseholdInDB
+from mealie.schema.user.user import GroupInDB
 from tests.utils import api_routes
 from tests.utils.assertion_helpers import assert_ignore_keys
 from tests.utils.factories import random_bool, random_string
@@ -28,6 +31,28 @@ def test_admin_group_routes_are_restricted(api_client: TestClient, unique_user: 
 def test_admin_create_group(api_client: TestClient, admin_user: TestUser):
     response = api_client.post(api_routes.admin_groups, json={"name": random_string()}, headers=admin_user.token)
     assert response.status_code == 201
+
+    # verify preferences are set and the default household is created
+    group = GroupInDB.model_validate(response.json())
+    assert group.preferences and len(group.households) == 1
+    created_household = group.households[0]
+    assert created_household.name == get_app_settings().DEFAULT_HOUSEHOLD
+
+    response = api_client.get(api_routes.admin_households_item_id(created_household.id), headers=admin_user.token)
+    assert response.status_code == 200
+    assert response.json()["id"] == str(created_household.id)
+
+    # verify no extra households are created
+    response = api_client.get(api_routes.admin_households, headers=admin_user.token, params={"page": 1, "perPage": -1})
+    assert response.status_code == 200
+    items = response.json()["items"]
+    filtered_item_ids: list[str] = []
+    for item in items:
+        if item["groupId"] == str(group.id):
+            filtered_item_ids.append(item["id"])
+
+    assert len(filtered_item_ids) == 1
+    assert filtered_item_ids[0] == str(created_household.id)
 
 
 def test_admin_update_group(api_client: TestClient, admin_user: TestUser, unique_user: TestUser):
