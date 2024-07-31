de85623b0e0033dda23bddcc5ce316a3295634f0
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-07-31 01:32:59 +0000

    add household name to new cookbooks

--- frontend/composables/use-group-cookbooks.ts
+++ frontend/composables/use-group-cookbooks.ts
@@ -2,6 +2,7 @@ import { useAsync, ref, Ref, useContext } from "@nuxtjs/composition-api";
 import { useAsyncKey } from "./use-utils";
 import { usePublicExploreApi } from "./api/api-client";
 import { useUserApi } from "~/composables/api";
+import { useHouseholdSelf } from "./use-households";
 import { ReadCookBook, UpdateCookBook } from "~/lib/api/types/cookbook";
 
 let cookbookStore: Ref<ReadCookBook[] | null> | null = null;
@@ -67,6 +68,7 @@ export const usePublicCookbooks = function (groupSlug: string) {
 
 export const useCookbooks = function () {
   const api = useUserApi();
+  const { household } = useHouseholdSelf();
   const loading = ref(false);
 
   const { i18n } = useContext();
@@ -100,7 +102,7 @@ export const useCookbooks = function () {
     async createOne() {
       loading.value = true;
       const { data } = await api.cookbooks.createOne({
-        name: i18n.t("cookbook.cookbook-with-name", [String((cookbookStore?.value?.length ?? 0) + 1)]) as string,
+        name: i18n.t("cookbook.household-cookbook-name", [household.value?.name || "", String((cookbookStore?.value?.length ?? 0) + 1)]) as string,
       });
       if (data && cookbookStore?.value) {
         cookbookStore.value.push(data);
