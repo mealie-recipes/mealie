8119e8b9c2fd6837d99ba441d552496fb5c38a88
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-09-02 20:58:43 +0000

    updated permissions composable

--- frontend/composables/recipes/use-recipe-permissions.ts
+++ frontend/composables/recipes/use-recipe-permissions.ts
@@ -1,34 +1,40 @@
 import { computed } from "@nuxtjs/composition-api";
 import { Recipe } from "~/lib/api/types/recipe";
+import { HouseholdSummary } from "~/lib/api/types/household";
 import { UserOut } from "~/lib/api/types/user";
 
-export function useRecipePermissions(recipe: Recipe, user: UserOut | null) {
-    const canEditRecipe = computed(() => {
-        // Check recipe owner
-        if (!user?.id) {
-          return false;
-        }
-        if (user.id === recipe.userId) {
-          return true;
-        }
-
-        // Check group and household
-        if (user.groupId !== recipe.groupId) {
-          return false;
-        }
-        if (user.householdId !== recipe.householdId) {
-          return false;
-        }
-
-        // Check recipe
-        if (recipe.settings?.locked) {
-          return false;
-        }
-
+export function useRecipePermissions(recipe: Recipe, recipeHousehold: HouseholdSummary | null, user: UserOut | null) {
+  const canEditRecipe = computed(() => {
+      // Check recipe owner
+      if (!user?.id) {
+        return false;
+      }
+      if (user.id === recipe.userId) {
         return true;
-      });
+      }
 
-    return {
-        canEditRecipe,
-    }
+      // Check group and household
+      if (user.groupId !== recipe.groupId) {
+        return false;
+      }
+      if (user.householdId !== recipe.householdId) {
+        if (!recipeHousehold?.preferences) {
+          return false;
+        }
+        if (recipeHousehold.preferences.lockRecipeEditsFromOtherHouseholds) {
+          return false;
+        }
+      }
+
+      // Check recipe
+      if (recipe.settings?.locked) {
+        return false;
+      }
+
+      return true;
+    });
+
+  return {
+    canEditRecipe,
+  }
 }
