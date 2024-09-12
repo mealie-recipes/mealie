6e0c6490cf20abb56edb6847fa579be8d3c7a0ac
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-09-12 19:06:12 +0000

    added missing store index

--- frontend/composables/store/index.ts
+++ frontend/composables/store/index.ts
@@ -1,5 +1,6 @@
 export { useCategoryStore, usePublicCategoryStore, useCategoryData } from "./use-category-store";
 export { useFoodStore, usePublicFoodStore, useFoodData } from "./use-food-store";
+export { useHouseholdStore, usePublicHouseholdStore } from "./use-household-store";
 export { useLabelStore, useLabelData } from "./use-label-store";
 export { useTagStore, usePublicTagStore, useTagData } from "./use-tag-store";
 export { useToolStore, usePublicToolStore, useToolData } from "./use-tool-store";
