ba17825c319779f654ab11e16ca6f70c09217f00
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-09-18 14:05:33 +0000

    fix/add tests

--- tests/integration_tests/public_explorer_tests/test_public_households.py
+++ tests/integration_tests/public_explorer_tests/test_public_households.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 import pytest
 from fastapi.testclient import TestClient
 
@@ -9,8 +11,9 @@ from tests.utils.factories import random_string
 from tests.utils.fixture_schemas import TestUser
 
 
-def test_get_all_households(api_client: TestClient, unique_user: TestUser):
-    unique_user.repos.groups.patch(unique_user.group_id, {"preferences": {"private_group": False}})
+@pytest.mark.parametrize("is_private_group", [True, False])
+def test_get_all_households(api_client: TestClient, unique_user: TestUser, is_private_group: bool):
+    unique_user.repos.group_preferences.patch(UUID(unique_user.group_id), {"private_group": is_private_group})
     households = [
         HouseholdService.create_household(
             unique_user.repos,
@@ -21,14 +24,18 @@ def test_get_all_households(api_client: TestClient, unique_user: TestUser):
     ]
 
     response = api_client.get(api_routes.explore_groups_group_slug_households(unique_user.group_id))
-    assert response.status_code == 200
-    response_ids = [item["id"] for item in response.json()["items"]]
-    for household in households:
-        assert str(household.id) in response_ids
+    if is_private_group:
+        assert response.status_code == 404
+    else:
+        assert response.status_code == 200
+        response_ids = [item["id"] for item in response.json()["items"]]
+        for household in households:
+            assert str(household.id) in response_ids
 
 
-def test_get_all_households_public_only(api_client: TestClient, unique_user: TestUser):
-    unique_user.repos.groups.patch(unique_user.group_id, {"preferences": {"private_group": False}})
+@pytest.mark.parametrize("is_private_group", [True, False])
+def test_get_all_households_public_only(api_client: TestClient, unique_user: TestUser, is_private_group: bool):
+    unique_user.repos.group_preferences.patch(UUID(unique_user.group_id), {"private_group": is_private_group})
     public_household = HouseholdService.create_household(
         unique_user.repos,
         HouseholdCreate(name=random_string()),
@@ -41,15 +48,21 @@ def test_get_all_households_public_only(api_client: TestClient, unique_user: Tes
     )
 
     response = api_client.get(api_routes.explore_groups_group_slug_households(unique_user.group_id))
-    assert response.status_code == 200
-    response_ids = [item["id"] for item in response.json()["items"]]
-    assert str(public_household.id) in response_ids
-    assert str(private_household.id) not in response_ids
+    if is_private_group:
+        assert response.status_code == 404
+    else:
+        assert response.status_code == 200
+        response_ids = [item["id"] for item in response.json()["items"]]
+        assert str(public_household.id) in response_ids
+        assert str(private_household.id) not in response_ids
 
 
+@pytest.mark.parametrize("is_private_group", [True, False])
 @pytest.mark.parametrize("is_private_household", [True, False])
-def test_get_household(api_client: TestClient, unique_user: TestUser, is_private_household: bool):
-    unique_user.repos.groups.patch(unique_user.group_id, {"preferences": {"private_group": False}})
+def test_get_household(
+    api_client: TestClient, unique_user: TestUser, is_private_group: bool, is_private_household: bool
+):
+    unique_user.repos.group_preferences.patch(UUID(unique_user.group_id), {"private_group": is_private_group})
     household = household = HouseholdService.create_household(
         unique_user.repos,
         HouseholdCreate(name=random_string()),
@@ -61,7 +74,7 @@ def test_get_household(api_client: TestClient, unique_user: TestUser, is_private
         headers=unique_user.token,
     )
 
-    if is_private_household:
+    if is_private_group or is_private_household:
         assert response.status_code == 404
     else:
         assert response.status_code == 200
