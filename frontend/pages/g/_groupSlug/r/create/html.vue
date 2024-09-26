d371c07afcaf89ea4980f62973b99dd0721aa7c4
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-09-26 20:07:13 +0000

    formatting

--- frontend/pages/g/_groupSlug/r/create/html.vue
+++ frontend/pages/g/_groupSlug/r/create/html.vue
@@ -1,60 +1,60 @@
 <template>
-    <v-form ref="domUrlForm" @submit.prevent="createFromHtmlOrJson(newRecipeData, importKeywordsAsTags, stayInEditMode)">
-      <div>
-        <v-card-title class="headline"> {{ $tc('recipe.import-from-html-or-json') }} </v-card-title>
-        <v-card-text>
-          <p>
-            {{ $tc("recipe.import-from-html-or-json-description") }}
-          </p>
-          <p>
-            {{ $tc("recipe.json-import-format-description-colon") }}
-            <a href="https://schema.org/Recipe" target="_blank">https://schema.org/Recipe</a>
-          </p>
-          <v-switch
-            v-model="isEditJSON"
-            :label="$tc('recipe.json-editor')"
-            class="mt-2"
-            @change="handleIsEditJson"
-          />
-          <LazyRecipeJsonEditor
-            v-if="isEditJSON"
-            v-model="newRecipeData"
-            height="250px"
-            class="mt-10"
-            :options="EDITOR_OPTIONS"
-          />
-          <v-textarea
-            v-else
-            v-model="newRecipeData"
-            :label="$tc('new-recipe.recipe-html-or-json')"
-            :prepend-inner-icon="$globals.icons.codeTags"
-            validate-on-blur
-            autofocus
-            filled
-            clearable
-            class="rounded-lg mt-2"
+  <v-form ref="domUrlForm" @submit.prevent="createFromHtmlOrJson(newRecipeData, importKeywordsAsTags, stayInEditMode)">
+    <div>
+      <v-card-title class="headline"> {{ $tc('recipe.import-from-html-or-json') }} </v-card-title>
+      <v-card-text>
+        <p>
+          {{ $tc("recipe.import-from-html-or-json-description") }}
+        </p>
+        <p>
+          {{ $tc("recipe.json-import-format-description-colon") }}
+          <a href="https://schema.org/Recipe" target="_blank">https://schema.org/Recipe</a>
+        </p>
+        <v-switch
+          v-model="isEditJSON"
+          :label="$tc('recipe.json-editor')"
+          class="mt-2"
+          @change="handleIsEditJson"
+        />
+        <LazyRecipeJsonEditor
+          v-if="isEditJSON"
+          v-model="newRecipeData"
+          height="250px"
+          class="mt-10"
+          :options="EDITOR_OPTIONS"
+        />
+        <v-textarea
+          v-else
+          v-model="newRecipeData"
+          :label="$tc('new-recipe.recipe-html-or-json')"
+          :prepend-inner-icon="$globals.icons.codeTags"
+          validate-on-blur
+          autofocus
+          filled
+          clearable
+          class="rounded-lg mt-2"
+          rounded
+          :hint="$tc('new-recipe.url-form-hint')"
+          persistent-hint
+        />
+        <v-checkbox v-model="importKeywordsAsTags" hide-details :label="$tc('recipe.import-original-keywords-as-tags')" />
+        <v-checkbox v-model="stayInEditMode" hide-details :label="$tc('recipe.stay-in-edit-mode')" />
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <div style="width: 250px">
+          <BaseButton
+            :disabled="!newRecipeData"
+            large
             rounded
-            :hint="$tc('new-recipe.url-form-hint')"
-            persistent-hint
+            block
+            type="submit"
+            :loading="loading"
           />
-          <v-checkbox v-model="importKeywordsAsTags" hide-details :label="$tc('recipe.import-original-keywords-as-tags')" />
-          <v-checkbox v-model="stayInEditMode" hide-details :label="$tc('recipe.stay-in-edit-mode')" />
-        </v-card-text>
-        <v-card-actions class="justify-center">
-          <div style="width: 250px">
-            <BaseButton
-              :disabled="!newRecipeData"
-              large
-              rounded
-              block
-              type="submit"
-              :loading="loading"
-            />
-          </div>
-        </v-card-actions>
-      </div>
-    </v-form>
-  </template>
+        </div>
+      </v-card-actions>
+    </div>
+  </v-form>
+</template>
 
 <script lang="ts">
 import { computed, defineComponent, reactive, toRefs, ref, useContext, useRoute, useRouter } from "@nuxtjs/composition-api";
