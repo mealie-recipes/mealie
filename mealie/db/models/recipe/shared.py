d064f23dabb5ccd53b5e6ce7a09a89a28fdf5706
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-10-03 15:52:20 +0000

    replace all instances of datetime with naivedatetime

--- mealie/db/models/recipe/shared.py
+++ mealie/db/models/recipe/shared.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from mealie.db.models._model_base import BaseMixins, SqlAlchemyBase
 from mealie.db.models._model_utils.auto_init import auto_init
+from mealie.db.models._model_utils.datetime import NaiveDateTime
 from mealie.db.models._model_utils.guid import GUID
 
 if TYPE_CHECKING:
@@ -26,7 +27,7 @@ class RecipeShareTokenModel(SqlAlchemyBase, BaseMixins):
     recipe_id: Mapped[GUID] = mapped_column(GUID, sa.ForeignKey("recipes.id"), nullable=False, index=True)
     recipe: Mapped["RecipeModel"] = sa.orm.relationship("RecipeModel", back_populates="share_tokens", uselist=False)
 
-    expires_at: Mapped[datetime] = mapped_column(sa.DateTime, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(NaiveDateTime, nullable=False)
 
     @auto_init()
     def __init__(self, **_) -> None:
--- mealie/db/models/server/task.py
+++ mealie/db/models/server/task.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import DateTime, ForeignKey, String, orm
+from sqlalchemy import ForeignKey, String, orm
 from sqlalchemy.orm import Mapped, mapped_column
 
 from mealie.db.models._model_base import BaseMixins, SqlAlchemyBase
+from mealie.db.models._model_utils.datetime import NaiveDateTime
 from mealie.db.models._model_utils.guid import GUID
 
 from .._model_utils.auto_init import auto_init
@@ -18,7 +19,7 @@ class ServerTaskModel(SqlAlchemyBase, BaseMixins):
 
     __tablename__ = "server_tasks"
     name: Mapped[str] = mapped_column(String, nullable=False)
-    completed_date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    completed_date: Mapped[datetime] = mapped_column(NaiveDateTime, nullable=True)
     status: Mapped[str] = mapped_column(String, nullable=False)
     log: Mapped[str] = mapped_column(String, nullable=True)
 
