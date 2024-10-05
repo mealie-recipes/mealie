1440cec3619862789bd215ffa1ce420be42b027d
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-10-05 21:01:36 +0000

    allow param override in stores

--- frontend/composables/partials/use-store-factory.ts
+++ frontend/composables/partials/use-store-factory.ts
@@ -2,6 +2,7 @@ import { ref, reactive, Ref } from "@nuxtjs/composition-api";
 import { useReadOnlyActions, useStoreActions } from "./use-actions-factory";
 import { BoundT } from "./types";
 import { BaseCRUDAPI, BaseCRUDAPIReadOnly } from "~/lib/api/base/base-clients";
+import { QueryValue } from "~/lib/api/base/route";
 
 export const useData = function<T extends BoundT>(defaultObject: T) {
   const data = reactive({ ...defaultObject });
@@ -16,16 +17,21 @@ export const useReadOnlyStore = function<T extends BoundT>(
   store: Ref<T[]>,
   loading: Ref<boolean>,
   api: BaseCRUDAPIReadOnly<T>,
+  params = {} as Record<string, QueryValue>,
 ) {
+  const storeActions = useReadOnlyActions(api, store, loading);
   const actions = {
-    ...useReadOnlyActions(api, store, loading),
+    ...storeActions,
+    async refresh() {
+      return await storeActions.refresh(1, -1, params);
+    },
     flushStore() {
       store.value = [];
     },
   };
 
   if (!loading.value && (!store.value || store.value.length === 0)) {
-    const result = actions.getAll();
+    const result = actions.getAll(1, -1, params);
     store.value = result.value || [];
   }
 
@@ -36,16 +42,21 @@ export const useStore = function<T extends BoundT>(
   store: Ref<T[]>,
   loading: Ref<boolean>,
   api: BaseCRUDAPI<unknown, T, unknown>,
+  params = {} as Record<string, QueryValue>,
 ) {
+  const storeActions = useStoreActions(api, store, loading);
   const actions = {
-    ...useStoreActions(api, store, loading),
+    ...storeActions,
+    async refresh() {
+      return await storeActions.refresh(1, -1, params);
+    },
     flushStore() {
       store = ref([]);
     },
   };
 
   if (!loading.value && (!store.value || store.value.length === 0)) {
-    const result = actions.getAll();
+    const result = actions.getAll(1, -1, params);
     store.value = result.value || [];
   }
 
