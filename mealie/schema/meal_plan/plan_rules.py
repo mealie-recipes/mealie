e82414de1d91a565c79a3f85189c5ff89d210326
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-10-17 15:16:09 +0000

    added validations

--- mealie/schema/meal_plan/plan_rules.py
+++ mealie/schema/meal_plan/plan_rules.py
@@ -2,9 +2,11 @@ import datetime
 from enum import Enum
 from typing import Annotated
 
+import sqlalchemy as sa
 from pydantic import UUID4, ConfigDict, Field, ValidationInfo, field_validator
 
 from mealie.core.root_logger import get_logger
+from mealie.db.models.recipe import RecipeModel
 from mealie.schema._mealie import MealieModel
 from mealie.schema.response.pagination import PaginationBase
 from mealie.schema.response.query_filter import QueryFilterBuilder, QueryFilterJSON
@@ -44,6 +46,19 @@ class PlanRulesCreate(MealieModel):
     entry_type: PlanRulesType = PlanRulesType.unset
     query_filter_string: str = ""
 
+    @field_validator("query_filter_string")
+    def validate_query_filter_string(cls, value: str) -> str:
+        # The query filter builder does additional validations while building the
+        # database query, so we make sure constructing the query is successful
+        builder = QueryFilterBuilder(value)
+
+        try:
+            builder.filter_query(sa.select(RecipeModel), RecipeModel)
+        except Exception as e:
+            raise ValueError("Invalid query filter string") from e
+
+        return value
+
 
 class PlanRulesSave(PlanRulesCreate):
     group_id: UUID4
@@ -55,6 +70,11 @@ class PlanRulesOut(PlanRulesSave):
     query_filter: Annotated[QueryFilterJSON, Field(validate_default=True)] = None  # type: ignore
 
     model_config = ConfigDict(from_attributes=True)
+
+    @field_validator("query_filter_string")
+    def validate_query_filter_string(value: str) -> str:
+        # Skip validation since we are not updating the query filter string
+        return value
 
     @field_validator("query_filter", mode="before")
     def validate_query_filter(cls, _, info: ValidationInfo) -> QueryFilterJSON:
