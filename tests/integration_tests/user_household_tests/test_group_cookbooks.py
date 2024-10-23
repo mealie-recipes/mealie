830b65e986fdc387ec8778504284408816215e17
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-10-23 20:22:23 +0000

    add/update tests

--- tests/integration_tests/user_household_tests/test_group_cookbooks.py
+++ tests/integration_tests/user_household_tests/test_group_cookbooks.py
@@ -60,6 +60,8 @@ def test_create_cookbook(api_client: TestClient, unique_user: TestUser):
     page_data = get_page_data(unique_user.group_id, unique_user.household_id)
     response = api_client.post(api_routes.households_cookbooks, json=page_data, headers=unique_user.token)
     assert response.status_code == 201
+    assert response.json()["groupId"] == unique_user.group_id
+    assert response.json()["householdId"] == unique_user.household_id
 
 
 @pytest.mark.parametrize("name_input", ["", " ", "@"])
@@ -78,9 +80,22 @@ def test_create_cookbook_bad_name(api_client: TestClient, unique_user: TestUser,
     assert response.status_code == 422
 
 
-def test_read_cookbook(api_client: TestClient, unique_user: TestUser, cookbooks: list[TestCookbook]):
+@pytest.mark.parametrize("use_other_household", [True, False])
+def test_read_cookbook(
+    api_client: TestClient,
+    unique_user: TestUser,
+    h2_user: TestUser,
+    cookbooks: list[TestCookbook],
+    use_other_household: bool,
+):
     sample = random.choice(cookbooks)
-    response = api_client.get(api_routes.households_cookbooks_item_id(sample.id), headers=unique_user.token)
+    if use_other_household:
+        headers = h2_user.token
+    else:
+        headers = unique_user.token
+
+    # all households should be able to fetch all cookbooks
+    response = api_client.get(api_routes.households_cookbooks_item_id(sample.id), headers=headers)
     assert response.status_code == 200
 
     page_data = response.json()
@@ -111,6 +126,28 @@ def test_update_cookbook(api_client: TestClient, unique_user: TestUser, cookbook
     assert page_data["slug"] == update_data["name"]
 
 
+def test_update_cookbook_other_household(
+    api_client: TestClient, unique_user: TestUser, h2_user: TestUser, cookbooks: list[TestCookbook]
+):
+    cookbook = random.choice(cookbooks)
+
+    update_data = get_page_data(unique_user.group_id, unique_user.household_id)
+
+    update_data["name"] = random_string(10)
+
+    response = api_client.put(
+        api_routes.households_cookbooks_item_id(cookbook.id), json=update_data, headers=h2_user.token
+    )
+    assert response.status_code == 404
+
+    response = api_client.get(api_routes.households_cookbooks_item_id(cookbook.id), headers=unique_user.token)
+    assert response.status_code == 200
+
+    page_data = response.json()
+    assert page_data["name"] != update_data["name"]
+    assert page_data["slug"] != update_data["name"]
+
+
 def test_update_cookbooks_many(api_client: TestClient, unique_user: TestUser, cookbooks: list[TestCookbook]):
     pages = [x.data for x in cookbooks]
 
@@ -135,6 +172,20 @@ def test_update_cookbooks_many(api_client: TestClient, unique_user: TestUser, co
         assert str(know) in server_ids
 
 
+def test_update_cookbooks_many_other_household(
+    api_client: TestClient, unique_user: TestUser, h2_user: TestUser, cookbooks: list[TestCookbook]
+):
+    pages = [x.data for x in cookbooks]
+
+    reverse_order = sorted(pages, key=lambda x: x["position"], reverse=True)
+    for x, page in enumerate(reverse_order):
+        page["position"] = x
+        page["group_id"] = str(unique_user.group_id)
+
+    response = api_client.put(api_routes.households_cookbooks, json=utils.jsonify(reverse_order), headers=h2_user.token)
+    assert response.status_code == 404
+
+
 def test_delete_cookbook(api_client: TestClient, unique_user: TestUser, cookbooks: list[TestCookbook]):
     sample = random.choice(cookbooks)
     response = api_client.delete(api_routes.households_cookbooks_item_id(sample.id), headers=unique_user.token)
@@ -143,6 +194,18 @@ def test_delete_cookbook(api_client: TestClient, unique_user: TestUser, cookbook
 
     response = api_client.get(api_routes.households_cookbooks_item_id(sample.slug), headers=unique_user.token)
     assert response.status_code == 404
+
+
+def test_delete_cookbook_other_household(
+    api_client: TestClient, unique_user: TestUser, h2_user: TestUser, cookbooks: list[TestCookbook]
+):
+    sample = random.choice(cookbooks)
+    response = api_client.delete(api_routes.households_cookbooks_item_id(sample.id), headers=h2_user.token)
+
+    assert response.status_code == 404
+
+    response = api_client.get(api_routes.households_cookbooks_item_id(sample.slug), headers=unique_user.token)
+    assert response.status_code == 200
 
 
 @pytest.mark.parametrize(
