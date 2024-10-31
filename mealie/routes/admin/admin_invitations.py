2acfa6ed080c0c33f01d8b5cba7bbefdd026f2d9
p0lycarpio <arsene.reymond@free.fr>
2024-10-31 17:18:33 +0000

    backend changes

--- mealie/routes/admin/admin_invitations.py
+++ mealie/routes/admin/admin_invitations.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from mealie.routes._base import BaseAdminController, controller
 from mealie.schema.household.invite_token import (
     ReadInviteToken,
 )
+from mealie.schema.make_dependable import make_dependable
 from mealie.schema.response.pagination import PaginationQuery
 
 router = APIRouter(prefix="/invitations")
@@ -12,14 +13,9 @@ router = APIRouter(prefix="/invitations")
 @controller(router)
 class AdminInvitationsController(BaseAdminController):
     @router.get("", response_model=list[ReadInviteToken])
-    def get_all(self):
+    def get_all(
+        self,
+        q: PaginationQuery = Depends(make_dependable(PaginationQuery)),
+    ):
         """Get all invite tokens"""
-        self.repos.group_id = None
-        self.repos.household_id = None
-        return self.repos.group_invite_tokens.page_all(PaginationQuery(page=1, per_page=-1)).items
-
-    @router.get("/{group_slug}", response_model=list[ReadInviteToken])
-    def get_one(self, group_slug: str):
-        """Get all invite tokens for a specific group"""
-        self.repos.group_id = self.repos.groups.get_by_slug_or_id(group_slug).id
-        return self.repos.group_invite_tokens.page_all(PaginationQuery(page=1, per_page=-1)).items
+        return self.repos.group_invite_tokens.page_all(pagination=q).items
