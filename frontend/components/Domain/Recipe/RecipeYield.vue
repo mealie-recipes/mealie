fc174e4db0a85db176fc9ff0ace9d0622036429e
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-11-08 19:08:11 +0000

    sanitize yield html

--- frontend/components/Domain/Recipe/RecipeYield.vue
+++ frontend/components/Domain/Recipe/RecipeYield.vue
@@ -16,6 +16,7 @@
 
 <script lang="ts">
 import { computed, defineComponent, useContext } from "@nuxtjs/composition-api";
+import DOMPurify from "dompurify";
 import { useScaledAmount } from "~/composables/recipes/use-scaled-amount";
 
 export default defineComponent({
@@ -39,6 +40,14 @@ export default defineComponent({
   },
   setup(props) {
     const { i18n } = useContext();
+
+    function sanitizeHTML(rawHtml: string) {
+      return DOMPurify.sanitize(rawHtml, {
+        USE_PROFILES: { html: true },
+        ALLOWED_TAGS: ["strong", "sup"],
+      });
+    }
+
     const displayText = computed(() => {
       if (!(props.yieldQuantity || props.yield)) {
         return "";
@@ -48,7 +57,7 @@ export default defineComponent({
 
       return scaledAmountDisplay ? i18n.t("recipe.yields-amount-with-text", {
         amount: scaledAmountDisplay,
-        text: props.yield,
+        text: sanitizeHTML(props.yield),
       }) as string : "";
     });
 
