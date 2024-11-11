e8bbd36693517df4a6dc16fe6407e77bd0214c80
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-11-11 23:07:59 +0000

    fix route order

--- mealie/routes/recipe/__init__.py
+++ mealie/routes/recipe/__init__.py
@@ -6,10 +6,9 @@ prefix = "/recipes"
 
 router = APIRouter()
 
-
+router.include_router(exports.router, tags=["Recipe: Exports"])
 router.include_router(recipe_crud_routes.router, tags=["Recipe: CRUD"])
 router.include_router(comments.router, prefix=prefix, tags=["Recipe: Comments"])
 router.include_router(bulk_actions.router, prefix=prefix, tags=["Recipe: Bulk Actions"])
 router.include_router(shared_routes.router, prefix=prefix, tags=["Recipe: Shared"])
-router.include_router(exports.router, tags=["Recipe: Exports"])
 router.include_router(timeline_events.router, prefix=prefix, tags=["Recipe: Timeline"])
