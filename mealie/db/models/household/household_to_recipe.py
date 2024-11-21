ce7180e54d0ebc86335c737c1f9a0d30e4dfe0b1
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-11-21 15:41:56 +0000

    fix sqlalchemy warnings

--- mealie/db/models/household/household_to_recipe.py
+++ mealie/db/models/household/household_to_recipe.py
@@ -23,9 +23,9 @@ class HouseholdToRecipe(SqlAlchemyBase, BaseMixins):
     __table_args__ = (UniqueConstraint("household_id", "recipe_id", name="household_id_recipe_id_key"),)
     id: Mapped[GUID] = mapped_column(GUID, primary_key=True, default=GUID.generate)
 
-    household: Mapped["Household"] = relationship("Household")
+    household: Mapped["Household"] = relationship("Household", viewonly=True)
     household_id = Column(GUID, ForeignKey("households.id"), index=True, primary_key=True)
-    recipe: Mapped["RecipeModel"] = relationship("RecipeModel")
+    recipe: Mapped["RecipeModel"] = relationship("RecipeModel", viewonly=True)
     recipe_id = Column(GUID, ForeignKey("recipes.id"), index=True, primary_key=True)
     group_id: AssociationProxy[GUID] = association_proxy("household", "group_id")
 
