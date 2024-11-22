468ab54d266dfa7386751d31d8eb48499ee9dd24
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2024-11-22 21:57:25 +0000

    added migration scripts

--- alembic/versions/2024-11-20-17.30.41_b9e516e2d3b3_add_household_to_recipe_last_made_.py
+++ alembic/versions/2024-11-20-17.30.41_b9e516e2d3b3_add_household_to_recipe_last_made_.py
@@ -6,12 +6,17 @@ Create Date: 2024-11-20 17:30:41.152332
 
 """
 
+from datetime import datetime
+
 import sqlalchemy as sa
 from sqlalchemy import orm
+from sqlalchemy.orm import DeclarativeBase
 
 import mealie.db.migration_types
 from alembic import op
 from mealie.core.root_logger import get_logger
+from mealie.db.models._model_utils.datetime import NaiveDateTime
+from mealie.db.models._model_utils.guid import GUID
 
 # revision identifiers, used by Alembic.
 revision = "b9e516e2d3b3"
@@ -22,16 +27,133 @@ depends_on: str | tuple[str, ...] | None = None
 logger = get_logger()
 
 
+class SqlAlchemyBase(DeclarativeBase):
+    pass
+
+
+# Intermediate table definitions
+class Group(SqlAlchemyBase):
+    __tablename__ = "groups"
+
+    id: orm.Mapped[GUID] = orm.mapped_column(GUID, primary_key=True, default=GUID.generate)
+
+
+class Household(SqlAlchemyBase):
+    __tablename__ = "households"
+
+    id: orm.Mapped[GUID] = orm.mapped_column(GUID, primary_key=True, default=GUID.generate)
+    group_id: orm.Mapped[GUID] = orm.mapped_column(GUID, sa.ForeignKey("groups.id"), nullable=False, index=True)
+
+
+class RecipeModel(SqlAlchemyBase):
+    __tablename__ = "recipes"
+
+    id: orm.Mapped[GUID] = orm.mapped_column(GUID, primary_key=True, default=GUID.generate)
+    group_id: orm.Mapped[GUID] = orm.mapped_column(GUID, sa.ForeignKey("groups.id"), nullable=False, index=True)
+    last_made: orm.Mapped[datetime | None] = orm.mapped_column(NaiveDateTime)
+
+
+class HouseholdToRecipe(SqlAlchemyBase):
+    __tablename__ = "households_to_recipes"
+
+    id: orm.Mapped[GUID] = orm.mapped_column(GUID, primary_key=True, default=GUID.generate)
+    household_id = sa.Column(GUID, sa.ForeignKey("households.id"), index=True, primary_key=True)
+    recipe_id = sa.Column(GUID, sa.ForeignKey("recipes.id"), index=True, primary_key=True)
+    last_made: orm.Mapped[datetime | None] = orm.mapped_column(NaiveDateTime)
+
+
+class IngredientFoodModel(SqlAlchemyBase):
+    __tablename__ = "ingredient_foods"
+
+    id: orm.Mapped[GUID] = orm.mapped_column(GUID, primary_key=True, default=GUID.generate)
+    group_id: orm.Mapped[GUID] = orm.mapped_column(GUID, sa.ForeignKey("groups.id"), nullable=False, index=True)
+    on_hand: orm.Mapped[bool] = orm.mapped_column(sa.Boolean, default=False)
+
+
+class Tool(SqlAlchemyBase):
+    __tablename__ = "tools"
+
+    id: orm.Mapped[GUID] = orm.mapped_column(GUID, primary_key=True, default=GUID.generate)
+    group_id: orm.Mapped[GUID] = orm.mapped_column(GUID, sa.ForeignKey("groups.id"), nullable=False, index=True)
+    on_hand: orm.Mapped[bool | None] = orm.mapped_column(sa.Boolean, default=False)
+
+
 def migrate_recipe_last_made_to_household(session: orm.Session):
-    pass  # TODO
+    for group in session.query(Group).all():
+        households = session.query(Household).filter(Household.group_id == group.id).all()
+        recipes = (
+            session.query(RecipeModel)
+            .filter(
+                RecipeModel.group_id == group.id,
+                RecipeModel.last_made != None,  # noqa E711
+            )
+            .all()
+        )
+
+        for recipe in recipes:
+            for household in households:
+                session.add(
+                    HouseholdToRecipe(
+                        household_id=household.id,
+                        recipe_id=recipe.id,
+                        last_made=recipe.last_made,
+                    )
+                )
 
 
 def migrate_foods_on_hand_to_household(session: orm.Session):
-    pass  # TODO
+    dialect = op.get_bind().dialect
+
+    for group in session.query(Group).all():
+        households = session.query(Household).filter(Household.group_id == group.id).all()
+        foods = (
+            session.query(IngredientFoodModel)
+            .filter(
+                IngredientFoodModel.group_id == group.id,
+                IngredientFoodModel.on_hand == True,  # noqa E712
+            )
+            .all()
+        )
+
+        for food in foods:
+            for household in households:
+                session.execute(
+                    sa.text(
+                        "INSERT INTO households_to_ingredient_foods (household_id, food_id)"
+                        "VALUES (:household_id, :food_id)"
+                    ),
+                    {
+                        "household_id": GUID.convert_value_to_guid(household.id, dialect),
+                        "food_id": GUID.convert_value_to_guid(food.id, dialect),
+                    },
+                )
 
 
 def migrate_tools_on_hand_to_household(session: orm.Session):
-    pass  # TODO
+    dialect = op.get_bind().dialect
+
+    for group in session.query(Group).all():
+        households = session.query(Household).filter(Household.group_id == group.id).all()
+        tools = (
+            session.query(Tool)
+            .filter(
+                Tool.group_id == group.id,
+                Tool.on_hand == True,  # noqa E712
+            )
+            .all()
+        )
+
+        for tool in tools:
+            for household in households:
+                session.execute(
+                    sa.text(
+                        "INSERT INTO households_to_tools (household_id, tool_id)" "VALUES (:household_id, :tool_id)"
+                    ),
+                    {
+                        "household_id": GUID.convert_value_to_guid(household.id, dialect),
+                        "tool_id": GUID.convert_value_to_guid(tool.id, dialect),
+                    },
+                )
 
 
 def migrate_to_new_models():
@@ -44,13 +166,13 @@ def migrate_to_new_models():
         migrate_tools_on_hand_to_household,
     ]:
         try:
+            logger.info(f"Running new model migration ({migration_func.__name__})")
             migration_func(session)
+            session.commit()
         except Exception:
             session.rollback()
-            logger.exception(f"Error during new model migration ({migration_func.__name__})")
+            logger.error(f"Error during new model migration ({migration_func.__name__})")
             raise
-
-    session.commit()
 
 
 def upgrade():
