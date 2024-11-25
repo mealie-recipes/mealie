d5ca15050f5cf0091eec21aa621e5ab7bdd74de9
Saireddy1369 <162394750+Saireddy1369@users.noreply.github.com>
2024-11-25 01:25:08 +0000

    fixed incorrect date format in Add to mealplan modal

--- frontend/components/Domain/Recipe/RecipeContextMenu.vue
+++ frontend/components/Domain/Recipe/RecipeContextMenu.vue
@@ -51,7 +51,7 @@
             <v-text-field
               v-model="newMealdate"
               :label="$t('general.date')"
-              :hint="$t('recipe.date-format-hint')"
+              :hint="$t('recipe.date-format-hint-yyyy-mm-dd')"
               persistent-hint
               :prepend-icon="$globals.icons.calendar"
               v-bind="attrs"
