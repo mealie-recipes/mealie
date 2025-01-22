f711eb0da284bdd079c9599a984eec99fa3b1116
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-01-22 15:45:12 +0000

    I don't think this does anything

--- mealie/routes/users/crud.py
+++ mealie/routes/users/crud.py
@@ -46,13 +46,6 @@ class AdminUserController(BaseAdminController):
 
     @admin_router.delete("/{item_id}")
     def delete_user(self, item_id: UUID4):
-        """Removes a user from the database. Must be the current user or a super user"""
-
-        assert_user_change_allowed(item_id, self.user, self.user)
-
-        if item_id == 1:  # TODO: identify super_user
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="SUPER_USER")
-
         self.mixins.delete_one(item_id)
 
 
