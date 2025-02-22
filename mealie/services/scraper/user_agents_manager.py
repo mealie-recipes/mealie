c2354f5e2a70ddd7796f09f58632757c3cef0dce
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-02-22 17:12:29 +0000

    fix file ref

--- mealie/services/scraper/user_agents_manager.py
+++ mealie/services/scraper/user_agents_manager.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 _USER_AGENTS_MANAGER: UserAgentsManager | None = None
 
 
@@ -15,6 +17,7 @@ def get_user_agents_manager() -> UserAgentsManager:
 class UserAgentsManager:
     def __init__(self) -> None:
         self._user_agents: list[str] | None = None
+        self._user_agents_text_path = os.path.join(os.path.dirname(__file__), "user-agents.txt")
 
     @property
     def user_agents(self) -> list[str]:
@@ -33,7 +36,7 @@ class UserAgentsManager:
         except (ImportError, KeyError):
             user_agents.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/128.0")
 
-        with open("user_agents.txt") as f:
+        with open(self._user_agents_text_path) as f:
             for line in f:
                 user_agents.append(line.strip())
 
