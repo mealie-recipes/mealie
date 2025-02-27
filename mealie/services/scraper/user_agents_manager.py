b29225fe761961ab0e18bf3da422132c1e2677c6
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-02-27 19:38:30 +0000

    remove br encoding

--- mealie/services/scraper/user_agents_manager.py
+++ mealie/services/scraper/user_agents_manager.py
@@ -29,7 +29,7 @@ class UserAgentsManager:
             "User-Agent": user_agent,
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Encoding": "gzip, deflate",
             "Connection": "keep-alive",
             "Upgrade-Insecure-Requests": "1",
             "Sec-Fetch-Dest": "document",
