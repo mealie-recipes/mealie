e76e820d37c767d3e6618be1b32da0becb742140
Litchi Pi <litchi.pi@proton.me>
2025-07-24 12:11:00 +0200

    fixup
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- mealie/services/recipe/recipe_service.py
+++ mealie/services/recipe/recipe_service.py
@@ -393,7 +393,7 @@ class RecipeService(RecipeServiceBase):
                     continue
 
                 if getattr(new_data, field) is None:
-                    raise exceptions.IncompleteData(missing, "Incomplete recipe")
+                    raise exceptions.IncompleteData(field, "Incomplete recipe")
 
         if recipe is None or recipe.settings is None:
             raise exceptions.NoEntryFound("Recipe not found.")
