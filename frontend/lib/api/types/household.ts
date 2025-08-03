cb340c8a09d26f1076f3b9e8d31ec15cc25c936c
Craig <c.matear@gmail.com>
2025-08-03 14:02:54 +0000

    Add Label notifier
    * Added new label notifier

--- frontend/lib/api/types/household.ts
+++ frontend/lib/api/types/household.ts
@@ -71,6 +71,9 @@ export interface GroupEventNotifierOptions {
   categoryCreated?: boolean;
   categoryUpdated?: boolean;
   categoryDeleted?: boolean;
+  labelCreated?: boolean;
+  labelUpdated?: boolean;
+  labelDeleted?: boolean;
 }
 export interface GroupEventNotifierOptionsOut {
   testMessage?: boolean;
@@ -95,6 +98,9 @@ export interface GroupEventNotifierOptionsOut {
   categoryCreated?: boolean;
   categoryUpdated?: boolean;
   categoryDeleted?: boolean;
+  labelCreated?: boolean;
+  labelUpdated?: boolean;
+  labelDeleted?: boolean;
   id: string;
 }
 export interface GroupEventNotifierOptionsSave {
@@ -120,6 +126,9 @@ export interface GroupEventNotifierOptionsSave {
   categoryCreated?: boolean;
   categoryUpdated?: boolean;
   categoryDeleted?: boolean;
+  labelCreated?: boolean;
+  labelUpdated?: boolean;
+  labelDeleted?: boolean;
   notifierId: string;
 }
 export interface GroupEventNotifierOut {
