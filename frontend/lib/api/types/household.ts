569170dac7e83257e280c23624aba0cc5cdd7274
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-08-05 16:29:14 +0000

    run codegen

--- frontend/lib/api/types/household.ts
+++ frontend/lib/api/types/household.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
-
+/* eslint-disable */
 /**
 /* This file was automatically generated from pydantic models by running pydantic2ts.
 /* Do not modify it by hand - just update the pydantic models and then re-run the script
@@ -166,6 +166,7 @@ export interface GroupRecipeActionOut {
 export interface GroupRecipeActionPayload {
   action: GroupRecipeActionOut;
   content: unknown;
+  recipeScale: number;
 }
 export interface HouseholdCreate {
   groupId?: string | null;
@@ -587,18 +588,21 @@ export interface RecipeSummary {
 }
 export interface RecipeCategory {
   id?: string | null;
+  groupId?: string | null;
   name: string;
   slug: string;
   [k: string]: unknown;
 }
 export interface RecipeTag {
   id?: string | null;
+  groupId?: string | null;
   name: string;
   slug: string;
   [k: string]: unknown;
 }
 export interface RecipeTool {
   id: string;
+  groupId?: string | null;
   name: string;
   slug: string;
   householdsWithTool?: string[];
