62c6cc6a72f160b4fec5b3908c5150fe056ba529
Richard van Leeuwen <rbavleeuwen@gmail.com>
2025-08-15 22:26:42 +0200

    Fixed ordering of cookbooks

--- frontend/pages/g/[groupSlug]/cookbooks/index.vue
+++ frontend/pages/g/[groupSlug]/cookbooks/index.vue
@@ -75,7 +75,7 @@
           :delay="250"
           :delay-on-touch-only="true"
           style="width: 100%"
-          @end="actions.updateOrder(myCookbooks)"
+          @end="updateAll(myCookbooks)"
         >
           <v-expansion-panel
             v-for="(cookbook, index) in myCookbooks"
@@ -160,7 +160,7 @@ export default defineNuxtComponent({
     });
 
     const $auth = useMealieAuth();
-    const { store: allCookbooks, actions } = useCookbookStore();
+    const { store: allCookbooks, actions, updateAll } = useCookbookStore();
 
     // Make a local reactive copy of myCookbooks
     const myCookbooks = ref<ReadCookBook[]>([]);
@@ -170,7 +170,7 @@ export default defineNuxtComponent({
         myCookbooks.value
           = cookbooks?.filter(
             cookbook => cookbook.householdId === $auth.user.value?.householdId,
-          ) ?? [];
+          ).sort((a, b) => a.position > b.position) ?? [];
       },
       { immediate: true },
     );
@@ -248,6 +248,9 @@ export default defineNuxtComponent({
       createTargetKey,
       createTarget,
       createCookbook,
+
+      // update
+      updateAll,
 
       // delete
       deleteTarget,
