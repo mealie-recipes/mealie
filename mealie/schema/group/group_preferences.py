031c5c02033f4ad9bad751646cfe776702dd18ac
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-08-23 01:36:33 +0000

    fix enum default using a validator instead

--- mealie/schema/group/group_preferences.py
+++ mealie/schema/group/group_preferences.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from typing import Any
 from uuid import UUID
 
-from pydantic import UUID4, ConfigDict
+from pydantic import UUID4, ConfigDict, Field, field_validator
 
 from mealie.schema._mealie import MealieModel
 
@@ -14,9 +15,20 @@ class GroupPreferencesPluralHandling(Enum):
 
 class UpdateGroupPreferences(MealieModel):
     private_group: bool = True
-    plural_handling: GroupPreferencesPluralHandling = GroupPreferencesPluralHandling.pluralize_food_without_unit
+    plural_handling: GroupPreferencesPluralHandling = Field(default=None, validate_default=True)
 
     model_config = ConfigDict(use_enum_values=True)
+
+    @field_validator("plural_handling", mode="before")
+    def validate_plural_handling(v: Any) -> str:
+        # We return the value as a string because Pydantic will convert it to an Enum automatically,
+        # and not doing this causes issues with sqlalchemy
+        if isinstance(v, Enum):
+            return v.value
+        elif not v:
+            return GroupPreferencesPluralHandling.pluralize_food_without_unit.value
+        else:
+            return v
 
 
 class CreateGroupPreferences(UpdateGroupPreferences):
