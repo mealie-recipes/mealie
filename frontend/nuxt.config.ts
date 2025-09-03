0a51f2a2393da7d08ac39828d31ee9a0c71e69ef
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-09-03 03:29:57 +0000

    fix frontend ignoring timeout var

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -148,7 +148,7 @@ export default defineNuxtConfig({
         signInResponseTokenPointer: "/access_token",
         type: "Bearer",
         cookieName: AUTH_TOKEN,
-        maxAgeInSeconds: 604800, // 7 days
+        maxAgeInSeconds: parseInt(process.env.TOKEN_TIME || "48") * 3600,
       },
       pages: {
         login: "/login",
