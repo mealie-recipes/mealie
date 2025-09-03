d29449c103516fa517733321af97222264ef4ae7
Kuchenpirat <24235032+Kuchenpirat@users.noreply.github.com>
2025-09-03 16:55:18 +0200

    Update frontend/components/Layout/LayoutParts/AppSidebar.vue
    Co-authored-by: Michael Genson <71845777+michael-genson@users.noreply.github.com>

--- frontend/components/Layout/LayoutParts/AppSidebar.vue
+++ frontend/components/Layout/LayoutParts/AppSidebar.vue
@@ -94,7 +94,7 @@
             <v-list-item :prepend-icon="$vuetify.theme.current.dark ? $globals.icons.weatherSunny : $globals.icons.weatherNight" :title="$vuetify.theme.current.dark ? $t('settings.theme.light-mode') : $t('settings.theme.dark-mode')" @click="toggleDark" />
             <v-divider v-if="loggedIn" class="my-2" />
             <v-list-item v-if="loggedIn" :prepend-icon="$globals.icons.cog" :title="$t('profile.user-settings')" to="/user/profile" />
-            <v-list-item v-if="canManage" :prepend-icon="$globals.icons.manageData" :title="$t('data-pages.data-management')" to="/user/profile" />
+            <v-list-item v-if="canManage" :prepend-icon="$globals.icons.manageData" :title="$t('data-pages.data-management')" to="/group/data" />
             <v-divider v-if="isAdmin" class="my-2" />
             <v-list-item v-if="isAdmin" :prepend-icon="$globals.icons.wrench" :title="$t('settings.admin-settings')" to="/admin/site-settings" />
           </v-list>
