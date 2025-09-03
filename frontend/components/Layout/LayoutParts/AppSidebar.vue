76bcb0b1eb003dce8aef2d44eb327c9dffe734b0
Kuchenpirat <jojow@gmx.net>
2025-09-03 08:32:10 +0000

    rename to user settings

--- frontend/components/Layout/LayoutParts/AppSidebar.vue
+++ frontend/components/Layout/LayoutParts/AppSidebar.vue
@@ -93,7 +93,7 @@
             <v-list-item :prepend-icon="$globals.icons.translate" :title="$t('sidebar.language')" @click="languageDialog=true" />
             <v-list-item :prepend-icon="$vuetify.theme.current.dark ? $globals.icons.weatherSunny : $globals.icons.weatherNight" :title="$vuetify.theme.current.dark ? $t('settings.theme.light-mode') : $t('settings.theme.dark-mode')" @click="toggleDark" />
             <v-divider class="my-2" />
-            <v-list-item v-if="loggedIn" :prepend-icon="$globals.icons.cog" :title="$t('general.settings')" to="/user/profile" />
+            <v-list-item v-if="loggedIn" :prepend-icon="$globals.icons.cog" :title="$t('settings.user-settings')" to="/user/profile" />
             <v-list-item v-if="isAdmin" :prepend-icon="$globals.icons.wrench" :title="$t('settings.admin-settings')" to="/admin/site-settings" />
           </v-list>
         </v-menu>
