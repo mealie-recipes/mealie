3b0c41c423c1d931b9fa0d2ef2fd154a3ac28597
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-09-07 01:49:03 +0000

    update the switch and the view

--- frontend/components/Domain/Recipe/RecipeNutrition.vue
+++ frontend/components/Domain/Recipe/RecipeNutrition.vue
@@ -1,11 +1,16 @@
 <template>
   <div v-if="valueNotNull || edit">
     <v-card class="mt-2">
-      <v-card-title v-if="showViewer && !useTableView"
-      class="pt-2 pb-0">
+      <v-card-title
+        v-if="showViewer"
+        class="pt-2 pb-0"
+      >
         {{ $t("recipe.nutrition") }}
       </v-card-title>
+
       <v-divider class="mx-2 my-1" />
+
+      <!-- Edit mode -->
       <v-card-text v-if="edit">
         <div
           v-for="(item, key, index) in modelValue"
@@ -23,6 +28,7 @@
           />
         </div>
       </v-card-text>
+
       <!-- Classic List View -->
       <v-list
         v-if="showViewer && !useTableView"
@@ -37,68 +43,73 @@
           <v-list-item-title class="pl-2 d-flex">
             <div>{{ item.label }}</div>
             <div class="ml-auto mr-1">
-              {{ item.value }}
+            {{ item.value }}
             </div>
             <div>{{ item.suffix }}</div>
           </v-list-item-title>
         </v-list-item>
       </v-list>
+
       <!-- Nutrition Information Table Format -->
-       <v-table
-          v-if="showViewer && useTableView"
-          class="mt-0 pt-0 nutrition-table"
-        >
-        <thead>
-          <tr>
-            <th colspan="3" style="text-align: center;">
-              NUTRITION INFORMATION
-            </th>
-          </tr>
-          <tr>
-            <th />
-            <th>Quantity per serving</th>
-          </tr>
-        </thead>
+      <div v-if="showViewer && useTableView" class="overflow-x-hidden">
+        <v-table density="compact">
+          <thead>
+            <tr>
+              <th />
+              <th class="text-end">
+Quantity per serving
+</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr v-for="(nutrient, index) in nutrients" :key="index">
-            <td :class="{ 'pl-8': nutrient.indent }">
-              {{ nutrient.label }}
-            </td>
-            <td class="pl-8 d-flex align-center">
-              <template v-if="nutrient.special">
-                <div class="mr-4">
-                  <span>
-                   {{ energyValue }}
-                  </span>
-                </div>
-                <v-switch
-                  v-model="showInKcal"
-                  color="primary"
-                  hide-details
-                  class="ml-auto"
-                  :label="showInKcal ? 'kcal' : 'kJ'"
-                />
-              </template>
-
-              <!-- Default -->
-              <template v-else>
-                {{ nutrient.value || '-' }} {{ nutrient.unit }}
-              </template>
-            </td>
-          </tr>
-        </tbody>
-      </v-table>
-      <v-switch
-          v-if="showViewer"
-          v-model="useTableView"
-          hide-details
-          color="primary"
-          class="ml-auto"
-          :label="useTableView ? 'Table view' : 'List view'"
-        />
+          <tbody>
+            <tr v-for="(nutrient, index) in nutrients" :key="index">
+              <td :class="{ 'pl-8': nutrient.indent }" style="width: 150px;">
+                {{ nutrient.label }}
+              </td>
+              <td class="ml-auto mr-1 text-right">
+                <template v-if="nutrient.special">
+                  <div>
+                    <span v-if="calView">
+                      {{ modelValue?.calories || '-' }} kcal
+                    </span>
+                    <span v-else>
+                      {{ modelValue?.calories ? (Number(modelValue.calories) * 4.184).toFixed(0) : '-' }} kJ
+                    </span>
+                  </div>
+                </template>
+                <template v-else>
+                  {{ nutrient.value || '-' }} {{ nutrient.unit }}
+                </template>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </div>
+      <!-- Toggle View -->
+        <div>
+          <v-switch
+            v-model="useTableView"
+            class="pl-8"
+            hide-details
+            density="compact"
+            :label="useTableView ? 'Table View' : 'Classic View'"
+            color="primary"
+          />
+        </div>
+      <!-- Toggle Cal View -->
+        <div v-if="useTableView ">
+          <v-switch
+            v-model="calView"
+            class="pl-8"
+            hide-details
+            density="compact"
+            :label="calView ? 'kcal' : 'kJ'"
+            color="primary"
+          />
+        </div>
     </v-card>
-</div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -147,29 +158,22 @@ const renderedList = computed(() => {
 });
 
 // Build nutrition information table format
-const useTableView = ref(true);
+const useTableView = ref(false);
+const calView = ref(false);
 
-const showInKcal = ref(true);
-
-const nutrients = computed(() => [
-  { label: "Energy", value: modelValue.value.calories, unit: "", special: true },
-  { label: "Protein", value: modelValue.value.proteinContent, unit: "g" },
-  { label: "Fat, total", value: modelValue.value.fatContent, unit: "g" },
-  { label: "– Saturated", value: modelValue.value.saturatedFatContent, unit: "g", indent: true },
-  { label: "Carbohydrate", value: modelValue.value.carbohydrateContent, unit: "g" },
-  { label: "– Sugars", value: modelValue.value.sugarContent, unit: "g", indent: true },
-  { label: "Dietary Fibre", value: modelValue.value.fiberContent, unit: "g" },
-  { label: "Sodium", value: modelValue.value.sodiumContent, unit: "mg" },
-]);
-
-const energyValue = computed(() => {
-  if (!modelValue.value.calories) return "-";
-  return showInKcal.value
-    ? `${modelValue.value.calories} kcal`
-    : `${(Number(modelValue.value.calories) * 4.184).toFixed(0)} kJ`;
+const nutrients = computed(() => {
+  const nutrition = modelValue.value || {} as Nutrition;
+  return [
+    { label: "Energy", value: nutrition.calories, unit: "", special: true },
+    { label: "Protein", value: nutrition.proteinContent, unit: "g" },
+    { label: "Fat, total", value: nutrition.fatContent, unit: "g" },
+    { label: "– Saturated", value: nutrition.saturatedFatContent, unit: "g", indent: true },
+    { label: "Carbohydrate", value: nutrition.carbohydrateContent, unit: "g" },
+    { label: "– Sugars", value: nutrition.sugarContent, unit: "g", indent: true },
+    { label: "Dietary Fibre", value: nutrition.fiberContent, unit: "g" },
+    { label: "Sodium", value: nutrition.sodiumContent, unit: "mg" },
+  ];
 });
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
