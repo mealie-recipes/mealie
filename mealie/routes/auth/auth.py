a784bc6e509074d7148cc625576d45650567cb1d
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-09-08 19:39:22 +0000

    disable samesite for auth cookies

--- mealie/routes/auth/auth.py
+++ mealie/routes/auth/auth.py
@@ -95,6 +95,7 @@ def get_token(
         httponly=True,
         max_age=expires_in,
         secure=settings.PRODUCTION,
+        samesite="none" if settings.PRODUCTION else "lax",  # required for iframes
     )
 
     return MealieAuthToken.respond(access_token)
@@ -155,6 +156,7 @@ async def oauth_callback(request: Request, response: Response, session: Session 
         httponly=True,
         max_age=expires_in,
         secure=settings.PRODUCTION,
+        samesite="none" if settings.PRODUCTION else "lax",  # required for iframes
     )
 
     return MealieAuthToken.respond(access_token)
