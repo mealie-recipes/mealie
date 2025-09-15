81c99d8d429a44f2fe604f284942747c2d320d1e
Aliyyan <muhammadqaliyyanwijaya@gmail.com>
2025-09-15 04:06:48 +0000

    Revert "add toggle to switch nutition tabel view"
    This reverts commit 7c84b7595fb2afd709f37a3ef39863a4c0a84aea.

--- frontend/components/Domain/Recipe/RecipeNutrition.vue
+++ frontend/components/Domain/Recipe/RecipeNutrition.vue
@@ -1,10 +1,7 @@
 <template>
   <div v-if="valueNotNull || edit">
     <v-card class="mt-2">
-      <v-card-title
-        v-if="showViewer"
-        class="pt-2 pb-0"
-      >
+      <v-card-title class="pt-2 pb-0">
         {{ $t("recipe.nutrition") }}
       </v-card-title>
 
@@ -28,10 +25,8 @@
           />
         </div>
       </v-card-text>
-
-      <!-- Classic List View -->
       <v-list
-        v-if="showViewer && !useTableView"
+        v-if="showViewer"
         density="compact"
         class="mt-0 pt-0"
       >
@@ -49,65 +44,6 @@
           </v-list-item-title>
         </v-list-item>
       </v-list>
-
-      <!-- Nutrition Information Table Format -->
-      <div v-if="showViewer && useTableView" class="overflow-x-hidden">
-        <v-table density="compact">
-          <thead>
-            <tr>
-              <th />
-              <th class="text-end">
-              Quantity per serving
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr v-for="(nutrient, index) in nutrients" :key="index">
-              <td :class="{ 'pl-8': nutrient.indent }" style="width: 150px;">
-                {{ nutrient.label }}
-              </td>
-              <td class="ml-auto mr-1 text-right">
-                <template v-if="nutrient.special">
-                  <div>
-                    <span v-if="calView">
-                      {{ modelValue?.calories || '-' }} kcal
-                    </span>
-                    <span v-else>
-                      {{ modelValue?.calories ? (Number(modelValue.calories) * 4.184).toFixed(0) : '-' }} kJ
-                    </span>
-                  </div>
-                </template>
-                <template v-else>
-                  {{ nutrient.value || '-' }} {{ nutrient.unit }}
-                </template>
-              </td>
-            </tr>
-          </tbody>
-        </v-table>
-      </div>
-      <!-- Toggle View -->
-        <div>
-          <v-switch
-            v-model="useTableView"
-            class="pl-8"
-            hide-details
-            density="compact"
-            :label="useTableView ? 'Table View' : 'Classic View'"
-            color="primary"
-          />
-        </div>
-      <!-- Toggle Cal View -->
-        <div v-if="useTableView ">
-          <v-switch
-            v-model="calView"
-            class="pl-8"
-            hide-details
-            density="compact"
-            :label="calView ? 'kcal' : 'kJ'"
-            color="primary"
-          />
-        </div>
     </v-card>
   </div>
 </template>
@@ -120,7 +56,6 @@ import type { NutritionLabelType } from "~/composables/recipes/use-recipe-nutrit
 interface Props {
   edit?: boolean;
 }
-
 const props = withDefaults(defineProps<Props>(), {
   edit: true,
 });
@@ -155,24 +90,6 @@ const renderedList = computed(() => {
     }
     return item;
   }, {});
-});
-
-// Build nutrition information table format
-const useTableView = ref(false);
-const calView = ref(false);
-
-const nutrients = computed(() => {
-  const nutrition = modelValue.value || {} as Nutrition;
-  return [
-    { label: "Energy", value: nutrition.calories, unit: "", special: true },
-    { label: "Protein", value: nutrition.proteinContent, unit: "g" },
-    { label: "Fat, total", value: nutrition.fatContent, unit: "g" },
-    { label: "– Saturated", value: nutrition.saturatedFatContent, unit: "g", indent: true },
-    { label: "Carbohydrate", value: nutrition.carbohydrateContent, unit: "g" },
-    { label: "– Sugars", value: nutrition.sugarContent, unit: "g", indent: true },
-    { label: "Dietary Fibre", value: nutrition.fiberContent, unit: "g" },
-    { label: "Sodium", value: nutrition.sodiumContent, unit: "mg" },
-  ];
 });
 </script>
 
