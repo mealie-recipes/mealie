5b5640d9ad72bc27d4a59cbb7adc8d913e3025c0
Nico Hirsch <mail@nicohirsch.com>
2025-10-03 15:45:30 +0000

    Update dialog Update this Meal Plan

--- frontend/pages/household/mealplan/planner/edit.vue
+++ frontend/pages/household/mealplan/planner/edit.vue
@@ -22,35 +22,18 @@
       "
       @close="resetDialog()"
     >
-      <v-card-text>
-        <v-menu
-          v-model="state.pickerMenu!"
-          :close-on-content-click="false"
-          transition="scale-transition"
-          offset-y
-          max-width="290px"
-          min-width="auto"
-        >
-          <template #activator="{ props }">
-            <v-text-field
-              v-model="newMealDateString"
-              :label="$t('general.date')"
-              :hint="$t('recipe.date-format-hint-yyyy-mm-dd')"
-              persistent-hint
-              :prepend-icon="$globals.icons.calendar"
-              v-bind="props"
-              readonly
-            />
-          </template>
-          <v-date-picker
-            v-model="newMeal.date"
-            hide-header
-            :first-day-of-week="firstDayOfWeek"
-            :local="$i18n.locale"
-            @update:model-value="state.pickerMenu = false"
-          />
-        </v-menu>
-        <v-card-text>
+      <v-card-text class="pb-2">
+        <v-date-picker
+          v-model="newMeal.date"
+          class="mx-auto"
+          hide-header
+          show-adjacent-months
+          color="primary"
+          :first-day-of-week="firstDayOfWeek"
+          :local="$i18n.locale"
+          @update:model-value="state.pickerMenu = false"
+        />
+        <v-card-text class="pb-0">
           <v-select
             v-model="newMeal.entryType"
             :return-object="false"
@@ -77,8 +60,8 @@
             <v-textarea v-model="newMeal.text" rows="2" :label="$t('meal-plan.meal-note')" />
           </template>
         </v-card-text>
-        <v-card-actions class="my-0 py-0">
-          <v-switch v-model="dialog.note" class="mt-n3" :label="$t('meal-plan.note-only')" />
+        <v-card-actions class="py-0 px-4">
+          <v-switch v-model="dialog.note" class="mt-n3 mb-n4" :label="$t('meal-plan.note-only')" />
         </v-card-actions>
       </v-card-text>
     </BaseDialog>
