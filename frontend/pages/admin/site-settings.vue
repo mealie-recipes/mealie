3d52198d6c82940541b4c1fb54c0b7003ab7d4e2
Brian Choromanski <BrianChoromanski@gmail.com>
2025-10-05 21:36:15 -0400

    Ignore nightly

--- frontend/pages/admin/site-settings.vue
+++ frontend/pages/admin/site-settings.vue
@@ -194,7 +194,7 @@
                   </a>
                 </v-list-item-subtitle>
               </template>
-              <template v-else-if="property.slot === 'version' && property.value !== 'develop'">
+              <template v-else-if="property.slot === 'version' && property.value !== 'develop' && property.value !== 'nightly'">
                 <v-list-item-subtitle>
                   <a
                     target="_blank"
