c3c314dca39db5abc3ee52a2cf5bda9522143364
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2025-10-07 19:08:31 +0000

    manually truncate passwords longer than 72 bytes

--- mealie/core/security/hasher.py
+++ mealie/core/security/hasher.py
@@ -3,7 +3,10 @@ from typing import Protocol
 
 import bcrypt
 
+from mealie.core import root_logger
 from mealie.core.config import get_app_settings
+
+logger = root_logger.get_logger()
 
 
 class Hasher(Protocol):
@@ -21,13 +24,24 @@ class FakeHasher:
 
 
 class BcryptHasher:
-    def hash(self, password: str) -> str:
+    def _get_password_bytes(self, password: str) -> bytes:
         password_bytes = password.encode("utf-8")
+        if len(password_bytes) > 72:
+            logger.warning(
+                "Password is longer than 72 bytes, which bcrypt does not support. "
+                "Manually truncating password to 72 bytes. Consider using a shorter password."
+            )
+            password_bytes = password_bytes[:72]
+
+        return password_bytes
+
+    def hash(self, password: str) -> str:
+        password_bytes = self._get_password_bytes(password)
         hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
         return hashed.decode("utf-8")
 
     def verify(self, password: str, hashed: str) -> bool:
-        password_bytes = password.encode("utf-8")
+        password_bytes = self._get_password_bytes(password)
         hashed_bytes = hashed.encode("utf-8")
         return bcrypt.checkpw(password_bytes, hashed_bytes)
 
