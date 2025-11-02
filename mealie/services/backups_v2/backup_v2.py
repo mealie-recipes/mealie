85edb4b8f6f6b669cce1930733e114284fde54eb
onemustpersist <55320914+onemustpersist@users.noreply.github.com>
2025-11-02 19:31:05 +0000

    Roll back to simpler clearing of settings cache

--- mealie/services/backups_v2/backup_v2.py
+++ mealie/services/backups_v2/backup_v2.py
@@ -65,6 +65,8 @@ class BackupV2(BaseService):
                 if f.name != ".secret":
                     continue
                 shutil.copyfile(f,self.directories.DATA_DIR / f.name) 
+                self.logger.info("invalidating appsettings cache")
+                get_app_settings.cache_clear()
                 continue
 
             shutil.rmtree(self.directories.DATA_DIR / f.name)
@@ -108,8 +110,4 @@ class BackupV2(BaseService):
             self.logger.info("restoring data directory")
             self._copy_data(contents.data_directory)
             self.logger.info("data directory restored successfully")
-            self.logger.info("invalidating settings cache")
-            get_app_settings.cache_clear()
-            get_app_settings()
-            self.logger.info("settings cache invalidated")
         self.logger.info("backup restore complete")
