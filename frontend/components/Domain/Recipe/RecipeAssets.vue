fe677580f8420b9116cac203d8256fc8529882e3
Simon Lam <simonlam6@gmail.com>
2025-11-23 16:37:28 -0600

    Fix rendering of asset entries

--- frontend/components/Domain/Recipe/RecipeAssets.vue
+++ frontend/components/Domain/Recipe/RecipeAssets.vue
@@ -28,11 +28,12 @@
           <v-list-item-title class="pl-2">
             {{ item.name }}
           </v-list-item-title>
-          <v-list-item-action>
+          <template #append>
             <v-btn
               v-if="!edit"
               color="primary"
               icon
+              size="small"
               :href="assetURL(item.fileName ?? '')"
               target="_blank"
               top
@@ -43,6 +44,7 @@
               <v-btn
                 color="error"
                 icon
+                size="small"
                 top
                 @click="model.splice(i, 1)"
               >
@@ -53,7 +55,7 @@
                 :copy-text="assetEmbed(item.fileName ?? '')"
               />
             </div>
-          </v-list-item-action>
+          </template>
         </v-list-item>
       </v-list>
     </v-card>
