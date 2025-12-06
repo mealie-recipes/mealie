37891f82db158ac0548fdaf515358f08df7c7d4b
darshanrathod05 <rathoddarshan676@gmail.com>
2025-12-06 21:02:50 +0000

    Added smart diet planner feature with calorie calculation

---
+++ frontend/pages/diet-planner/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <DietPlanner />
+</template>
+
+<script setup lang="ts">
+import DietPlanner from "~/components/DietPlanner.vue";
+
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<style scoped></style>
