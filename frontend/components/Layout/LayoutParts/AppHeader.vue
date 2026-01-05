230c06f712e70a142cb909dace0fe3d57e31567a
Patrick Lehner <patrick@lehnerpat.com>
2026-01-05 04:51:45 +0000

    fix: Move Data Management item in user menu further up

--- frontend/components/Layout/LayoutParts/AppHeader.vue
+++ frontend/components/Layout/LayoutParts/AppHeader.vue
@@ -87,13 +87,12 @@
               <v-divider class="my-2" />
             </template>
             <v-list-item :prepend-icon="$globals.icons.user" :title="$t('settings.profile')" :to="userProfileLink" />
+            <v-list-item v-if="canManage" :prepend-icon="$globals.icons.manageData" :title="$t('data-pages.data-management')" to="/group/data" />
             <v-divider class="my-2" />
             <v-list-item :prepend-icon="$globals.icons.translate" :title="$t('sidebar.language')" @click="languageDialog=true" />
             <!-- TODO: prevent menu from closing when toggling light/dark mode -->
             <v-list-item :prepend-icon="$vuetify.theme.current.dark ? $globals.icons.weatherSunny : $globals.icons.weatherNight" :title="$vuetify.theme.current.dark ? $t('settings.theme.light-mode') : $t('settings.theme.dark-mode')" @click="toggleDark" />
             <!-- TODO: what was the use case for sidebar settings in logged-out state? -->
-            <v-divider v-if="loggedIn" class="my-2" />
-            <v-list-item v-if="canManage" :prepend-icon="$globals.icons.manageData" :title="$t('data-pages.data-management')" to="/group/data" />
             <v-divider v-if="isAdmin" class="my-2" />
             <v-list-item v-if="isAdmin" :prepend-icon="$globals.icons.wrench" :title="$t('settings.admin-settings')" to="/admin/site-settings" />
             <v-divider class="my-2" />
