8a925b0df5fde659d2b4405abb8f8c13420ebbb1
Kuchenpirat <jojow@gmx.net>
2026-02-20 11:48:41 +0000

    more cleanup

--- frontend/composables/forms/use-field-types.ts
+++ frontend/composables/forms/use-field-types.ts
@@ -1,10 +1,7 @@
 export const fieldTypes = {
   TEXT: "text",
   TEXT_AREA: "textarea",
-  LIST: "list",
   SELECT: "select",
-  OBJECT: "object",
   BOOLEAN: "boolean",
-  COLOR: "color",
   PASSWORD: "password",
 } as const;
