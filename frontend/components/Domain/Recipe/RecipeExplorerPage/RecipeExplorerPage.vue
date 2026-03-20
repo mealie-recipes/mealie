f8504a4d1593524632f57676c95c4ea7a36af58a
Kuchenpirat <jojow@gmx.net>
2026-03-20 13:30:13 +0000

    Domain Recipe

--- frontend/components/Domain/Recipe/RecipeExplorerPage/RecipeExplorerPage.vue
+++ frontend/components/Domain/Recipe/RecipeExplorerPage/RecipeExplorerPage.vue
@@ -25,48 +25,32 @@
   </v-container>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import RecipeExplorerPageSearch from "./RecipeExplorerPageParts/RecipeExplorerPageSearch.vue";
 import { useLoggedInState } from "~/composables/use-logged-in-state";
 import RecipeCardSection from "~/components/Domain/Recipe/RecipeCardSection.vue";
 import { useLazyRecipes } from "~/composables/recipes";
 
-export default defineNuxtComponent({
-  components: { RecipeCardSection, RecipeExplorerPageSearch },
-  setup() {
-    const auth = useMealieAuth();
-    const route = useRoute();
+const auth = useMealieAuth();
+const route = useRoute();
 
-    const { isOwnGroup } = useLoggedInState();
-    const groupSlug = computed(() => route.params.groupSlug as string || auth.user.value?.groupSlug || "");
+const { isOwnGroup } = useLoggedInState();
+const groupSlug = computed(() => route.params.groupSlug as string || auth.user.value?.groupSlug || "");
 
-    const { recipes, appendRecipes, replaceRecipes } = useLazyRecipes(isOwnGroup.value ? null : groupSlug.value);
+const { recipes, appendRecipes, replaceRecipes } = useLazyRecipes(isOwnGroup.value ? null : groupSlug.value);
 
-    const ready = ref(false);
-    const searchComponent = ref<InstanceType<typeof RecipeExplorerPageSearch>>();
+const ready = ref(false);
+const searchComponent = ref<InstanceType<typeof RecipeExplorerPageSearch>>();
 
-    const searchQuery = computed(() => {
-      return searchComponent.value?.passedQueryWithSeed || {};
-    });
-
-    function onSearchReady() {
-      ready.value = true;
-    }
-
-    function onItemSelected(item: any, urlPrefix: string) {
-      searchComponent.value?.filterItems(item, urlPrefix);
-    }
-
-    return {
-      ready,
-      searchComponent,
-      searchQuery,
-      recipes,
-      appendRecipes,
-      replaceRecipes,
-      onSearchReady,
-      onItemSelected,
-    };
-  },
+const searchQuery = computed(() => {
+  return searchComponent.value?.passedQueryWithSeed || {};
 });
+
+function onSearchReady() {
+  ready.value = true;
+}
+
+function onItemSelected(item: any, urlPrefix: string) {
+  searchComponent.value?.filterItems(item, urlPrefix);
+}
 </script>
