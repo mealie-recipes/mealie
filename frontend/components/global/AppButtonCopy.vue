28b7760a0b1da62dd327005e6ba8a32d31093ace
Kuchenpirat <jojow@gmx.net>
2026-03-23 13:54:18 +0000

    Global App*

--- frontend/components/global/AppButtonCopy.vue
+++ frontend/components/global/AppButtonCopy.vue
@@ -8,7 +8,7 @@
     close-delay="500"
     transition="slide-y-transition"
   >
-    <template #activator="{ props }">
+    <template #activator="{ props: hoverProps }">
       <v-btn
         variant="flat"
         :icon="icon"
@@ -16,7 +16,7 @@
         retain-focus-on-click
         :class="btnClass"
         :disabled="copyText !== '' ? false : true"
-        v-bind="props"
+        v-bind="hoverProps"
         @click="textToClipboard()"
       >
         <v-icon>{{ $globals.icons.contentCopy }}</v-icon>
@@ -33,66 +33,53 @@
   </v-tooltip>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { useClipboard } from "@vueuse/core";
 
-export default defineNuxtComponent({
-  props: {
-    copyText: {
-      type: String,
-      required: true,
-    },
-    color: {
-      type: String,
-      default: "",
-    },
-    icon: {
-      type: Boolean,
-      default: true,
-    },
-    btnClass: {
-      type: String,
-      default: "",
-    },
+const props = defineProps({
+  copyText: {
+    type: String,
+    required: true,
   },
-  setup(props) {
-    const { copy, copied, isSupported } = useClipboard();
-    const show = ref(false);
-    const copyToolTip = ref<VTooltip | null>(null);
-    const copiedSuccess = ref<boolean | null>(null);
-
-    async function textToClipboard() {
-      if (isSupported.value) {
-        await copy(props.copyText);
-        if (copied.value) {
-          copiedSuccess.value = true;
-          console.info(`Copied\n${props.copyText}`);
-        }
-        else {
-          copiedSuccess.value = false;
-          console.error("Copy failed: ", copied.value);
-        }
-      }
-      else {
-        console.warn("Clipboard is currently not supported by your browser. Ensure you're on a secure (https) site.");
-      }
-
-      show.value = true;
-      setTimeout(() => {
-        show.value = false;
-      }, 3000);
-    }
-
-    return {
-      show,
-      copyToolTip,
-      textToClipboard,
-      copied,
-      isSupported,
-      copiedSuccess,
-    };
+  color: {
+    type: String,
+    default: "",
+  },
+  icon: {
+    type: Boolean,
+    default: true,
+  },
+  btnClass: {
+    type: String,
+    default: "",
   },
 });
+
+const { copy, copied, isSupported } = useClipboard();
+const show = ref(false);
+const copiedSuccess = ref<boolean | null>(null);
+
+async function textToClipboard() {
+  if (isSupported.value) {
+    await copy(props.copyText);
+    if (copied.value) {
+      copiedSuccess.value = true;
+      console.info(`Copied\n${props.copyText}`);
+    }
+    else {
+      copiedSuccess.value = false;
+      console.error("Copy failed: ", copied.value);
+    }
+  }
+  else {
+    console.warn("Clipboard is currently not supported by your browser. Ensure you're on a secure (https) site.");
+  }
+
+  show.value = true;
+  setTimeout(() => {
+    show.value = false;
+  }, 3000);
+}
 </script>
 
 <style lang="scss" scoped></style>
--- frontend/components/global/AppLoader.vue
+++ frontend/components/global/AppLoader.vue
@@ -39,71 +39,63 @@
   </div>
 </template>
 
-<script lang="ts">
-export default defineNuxtComponent({
-  props: {
-    loading: {
-      type: Boolean,
-      default: true,
-    },
-    tiny: {
-      type: Boolean,
-      default: false,
-    },
-    small: {
-      type: Boolean,
-      default: false,
-    },
-    medium: {
-      type: Boolean,
-      default: true,
-    },
-    large: {
-      type: Boolean,
-      default: false,
-    },
-    waitingText: {
-      type: String,
-      default: undefined,
-    },
+<script setup lang="ts">
+const props = defineProps({
+  loading: {
+    type: Boolean,
+    default: true,
   },
-  setup(props) {
-    const size = computed(() => {
-      if (props.tiny) {
-        return {
-          width: 2,
-          icon: 0,
-          size: 25,
-        };
-      }
-      if (props.small) {
-        return {
-          width: 2,
-          icon: 30,
-          size: 50,
-        };
-      }
-      else if (props.large) {
-        return {
-          width: 4,
-          icon: 120,
-          size: 200,
-        };
-      }
-      return {
-        width: 3,
-        icon: 75,
-        size: 125,
-      };
-    });
-
-    const i18n = useI18n();
-    const waitingTextCalculated = props.waitingText == null ? i18n.t("general.loading-recipes") : props.waitingText;
-
-    return {
-      size,
-      waitingTextCalculated,
-    };
+  tiny: {
+    type: Boolean,
+    default: false,
+  },
+  small: {
+    type: Boolean,
+    default: false,
+  },
+  medium: {
+    type: Boolean,
+    default: true,
+  },
+  large: {
+    type: Boolean,
+    default: false,
+  },
+  waitingText: {
+    type: String,
+    default: undefined,
   },
 });
+
+const size = computed(() => {
+  if (props.tiny) {
+    return {
+      width: 2,
+      icon: 0,
+      size: 25,
+    };
+  }
+  if (props.small) {
+    return {
+      width: 2,
+      icon: 30,
+      size: 50,
+    };
+  }
+  else if (props.large) {
+    return {
+      width: 4,
+      icon: 120,
+      size: 200,
+    };
+  }
+  return {
+    width: 3,
+    icon: 75,
+    size: 125,
+  };
+});
+
+const i18n = useI18n();
+const waitingTextCalculated = props.waitingText == null ? i18n.t("general.loading-recipes") : props.waitingText;
 </script>
