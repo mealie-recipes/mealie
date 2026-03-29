6db7f45237cfcd2141da22785f7ea8112af96b05
Michael Genson <genson.michael@gmail.com>
2026-03-29 00:58:05 +0000

    added comment for clarity

--- mealie/services/scraper/scraper_strategies.py
+++ mealie/services/scraper/scraper_strategies.py
@@ -78,7 +78,7 @@ async def safe_scrape_html(url: str) -> str:
 
         transport = safehttp.AsyncSafeTransport(
             impersonate=impersonation,
-            verify=False,
+            verify=False,  # disable SSL verification since we can handle untrusted data and some sites don't have certs
         )
         async with AsyncClient(transport=transport) as client:
             async with client.stream(
