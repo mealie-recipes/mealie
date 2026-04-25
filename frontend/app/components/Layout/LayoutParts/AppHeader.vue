921a8dadc1b0239f044565c31627db934cb98fa8
Dan Schulte <dan.schulte.qub@live.com>
2026-04-25 14:57:26 -0700

    Make a dummy change to try to trigger the GitHub Actions

--- frontend/app/components/Layout/LayoutParts/AppHeader.vue
+++ frontend/app/components/Layout/LayoutParts/AppHeader.vue
@@ -25,7 +25,7 @@
         style="cursor: pointer"
         @click="$router.push(routerLink)"
       >
-        TrailFood Library
+        TrailFood Library (I think?)
       </v-toolbar-title>
     </div>
     <RecipeDialogSearch ref="domSearchDialog" />
