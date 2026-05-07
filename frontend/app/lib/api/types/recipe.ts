1b834efb0d0ec4762c938ad676339f7281ad5539
Justin Hallier <justin.hallier+git@gmail.com>
2026-05-07 21:16:41 +0000

    Update linked ingredients to include linked notes
    Update the linked ingredients menu item, picker and cook mode rendering to render both the linked
    ingredients an the linked notes in the same places

--- frontend/app/lib/api/types/recipe.ts
+++ frontend/app/lib/api/types/recipe.ts
@@ -298,12 +298,17 @@ export interface RecipeTool {
   recipeCount?: number;
   householdsWithTool?: string[];
 }
+export interface NoteReference {
+  referenceId?: string | null;
+}
+
 export interface RecipeStep {
   id?: string | null;
   title?: string | null;
   summary?: string | null;
   text: string;
   ingredientReferences?: IngredientReferences[];
+  noteReferences?: NoteReference[];
 }
 export interface RecipeAsset {
   name: string;
@@ -313,6 +318,7 @@ export interface RecipeAsset {
 export interface RecipeNote {
   title: string;
   text: string;
+  referenceId?: string;
 }
 export interface RecipeCommentOut {
   recipeId: string;
