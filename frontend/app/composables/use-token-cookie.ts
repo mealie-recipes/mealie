baacdc3efddfff50bd4f827b50d22fe2751c2139
Michael Genson <genson.michael@gmail.com>
2026-06-17 15:44:56 +0000

    fix jank type

--- frontend/app/composables/use-token-cookie.ts
+++ frontend/app/composables/use-token-cookie.ts
@@ -1,9 +1,9 @@
-export function getTokenCookieOptions(): Parameters<typeof useCookie>[1] {
+export function getTokenCookieOptions() {
   const isSecureConnection = useNuxtApp().$appInfo.production && window?.location?.protocol === "https:";
   return {
     maxAge: useNuxtApp().$appInfo.tokenTime * 60 * 60,
     secure: isSecureConnection,
-    sameSite: isSecureConnection ? "none" : "lax",
+    sameSite: (isSecureConnection ? "none" : "lax") as "none" | "lax",
     partitioned: isSecureConnection,
   };
 }
