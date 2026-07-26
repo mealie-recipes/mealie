944c5e911f4e918ff0c69c25716c67a758893e48
Miah <miah120@proton.me>
2026-07-26 21:05:36 +0000

    data unit tests

--- frontend/app/composables/shopping-list-page/sub-composables/use-shopping-list-data.ts
+++ frontend/app/composables/shopping-list-page/sub-composables/use-shopping-list-data.ts
@@ -5,7 +5,12 @@ import { useShoppingListItemActions } from "~/composables/shopping-list-page/sub
 /**
  * Composable for managing shopping list data fetching and polling
  */
-export function useShoppingListData(listId: string, shoppingList: Ref<ShoppingListOut | null>, loadingCounter: Ref<number>) {
+export function useShoppingListData(
+  listId: string,
+  shoppingList: Ref<ShoppingListOut | null>,
+  loadingCounter: Ref<number>,
+  maxAttempts = 17280,
+) {
   const isOffline = computed(() => useOnline().value === false);
   const { idle } = useIdle(5 * 60 * 1000); // 5 minutes
   const shoppingListItemActions = useShoppingListItemActions(listId);
@@ -88,7 +93,6 @@ export function useShoppingListData(listId: string, shoppingList: Ref<ShoppingLi
   // max poll time = pollFrequency * maxAttempts = 24 hours
   // we use a long max poll time since polling stops when the user is idle anyway
   const pollFrequency = 5000;
-  const maxAttempts = 17280;
   let attempts = 0;
   let pollTimer: ReturnType<typeof setInterval>;
 
