d3cedf23d9c5d4b350c50351621564c40e161800
Michael Genson <genson.michael@gmail.com>
2026-08-10 16:21:54 +0000

    fix missing admin passthrough var

--- mealie/routes/admin/admin_about.py
+++ mealie/routes/admin/admin_about.py
@@ -36,6 +36,7 @@ class AdminAboutController(BaseAdminController):
             enable_oidc=settings.OIDC_AUTH_ENABLED,
             oidc_redirect=settings.OIDC_AUTO_REDIRECT,
             oidc_provider_name=settings.OIDC_PROVIDER_NAME,
+            default_locale=settings.DEFAULT_LOCALE,
         )
 
     @router.get("/statistics", response_model=AppStatistics)
