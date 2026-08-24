2c58d140e5e1a20d23c0e8040abce457493a5970
Michael Genson <genson.michael@gmail.com>
2026-08-24 03:17:27 +0000

    remove deprecated config option

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -198,7 +198,6 @@ export default defineNuxtConfig({
     ],
     strategy: "no_prefix",
     lazy: true,
-    types: "composition",
     langDir: "./../app/lang/locales", // note: we need to up one ../ because the default root of lang dir is the /frontend/i18n, which can not be configured
     defaultLocale: "en-US",
     detectBrowserLanguage: {
