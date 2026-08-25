9508e28d5cadff1da8178782238419211bba70e7
Aditya <aditya@bncw.in>
2026-08-26 04:05:00 +0530

    fix(ui): show nutrition values with units in editor

--- frontend/app/composables/recipes/index.ts
+++ frontend/app/composables/recipes/index.ts
@@ -2,6 +2,6 @@ export { useFraction } from "./use-fraction";
 export { useRecipe } from "./use-recipe";
 export { useRecipes, recentRecipes, allRecipes, useLazyRecipes } from "./use-recipes";
 export { useIngredientTextParser } from "./use-recipe-ingredients";
-export { useNutritionLabels } from "./use-recipe-nutrition";
+export { parseNutritionValue, useNutritionLabels } from "./use-recipe-nutrition";
 export { useTools } from "./use-recipe-tools";
 export { useRecipePermissions } from "./use-recipe-permissions";
