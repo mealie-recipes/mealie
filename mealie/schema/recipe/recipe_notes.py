9d657a0d5060216a0df8867babd1d7062f025085
jallier <jallier@users.noreply.github.com>
2026-09-13 00:15:30 +0000

    feat: add recipe note linking to recipe instructions (#7591)

--- mealie/schema/recipe/recipe_notes.py
+++ mealie/schema/recipe/recipe_notes.py
@@ -1,7 +1,17 @@
-from pydantic import BaseModel, ConfigDict
+from uuid import UUID, uuid4
+
+from pydantic import ConfigDict, Field, field_validator
+
+from mealie.schema._mealie import MealieModel
 
 
-class RecipeNote(BaseModel):
+class RecipeNote(MealieModel):
     title: str
     text: str
+    reference_id: UUID = Field(default_factory=uuid4)
     model_config = ConfigDict(from_attributes=True)
+
+    @field_validator("reference_id", mode="before")
+    @classmethod
+    def ensure_reference_id(cls, value) -> UUID:
+        return value or uuid4()
