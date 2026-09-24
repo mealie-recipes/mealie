62aa908512984c0337c29206796f4d05585dc3c7
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2026-09-24 13:18:44 -0500

    feat: convert between metric/imperial units (#8292)

--- frontend/app/lang/locales/zh-CN.ts
+++ frontend/app/lang/locales/zh-CN.ts
@@ -1,8 +1,12 @@
 export default defineI18nLocale(async () => {
   const { zhHans: $vuetify } = await import("vuetify/locale");
   const { default: zhCN } = await import("../messages/zh-CN.json");
+  const { default: unitNames } = await import(
+    "../../../../mealie/repos/seed/resources/units/locales/zh-CN.json",
+  );
   return {
     ...zhCN,
+    "unit-names": unitNames,
     $vuetify,
   };
 });
